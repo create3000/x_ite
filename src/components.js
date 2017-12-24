bc75cc3bfb48a52f88a37a2f55c4d02b537d1253
Holger Seelig <holger.seelig@yahoo.de>
2017-12-25 00:07:29 +0100

    Published version 4.1.3-190

---
+++ src/components.js
@@ -0,0 +1,117 @@
+	var X_ITE_COMPONENTS_EXAMPLES =
+	[
+		{ name: 'Components' },
+
+		{ name: 'Cube Map Texturing' },
+		{ name: 'Composed Cube Map Texture', url: [ "http://media.create3000.de/components/CubeMapTexturing/ComposedCubeMapTexture/ComposedCubeMapTexture.x3d" ], archive: "http://media.create3000.de/components/CubeMapTexturing/ComposedCubeMapTexture/ComposedCubeMapTexture.zip" },
+
+		{ name: 'Environmental Effects' },
+		{ name: 'Background', url: [ "http://media.create3000.de/components/EnvironmentalEffects/Background/Background.x3d" ], archive: "http://media.create3000.de/components/EnvironmentalEffects/Background/Background.zip" },
+		{ name: 'Fog', url: [ "http://media.create3000.de/components/EnvironmentalEffects/Fog/Fog.x3d" ], archive: "http://media.create3000.de/components/EnvironmentalEffects/Fog/Fog.zip" },
+		{ name: 'Local Fog', url: [ "http://media.create3000.de/components/EnvironmentalEffects/LocalFog/LocalFog.x3d" ], archive: "http://media.create3000.de/components/EnvironmentalEffects/LocalFog/LocalFog.zip" },
+		{ name: 'Texture Background', url: [ "http://media.create3000.de/components/EnvironmentalEffects/TextureBackground/TextureBackground.x3d" ], archive: "http://media.create3000.de/components/EnvironmentalEffects/TextureBackground/TextureBackground.zip" },
+
+		{ name: 'Environmental Sensor' },
+		{ name: 'Proximity Sensor', url: [ "http://media.create3000.de/components/EnvironmentalSensor/ProximitySensor/ProximitySensor.x3d" ], archive: "http://media.create3000.de/components/EnvironmentalSensor/ProximitySensor/ProximitySensor.zip" },
+		{ name: 'Transform Sensor', url: [ "http://media.create3000.de/components/EnvironmentalSensor/TransformSensor/TransformSensor.x3d" ], archive: "http://media.create3000.de/components/EnvironmentalSensor/TransformSensor/TransformSensor.zip" },
+		{ name: 'Visibility Sensor', url: [ "http://media.create3000.de/components/EnvironmentalSensor/VisibilitySensor/VisibilitySensor.x3d" ], archive: "http://media.create3000.de/components/EnvironmentalSensor/VisibilitySensor/VisibilitySensor.zip" },
+
+		{ name: 'Event Utilities' },
+		{ name: 'Integer Sequencer', url: [ "http://media.create3000.de/components/EventUtilities/IntegerSequencer/IntegerSequencer.x3d" ], archive: "http://media.create3000.de/components/EventUtilities/IntegerSequencer/IntegerSequencer.zip" },
+
+		{ name: 'Follower' },
+		{ name: 'Color Chaser', url: [ "http://media.create3000.de/components/Follower/ColorChaser/ColorChaser.x3d" ], archive: "http://media.create3000.de/components/Follower/ColorChaser/ColorChaser.zip" },
+		{ name: 'Color Damper', url: [ "http://media.create3000.de/components/Follower/ColorDamper/ColorDamper.x3d" ], archive: "http://media.create3000.de/components/Follower/ColorDamper/ColorDamper.zip" },
+
+		{ name: 'Geometry2D' },
+		{ name: 'Arc2D', url: [ "http://media.create3000.de/components/Geometry2D/Arc2D/Arc2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/Arc2D/Arc2D.zip" },
+		{ name: 'Arc Close2D', url: [ "http://media.create3000.de/components/Geometry2D/ArcClose2D/ArcClose2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/ArcClose2D/ArcClose2D.zip" },
+		{ name: 'Circle2D', url: [ "http://media.create3000.de/components/Geometry2D/Circle2D/Circle2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/Circle2D/Circle2D.zip" },
+		{ name: 'Disk2D', url: [ "http://media.create3000.de/components/Geometry2D/Disk2D/Disk2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/Disk2D/Disk2D.zip" },
+		{ name: 'Polyline2D', url: [ "http://media.create3000.de/components/Geometry2D/Polyline2D/Polyline2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/Polyline2D/Polyline2D.zip" },
+		{ name: 'Polypoint2D', url: [ "http://media.create3000.de/components/Geometry2D/Polypoint2D/Polypoint2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/Polypoint2D/Polypoint2D.zip" },
+		{ name: 'Rectangle2D', url: [ "http://media.create3000.de/components/Geometry2D/Rectangle2D/Rectangle2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/Rectangle2D/Rectangle2D.zip" },
+		{ name: 'Triangle Set2D', url: [ "http://media.create3000.de/components/Geometry2D/TriangleSet2D/TriangleSet2D.x3d" ], archive: "http://media.create3000.de/components/Geometry2D/TriangleSet2D/TriangleSet2D.zip" },
+
+		{ name: 'Geometry3D' },
+		{ name: 'Box', url: [ "http://media.create3000.de/components/Geometry3D/Box/Box.x3d" ], archive: "http://media.create3000.de/components/Geometry3D/Box/Box.zip" },
+		{ name: 'Cone', url: [ "http://media.create3000.de/components/Geometry3D/Cone/Cone.x3d" ], archive: "http://media.create3000.de/components/Geometry3D/Cone/Cone.zip" },
+		{ name: 'Cylinder', url: [ "http://media.create3000.de/components/Geometry3D/Cylinder/Cylinder.x3d" ], archive: "http://media.create3000.de/components/Geometry3D/Cylinder/Cylinder.zip" },
+		{ name: 'Elevation Grid', url: [ "http://media.create3000.de/components/Geometry3D/ElevationGrid/ElevationGrid.x3d" ], archive: "http://media.create3000.de/components/Geometry3D/ElevationGrid/ElevationGrid.zip" },
+		{ name: 'Extrusion', url: [ "http://media.create3000.de/components/Geometry3D/Extrusion/Extrusion.x3d" ], archive: "http://media.create3000.de/components/Geometry3D/Extrusion/Extrusion.zip" },
+		{ name: 'Indexed Face Set', url: [ "http://media.create3000.de/components/Geometry3D/IndexedFaceSet/IndexedFaceSet.x3d" ], archive: "http://media.create3000.de/components/Geometry3D/IndexedFaceSet/IndexedFaceSet.zip" },
+		{ name: 'Sphere', url: [ "http://media.create3000.de/components/Geometry3D/Sphere/Sphere.x3d" ], archive: "http://media.create3000.de/components/Geometry3D/Sphere/Sphere.zip" },
+
+		{ name: 'Geospatial' },
+		{ name: 'Geo Elevation Grid', url: [ "http://media.create3000.de/components/Geospatial/GeoElevationGrid/GeoElevationGrid.x3d" ], archive: "http://media.create3000.de/components/Geospatial/GeoElevationGrid/GeoElevationGrid.zip" },
+		{ name: 'Geo Viewpoint', url: [ "http://media.create3000.de/components/Geospatial/GeoViewpoint/GeoViewpoint.x3d" ], archive: "http://media.create3000.de/components/Geospatial/GeoViewpoint/GeoViewpoint.zip" },
+
+		{ name: 'Grouping' },
+		{ name: 'Group', url: [ "http://media.create3000.de/components/Grouping/Group/Group.x3d" ], archive: "http://media.create3000.de/components/Grouping/Group/Group.zip" },
+		{ name: 'Transform', url: [ "http://media.create3000.de/components/Grouping/Transform/Transform.x3d" ], archive: "http://media.create3000.de/components/Grouping/Transform/Transform.zip" },
+
+		{ name: 'Interpolation' },
+		{ name: 'Color Interpolator', url: [ "http://media.create3000.de/components/Interpolation/ColorInterpolator/ColorInterpolator.x3d" ], archive: "http://media.create3000.de/components/Interpolation/ColorInterpolator/ColorInterpolator.zip" },
+		{ name: 'Orientation Interpolator', url: [ "http://media.create3000.de/components/Interpolation/OrientationInterpolator/OrientationInterpolator.x3d" ], archive: "http://media.create3000.de/components/Interpolation/OrientationInterpolator/OrientationInterpolator.zip" },
+		{ name: 'Position Interpolator', url: [ "http://media.create3000.de/components/Interpolation/PositionInterpolator/PositionInterpolator.x3d" ], archive: "http://media.create3000.de/components/Interpolation/PositionInterpolator/PositionInterpolator.zip" },
+		{ name: 'Position Interpolator2D', url: [ "http://media.create3000.de/components/Interpolation/PositionInterpolator2D/PositionInterpolator2D.x3d" ], archive: "http://media.create3000.de/components/Interpolation/PositionInterpolator2D/PositionInterpolator2D.zip" },
+		{ name: 'Scalar Interpolator', url: [ "http://media.create3000.de/components/Interpolation/ScalarInterpolator/ScalarInterpolator.x3d" ], archive: "http://media.create3000.de/components/Interpolation/ScalarInterpolator/ScalarInterpolator.zip" },
+		{ name: 'Spline Scalar Interpolator', url: [ "http://media.create3000.de/components/Interpolation/SplineScalarInterpolator/SplineScalarInterpolator.x3d" ], archive: "http://media.create3000.de/components/Interpolation/SplineScalarInterpolator/SplineScalarInterpolator.zip" },
+		{ name: 'Squad Orientation Interpolator', url: [ "http://media.create3000.de/components/Interpolation/SquadOrientationInterpolator/SquadOrientationInterpolator.x3d" ], archive: "http://media.create3000.de/components/Interpolation/SquadOrientationInterpolator/SquadOrientationInterpolator.zip" },
+
+		{ name: 'Layering' },
+		{ name: 'Layer Set', url: [ "http://media.create3000.de/components/Layering/LayerSet/LayerSet.x3d" ], archive: "http://media.create3000.de/components/Layering/LayerSet/LayerSet.zip" },
+		{ name: 'Viewport', url: [ "http://media.create3000.de/components/Layering/Viewport/Viewport.x3d" ], archive: "http://media.create3000.de/components/Layering/Viewport/Viewport.zip" },
+
+		{ name: 'Navigation' },
+		{ name: 'Billboard', url: [ "http://media.create3000.de/components/Navigation/Billboard/Billboard.x3d" ], archive: "http://media.create3000.de/components/Navigation/Billboard/Billboard.zip" },
+		{ name: 'Collision', url: [ "http://media.create3000.de/components/Navigation/Collision/Collision.x3d" ], archive: "http://media.create3000.de/components/Navigation/Collision/Collision.zip" },
+		{ name: 'Viewpoint', url: [ "http://media.create3000.de/components/Navigation/Viewpoint/Viewpoint.x3d" ], archive: "http://media.create3000.de/components/Navigation/Viewpoint/Viewpoint.zip" },
+
+		{ name: 'Networking' },
+		{ name: 'Anchor', url: [ "http://media.create3000.de/components/Networking/Anchor/Anchor.x3d" ], archive: "http://media.create3000.de/components/Networking/Anchor/Anchor.zip" },
+		{ name: 'Inline', url: [ "http://media.create3000.de/components/Networking/Inline/Inline.x3d" ], archive: "http://media.create3000.de/components/Networking/Inline/Inline.zip" },
+		{ name: 'Load Sensor', url: [ "http://media.create3000.de/components/Networking/LoadSensor/LoadSensor.x3d" ], archive: "http://media.create3000.de/components/Networking/LoadSensor/LoadSensor.zip" },
+
+		{ name: 'Particle Systems' },
+		{ name: 'Cone Emitter', url: [ "http://media.create3000.de/components/ParticleSystems/ConeEmitter/ConeEmitter.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/ConeEmitter/ConeEmitter.zip" },
+		{ name: 'Explosion Emitter', url: [ "http://media.create3000.de/components/ParticleSystems/ExplosionEmitter/ExplosionEmitter.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/ExplosionEmitter/ExplosionEmitter.zip" },
+		{ name: 'Force Physics Model', url: [ "http://media.create3000.de/components/ParticleSystems/ForcePhysicsModel/ForcePhysicsModel.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/ForcePhysicsModel/ForcePhysicsModel.zip" },
+		{ name: 'Particle System', url: [ "http://media.create3000.de/components/ParticleSystems/ParticleSystem/ParticleSystem.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/ParticleSystem/ParticleSystem.zip" },
+		{ name: 'Point Emitter', url: [ "http://media.create3000.de/components/ParticleSystems/PointEmitter/PointEmitter.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/PointEmitter/PointEmitter.zip" },
+		{ name: 'Polyline Emitter', url: [ "http://media.create3000.de/components/ParticleSystems/PolylineEmitter/PolylineEmitter.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/PolylineEmitter/PolylineEmitter.zip" },
+		{ name: 'Surface Emitter', url: [ "http://media.create3000.de/components/ParticleSystems/SurfaceEmitter/SurfaceEmitter.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/SurfaceEmitter/SurfaceEmitter.zip" },
+		{ name: 'Volume Editor', url: [ "http://media.create3000.de/components/ParticleSystems/VolumeEditor/VolumeEditor.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/VolumeEditor/VolumeEditor.zip" },
+		{ name: 'Wind Physics Model', url: [ "http://media.create3000.de/components/ParticleSystems/WindPhysicsModel/WindPhysicsModel.x3d" ], archive: "http://media.create3000.de/components/ParticleSystems/WindPhysicsModel/WindPhysicsModel.zip" },
+
+		{ name: 'Pointing Device Sensor' },
+		{ name: 'Cylinder Sensor', url: [ "http://media.create3000.de/components/PointingDeviceSensor/CylinderSensor/CylinderSensor.x3d" ], archive: "http://media.create3000.de/components/PointingDeviceSensor/CylinderSensor/CylinderSensor.zip" },
+		{ name: 'Plane Sensor', url: [ "http://media.create3000.de/components/PointingDeviceSensor/PlaneSensor/PlaneSensor.x3d" ], archive: "http://media.create3000.de/components/PointingDeviceSensor/PlaneSensor/PlaneSensor.zip" },
+
+		{ name: 'Rendering' },
+		{ name: 'Color', url: [ "http://media.create3000.de/components/Rendering/Color/Color.x3d" ], archive: "http://media.create3000.de/components/Rendering/Color/Color.zip" },
+		{ name: 'Line Set', url: [ "http://media.create3000.de/components/Rendering/LineSet/LineSet.x3d" ], archive: "http://media.create3000.de/components/Rendering/LineSet/LineSet.zip" },
+
+		{ name: 'Shaders' },
+		{ name: 'Composed Shader', url: [ "http://media.create3000.de/components/Shaders/ComposedShader/ComposedShader.x3d" ], archive: "http://media.create3000.de/components/Shaders/ComposedShader/ComposedShader.zip" },
+		{ name: 'Shader Part', url: [ "http://media.create3000.de/components/Shaders/ShaderPart/ShaderPart.x3d" ], archive: "http://media.create3000.de/components/Shaders/ShaderPart/ShaderPart.zip" },
+
+		{ name: 'Shape' },
+		{ name: 'Appearance', url: [ "http://media.create3000.de/components/Shape/Appearance/Appearance.x3d" ], archive: "http://media.create3000.de/components/Shape/Appearance/Appearance.zip" },
+		{ name: 'Material', url: [ "http://media.create3000.de/components/Shape/Material/Material.x3d" ], archive: "http://media.create3000.de/components/Shape/Material/Material.zip" },
+		{ name: 'Two Sided Material', url: [ "http://media.create3000.de/components/Shape/TwoSidedMaterial/TwoSidedMaterial.x3d" ], archive: "http://media.create3000.de/components/Shape/TwoSidedMaterial/TwoSidedMaterial.zip" },
+
+		{ name: 'Text' },
+		{ name: 'Font Style', url: [ "http://media.create3000.de/components/Text/FontStyle/FontStyle.x3d" ], archive: "http://media.create3000.de/components/Text/FontStyle/FontStyle.zip" },
+		{ name: 'Text', url: [ "http://media.create3000.de/components/Text/Text/Text.x3d" ], archive: "http://media.create3000.de/components/Text/Text/Text.zip" },
+
+		{ name: 'Texturing' },
+		{ name: 'Pixel Texture', url: [ "http://media.create3000.de/components/Texturing/PixelTexture/PixelTexture.x3d" ], archive: "http://media.create3000.de/components/Texturing/PixelTexture/PixelTexture.zip" },
+
+		{ name: 'Time' },
+		{ name: 'Time Sensor', url: [ "http://media.create3000.de/components/Time/TimeSensor/TimeSensor.x3d" ], archive: "http://media.create3000.de/components/Time/TimeSensor/TimeSensor.zip" },
+
+		{ name: 'X_ITE' },
+		{ name: 'Blend Mode', url: [ "http://media.create3000.de/components/X_ITE/BlendMode/BlendMode.x3d" ], archive: "http://media.create3000.de/components/X_ITE/BlendMode/BlendMode.zip" },
+
+	];
