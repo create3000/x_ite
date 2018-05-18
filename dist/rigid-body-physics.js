a63760e779105e6020629c862001ab81f187503a
Holger Seelig <holger.seelig@yahoo.de>
2018-05-18 06:47:13 +0200

    Published version 4.1.6a-237

--- dist/rigid-body-physics.js
+++ dist/rigid-body-physics.js
@@ -1399,6 +1399,306 @@ function (Fields,
  ******************************************************************************/
 
 
+define ('x_ite/Components/RigidBodyPhysics/X3DNBodyCollisionSpaceNode',[
+	"x_ite/Components/Core/X3DNode",
+	"x_ite/Components/Grouping/X3DBoundedObject",
+	"x_ite/Bits/X3DConstants",
+],
+function (X3DNode, 
+          X3DBoundedObject, 
+          X3DConstants)
+{
+"use strict";
+
+	function X3DNBodyCollisionSpaceNode (executionContext)
+	{
+		X3DNode          .call (this, executionContext);
+		X3DBoundedObject .call (this, executionContext);
+
+		this .addType (X3DConstants .X3DNBodyCollisionSpaceNode);
+	}
+
+	X3DNBodyCollisionSpaceNode .prototype = Object .assign (Object .create (X3DNode .prototype),
+		X3DBoundedObject .prototype,
+	{
+		constructor: X3DNBodyCollisionSpaceNode,
+	});
+
+	return X3DNBodyCollisionSpaceNode;
+});
+
+
+
+/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraße 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2015, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the X_ITE Project.
+ *
+ * X_ITE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * X_ITE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with X_ITE.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+
+define ('x_ite/Components/RigidBodyPhysics/CollisionSpace',[
+	"x_ite/Fields",
+	"x_ite/Basic/X3DFieldDefinition",
+	"x_ite/Basic/FieldDefinitionArray",
+	"x_ite/Components/RigidBodyPhysics/X3DNBodyCollisionSpaceNode",
+	"x_ite/Bits/X3DConstants",
+],
+function (Fields,
+          X3DFieldDefinition,
+          FieldDefinitionArray,
+          X3DNBodyCollisionSpaceNode, 
+          X3DConstants)
+{
+"use strict";
+
+	function CollisionSpace (executionContext)
+	{
+		X3DNBodyCollisionSpaceNode .call (this, executionContext);
+
+		this .addType (X3DConstants .CollisionSpace);
+	}
+
+	CollisionSpace .prototype = Object .assign (Object .create (X3DNBodyCollisionSpaceNode .prototype),
+	{
+		constructor: CollisionSpace,
+		fieldDefinitions: new FieldDefinitionArray ([
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "metadata",    new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "enabled",     new Fields .SFBool (true)),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "useGeometry", new Fields .SFBool ()),
+			new X3DFieldDefinition (X3DConstants .initializeOnly, "bboxSize",    new Fields .SFVec3f (-1, -1, -1)),
+			new X3DFieldDefinition (X3DConstants .initializeOnly, "bboxCenter",  new Fields .SFVec3f ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "collidables", new Fields .MFNode ()),
+		]),
+		getTypeName: function ()
+		{
+			return "CollisionSpace";
+		},
+		getComponentName: function ()
+		{
+			return "RigidBodyPhysics";
+		},
+		getContainerField: function ()
+		{
+			return "children";
+		},
+	});
+
+	return CollisionSpace;
+});
+
+
+
+/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraße 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2015, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the X_ITE Project.
+ *
+ * X_ITE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * X_ITE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with X_ITE.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+
+define ('x_ite/Components/RigidBodyPhysics/Contact',[
+	"x_ite/Fields",
+	"x_ite/Basic/X3DFieldDefinition",
+	"x_ite/Basic/FieldDefinitionArray",
+	"x_ite/Components/Core/X3DNode",
+	"x_ite/Bits/X3DConstants",
+],
+function (Fields,
+          X3DFieldDefinition,
+          FieldDefinitionArray,
+          X3DNode, 
+          X3DConstants)
+{
+"use strict";
+
+	function Contact (executionContext)
+	{
+		X3DNode .call (this, executionContext);
+
+		this .addType (X3DConstants .Contact);
+
+		// Units
+	
+		this .position_                 .setUnit ("length");
+		this .depth_                    .setUnit ("length");
+		this .minBounceSpeed_           .setUnit ("speed");
+		this .surfaceSpeed_             .setUnit ("speed");
+		this .softnessConstantForceMix_ .setUnit ("force");
+	}
+
+	Contact .prototype = Object .assign (Object .create (X3DNode .prototype),
+	{
+		constructor: Contact,
+		fieldDefinitions: new FieldDefinitionArray ([
+			new X3DFieldDefinition (X3DConstants .inputOutput, "metadata",                 new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "position",                 new Fields .SFVec3f ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "contactNormal",            new Fields .SFVec3f (0, 1, 0)),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "depth",                    new Fields .SFFloat ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "frictionDirection",        new Fields .SFVec3f (0, 1, 0)),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "appliedParameters",        new Fields .MFString ("BOUNCE")),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "bounce",                   new Fields .SFFloat ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "minbounceSpeed",           new Fields .SFFloat ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "frictionCoefficients",     new Fields .SFVec2f ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "surfaceSpeed",             new Fields .SFVec2f ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "slipCoefficients",         new Fields .SFVec2f ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "softnessConstantForceMix", new Fields .SFFloat (0.0001)),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "softnessErrorCorrection",  new Fields .SFFloat (0.8)),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "geometry1",                new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "geometry2",                new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "body1",                    new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput, "body2",                    new Fields .SFNode ()),
+		]),
+		getTypeName: function ()
+		{
+			return "Contact";
+		},
+		getComponentName: function ()
+		{
+			return "RigidBodyPhysics";
+		},
+		getContainerField: function ()
+		{
+			return "children";
+		},
+	});
+
+	return Contact;
+});
+
+
+
+/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraße 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2015, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the X_ITE Project.
+ *
+ * X_ITE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * X_ITE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with X_ITE.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+
 define ('x_ite/Components/RigidBodyPhysics/DoubleAxisHingeJoint',[
 	"x_ite/Fields",
 	"x_ite/Basic/X3DFieldDefinition",
@@ -3243,8 +3543,8 @@ require ([
 	"x_ite/Components/RigidBodyPhysics/CollidableShape",
 	"x_ite/Components/RigidBodyPhysics/CollisionCollection",
 	"x_ite/Components/RigidBodyPhysics/CollisionSensor",
-	//"x_ite/Components/RigidBodyPhysics/CollisionSpace",
-	//"x_ite/Components/RigidBodyPhysics/Contact",
+	"x_ite/Components/RigidBodyPhysics/CollisionSpace",
+	"x_ite/Components/RigidBodyPhysics/Contact",
 	"x_ite/Components/RigidBodyPhysics/DoubleAxisHingeJoint",
 	"x_ite/Components/RigidBodyPhysics/MotorJoint",
 	"x_ite/Components/RigidBodyPhysics/RigidBody",
@@ -3259,8 +3559,8 @@ function (SupportedNodes,
           CollidableShape,
           CollisionCollection,
           CollisionSensor,
-          //CollisionSpace,
-          //Contact,
+          CollisionSpace,
+          Contact,
           DoubleAxisHingeJoint,
           MotorJoint,
           RigidBody,
@@ -3278,8 +3578,8 @@ function (SupportedNodes,
 		CollidableShape:      CollidableShape,
 		CollisionCollection:  CollisionCollection,
 		CollisionSensor:      CollisionSensor,
-		//CollisionSpace:       CollisionSpace,
-		//Contact:              Contact,
+		CollisionSpace:       CollisionSpace,
+		Contact:              Contact,
 		DoubleAxisHingeJoint: DoubleAxisHingeJoint,
 		MotorJoint:           MotorJoint,
 		RigidBody:            RigidBody,
