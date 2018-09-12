0ef979796e60ec8181503001a2bab09435be0173
Holger Seelig <holger.seelig@yahoo.de>
2018-09-12 11:08:57 +0200

    Applied patch from coderextreme (#17).

--- src/x_ite/Parser/JSONParser.js
+++ src/x_ite/Parser/JSONParser.js
@@ -157,7 +157,7 @@ function (
 		 */
 	JSONParser .prototype.
 		CDATACreateFunction = function(document, element, str) {
-			var y = str.replace(/\\"/g, "\\\"")
+			var y = str.trim().replace(/\\"/g, "\\\"")
 				.replace(/&lt;/g, "<")
 				.replace(/&gt;/g, ">")
 				.replace(/&amp;/g, "&");
