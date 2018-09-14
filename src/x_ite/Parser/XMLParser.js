c5b25753f441967b6bc9dc5d7731110be9048dc9
Holger Seelig <holger.seelig@yahoo.de>
2018-09-14 18:24:38 +0200

    Added layzy field value parse functions to XMLParser.

--- src/x_ite/Parser/XMLParser.js
+++ src/x_ite/Parser/XMLParser.js
@@ -986,5 +986,31 @@ function ($,
 	XMLParser .prototype .fieldTypes [X3DConstants .MFVec4d]     = Parser .prototype .sfvec4dValues;
 	XMLParser .prototype .fieldTypes [X3DConstants .MFVec4f]     = Parser .prototype .sfvec4fValues;
 
+	// Lazy parse functions.
+
+	XMLParser .prototype .fieldTypes [X3DConstants .MFColor] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFColorRGBA] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFDouble] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFFloat] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFMatrix3d] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFMatrix3f] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFMatrix4d] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFMatrix4f] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFRotation] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFVec2d] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFVec2f] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFVec3d] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFVec3f] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFVec4d] =
+	XMLParser .prototype .fieldTypes [X3DConstants .MFVec4f] = function (field)
+	{
+		field .setValue (this .input .trim () .split (/[\s,]+/) .map (function (value) { return parseFloat (value); }));
+	};
+
+	XMLParser .prototype .fieldTypes [X3DConstants .SFInt32] = function (field)
+	{
+		field .setValue (this .input .trim () .split (/[\s,]+/) .map (function (value) { return parseInt (value); }));
+	};
+
 	return XMLParser;
 });
