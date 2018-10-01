9f8612a100603cecf1dafa3df386864ad1112c87
Holger Seelig <holger.seelig@yahoo.de>
2018-10-01 19:17:39 +0200

    Published version 4.2.5-400

--- dist/x_ite.js
+++ dist/x_ite.js
@@ -1,4 +1,4 @@
-/* X_ITE v4.2.5-399 */
+/* X_ITE v4.2.5-400 */
 
 (function () {
 
@@ -24628,7 +24628,7 @@ function (SFBool,
 
 define ('x_ite/Browser/VERSION',[],function ()
 {
-	return "4.2.5a";
+	return "4.2.5";
 });
 
 /* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
