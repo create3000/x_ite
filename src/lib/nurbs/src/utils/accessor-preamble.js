fed7e37cebc4f4c836d1f7864073e9b445e99d4f
Holger Seelig <holger.seelig@gmail.com>
2019-01-18 03:38:20 +0100

    Updated nurbs.

--- src/lib/nurbs/src/utils/accessor-preamble.js
+++ src/lib/nurbs/src/utils/accessor-preamble.js
@@ -6,23 +6,27 @@ function (inferType)
 {
 'use strict';
 
-	return function (nurbs, variableName, propertyName, data) {
-	  var i;
-	  var code = [];
-	
-	  switch (inferType(data)) {
-	    case inferType.NDARRAY:
-	      code.push('  var ' + variableName + ' = ' + propertyName + '.data;');
-	      code.push('  var ' + variableName + 'Offset = ' + propertyName + '.offset;');
-	
-	      for (i = 0; i < data.dimension; i++) {
-	        code.push('  var ' + variableName + 'Stride' + i + ' = ' + propertyName + '.stride[' + i + '];');
-	      }
-	      break;
-	    case inferType.ARRAY_OF_ARRAYS:
-	      code.push('  var ' + variableName + ' = ' + propertyName + ';');
-	  }
-	
-	  return code.join('\n');
+	return function (nurbs, variableName, propertyName, data)
+	{
+		var code = [ ];
+		
+		switch (inferType (data))
+		{
+			case inferType .NDARRAY:
+			{
+				code .push ('  var ' + variableName + ' = ' + propertyName + '.data;');
+				code .push ('  var ' + variableName + 'Offset = ' + propertyName + '.offset;');
+			
+				for (var i = 0; i < data .dimension; i++) {
+					code .push ('  var ' + variableName + 'Stride' + i + ' = ' + propertyName + '.stride[' + i + '];');
+				}
+
+				break;
+			}
+			case inferType .ARRAY_OF_ARRAYS:
+				code .push ('  var ' + variableName + ' = ' + propertyName + ';');
+		}
+
+		return code .join ('\n');
 	};
 });
--- src/lib/nurbs/src/utils/is-ndarray.js
+++ src/lib/nurbs/src/utils/is-ndarray.js
@@ -6,10 +6,16 @@ define (function ()
 	// Source: https://github.com/scijs/isndarray
 	// By Kyle Robinson Young, MIT Licensed.
 	
-	return function (arr) {
-	  if (!arr) return false;
-	  if (!arr.dtype) return false;
-	  var re = new RegExp('function View[0-9]+d(:?' + arr.dtype + ')+');
-	  return re.test(String(arr.constructor));
+	return function (arr)
+	{
+		if (! arr)
+			return false;
+
+		if (! arr .dtype)
+			return false;
+
+		var re = new RegExp ('function View[0-9]+d(:?' + arr .dtype + ')+');
+
+		return re .test (String (arr .constructor));
 	};
 });
