8e424d0de907d0ed08d299037edbc6b6e2ac32c2
Holger Seelig <holger.seelig@gmail.com>
2019-03-28 10:19:15 +0100

    Published version 4.4.7a-703

--- dist/components/rigid-body-physics.js
+++ dist/components/rigid-body-physics.js
@@ -1409,18 +1409,93 @@ function (Fields,
  ******************************************************************************/
 
 
+define ('x_ite/Browser/RigidBodyPhysics/AppliedParametersType',[],function ()
+{
+"use strict";
+
+	var i = 0;
+
+	var AppliedParametersType =
+	{
+		BOUNCE:                 i ++,
+		USER_FRICTION:          i ++,
+		FRICTION_COEFFICIENT_2: i ++,
+		ERROR_REDUCTION:        i ++,
+		CONSTANT_FORCE:         i ++,
+		SPEED_1:                i ++,
+		SPEED_2:                i ++,
+		SLIP_1:                 i ++,
+		SLIP_2:                 i ++,
+	};
+
+	return AppliedParametersType;
+});
+
+/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraße 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2015, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the X_ITE Project.
+ *
+ * X_ITE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * X_ITE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with X_ITE.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+
 define ('x_ite/Components/RigidBodyPhysics/CollisionCollection',[
 	"x_ite/Fields",
 	"x_ite/Basic/X3DFieldDefinition",
 	"x_ite/Basic/FieldDefinitionArray",
 	"x_ite/Components/Core/X3DChildNode",
 	"x_ite/Bits/X3DConstants",
+	"x_ite/Bits/X3DCast",
+	"x_ite/Browser/RigidBodyPhysics/AppliedParametersType",
 ],
 function (Fields,
           X3DFieldDefinition,
           FieldDefinitionArray,
           X3DChildNode, 
-          X3DConstants)
+          X3DConstants,
+          X3DCast,
+          AppliedParametersType)
 {
 "use strict";
 
@@ -1434,7 +1509,8 @@ function (Fields,
 		this .surfaceSpeed_             .setUnit ("speed");
 		this .softnessConstantForceMix_ .setUnit ("force");
 
-		this .appliedParameters = { };
+		this .appliedParameters = new Set ();
+		this .collidableNodes   = [ ];
 	}
 
 	CollisionCollection .prototype = Object .assign (Object .create (X3DChildNode .prototype),
@@ -1470,20 +1546,72 @@ function (Fields,
 			X3DChildNode .prototype .initialize .call (this);
 
 			this .appliedParameters_ .addInterest ("set_appliedParameters__", this);
+			this .collidables_       .addInterest ("set_collidables__",       this);
 
 			this .set_appliedParameters__ ();
+			this .set_collidables__ ();
 		},
 		getAppliedParameters: function ()
 		{
 			return this .appliedParameters;
 		},
-		set_appliedParameters__: function ()
+		getCollidables: function ()
 		{
-			for (var key in this .appliedParameters)
-				delete this .appliedParameters [key];
+			return this .collidableNodes;
+		},
+		set_appliedParameters__: (function ()
+		{
+			var appliedParametersIndex = new Map ([
+				["BOUNCE",                 AppliedParametersType .BOUNCE],
+				["USER_FRICTION",          AppliedParametersType .USER_FRICTION],
+				["FRICTION_COEFFICIENT-2", AppliedParametersType .FRICTION_COEFFICIENT_2],
+				["ERROR_REDUCTION",        AppliedParametersType .ERROR_REDUCTION],
+				["CONSTANT_FORCE",         AppliedParametersType .CONSTANT_FORCE],
+				["SPEED-1",                AppliedParametersType .SPEED_1],
+				["SPEED-2",                AppliedParametersType .SPEED_2],
+				["SLIP-1",                 AppliedParametersType .SLIP_1],
+				["SLIP-2",                 AppliedParametersType .SLIP_2],
+			]);
 
-			for (var i = 0, length = this .appliedParameters_ .length; i < length; ++ i)
-				this .appliedParameters [this .appliedParameters_ [i]] = true;
+			return function ()
+			{
+				this .appliedParameters .clear ();
+	
+				for (var i = 0, length = this .appliedParameters_ .length; i < length; ++ i)
+				{
+					var appliedParameter = appliedParametersIndex .get (this .appliedParameters_ [i]);
+
+					if (appliedParameter !== undefined)
+						this .appliedParameters .add (appliedParameter);
+				}
+			};
+		})(),
+		set_collidables__: function ()
+		{
+			this .collidableNodes .length = 0;
+
+			for (var i = 0, iLength = this .collidables_ .length; i < iLength; ++ i)
+			{
+				var collidableNode = X3DCast (X3DConstants .X3DNBodyCollidableNode, this .collidables_ [i]);
+
+				if (collidableNode)
+				{
+					this .collidableNodes .push (collidableNode);
+					continue;
+				}
+
+				var collisionSpaceNode = X3DCast (X3DConstants .X3DNBodyCollisionSpaceNode, this .collidables_ [i]);
+
+				if (collisionSpaceNode)
+				{
+					var collidableNodes = collisionSpaceNode .getCollidables ();
+
+					for (var c = 0, cLength = collidableNodes .length; c < cLength; ++ c)
+					{
+						this .collidableNodes .push (collidableNodes [c]);
+					}
+				}
+			}
 		},
 	});
 
@@ -1547,12 +1675,16 @@ define ('x_ite/Components/RigidBodyPhysics/CollisionSensor',[
 	"x_ite/Basic/FieldDefinitionArray",
 	"x_ite/Components/Core/X3DSensorNode",
 	"x_ite/Bits/X3DConstants",
+	"x_ite/Bits/X3DCast",
+	"standard/Math/Numbers/Vector3",
 ],
 function (Fields,
           X3DFieldDefinition,
           FieldDefinitionArray,
           X3DSensorNode, 
-          X3DConstants)
+          X3DConstants,
+          X3DCast,
+          Vector3)
 {
 "use strict";
 
@@ -1561,6 +1693,8 @@ function (Fields,
 		X3DSensorNode .call (this, executionContext);
 
 		this .addType (X3DConstants .CollisionSensor);
+
+		this .colliderNode = null;
 	}
 
 	CollisionSensor .prototype = Object .assign (Object .create (X3DSensorNode .prototype),
@@ -1586,6 +1720,170 @@ function (Fields,
 		{
 			return "children";
 		},
+		initialize: function ()
+		{
+			X3DSensorNode .prototype .initialize .call (this);
+
+			this .isLive () .addInterest ("set_live__", this);
+
+			this .collider_ .addInterest ("set_collider__", this);
+
+			this .set_collider__ ();
+		},
+		set_live__: function ()
+		{
+			if (this .getLive () && this .enabled_ .getValue () && this .colliderNode)
+			{
+				this .getBrowser () .sensorEvents () .addInterest ("update", this);
+			}
+			else
+			{
+				this .getBrowser () .sensorEvents () .removeInterest ("update", this);
+			}
+		},
+		set_collider__: function ()
+		{
+			this .colliderNode = X3DCast (X3DConstants .CollisionCollection, this .collider_);
+
+			this .set_live__ ();
+		},
+		update: (function ()
+		{
+			var
+				collidableNodesIndex = new Map (),
+				collisionWorlds      = new Set (),
+				intersectionNodes    = new Set (),
+				contactNodes         = [ ],
+				position             = new Vector3 (0, 0, 0),
+				contactNormal        = new Vector3 (0, 0, 0);
+	
+			return function ()
+			{
+				var
+					colliderNode    = this .colliderNode,
+					collidableNodes = colliderNode .getCollidables ();
+	
+				collidableNodesIndex .clear ();
+				collisionWorlds      .clear ();
+	
+				for (var i = 0, length = collidableNodes .length; i < length; ++ i)
+				{
+					var
+						collidableNode = collidableNodes [i],
+						bodyNode       = collidableNodes [i] .getBody ();
+	
+					if (bodyNode)
+					{
+						collidableNodesIndex .set (bodyNode .getRigidBody () .ptr, collidableNode);
+	
+						var collection = bodyNode .getCollection ();
+	
+						if (collection)
+							collisionWorlds .add (collection .getDynamicsWorld ());
+					}
+				}
+	
+				intersectionNodes .clear ();
+				contactNodes .length = 0;
+
+				collisionWorlds .forEach (function (collisionWorld)
+				{
+					collisionWorld .performDiscreteCollisionDetection ();
+		
+					var
+						dispatcher   = collisionWorld .getDispatcher (),
+						numManifolds = dispatcher .getNumManifolds ();
+
+					for (var i = 0; i < numManifolds; ++ i)
+					{
+						var
+							contactManifold = dispatcher .getManifoldByIndexInternal (i),
+							numContacts     = contactManifold .getNumContacts ();
+
+						for (var j = 0; j < numContacts; ++ j)
+						{
+							var pt = contactManifold .getContactPoint (j);
+		
+							if (pt .getDistance () <= 0)
+							{
+								var
+									collidableNode1 = collidableNodesIndex .get (contactManifold .getBody0 () .ptr),
+									collidableNode2 = collidableNodesIndex .get (contactManifold .getBody1 () .ptr);
+
+								if (collidableNode1)
+									intersectionNodes .add (collidableNode1);
+
+								if (collidableNode2)
+									intersectionNodes .add (collidableNode2);
+
+								var contactNode = this .getExecutionContext () .createNode ("Contact", false);
+
+								var
+									btPosition      = pt .getPositionWorldOnA (),
+									btContactNormal = pt .get_m_positionWorldOnA ();
+
+								contactNode .position_                 = position .set (btPosition .x (), btPosition .y (), btPosition .z ());
+								contactNode .contactNormal_            = contactNormal .set (btContactNormal .x (), btContactNormal .y (), btContactNormal .z ());
+								contactNode .depth_                    = -pt .getDistance ();
+//								contactNode .frictionDirection_        = context .frictionDirection;
+								contactNode .appliedParameters_        = colliderNode .appliedParameters_;
+								contactNode .bounce_                   = colliderNode .bounce_;
+								contactNode .minBounceSpeed_           = colliderNode .minBounceSpeed_;
+								contactNode .frictionCoefficients_     = colliderNode .frictionCoefficients_;
+								contactNode .surfaceSpeed_             = colliderNode .surfaceSpeed_;
+								contactNode .slipCoefficients_         = colliderNode .slipFactors_;
+								contactNode .softnessConstantForceMix_ = colliderNode .softnessConstantForceMix_;
+								contactNode .softnessErrorCorrection_  = colliderNode .softnessErrorCorrection_;
+
+								if (collidableNode1)
+								{
+									contactNode .geometry1_ = collidableNode1;
+									contactNode .body1_     = collidableNode1 .getBody ();
+								}
+
+								if (collidableNode2)
+								{
+									contactNode .geometry2_ = collidableNode2;
+									contactNode .body2_     = collidableNode2 .getBody ();
+								}
+								
+								contactNode .setup ();
+
+								contactNodes .push (contactNode);
+							}
+						}
+					}
+				},
+				this);
+
+				var active = Boolean (contactNodes .length);
+
+				if (this .isActive_ .getValue () !== active)
+					this .isActive_ = active;
+
+				if (intersectionNodes .size)
+				{
+					this .intersections_ .length = 0;
+
+					intersectionNodes .forEach (function (intersectionNode)
+					{
+						this .intersections_ .push (intersectionNode);
+					},
+					this);
+				}
+
+				if (contactNodes .length)
+				{
+					this .contacts_ .length = 0;
+
+					contactNodes .forEach (function (contactNode)
+					{
+						this .contacts_ .push (contactNode);
+					},
+					this);
+				}
+			};
+		})(),
 	});
 
 	return CollisionSensor;
@@ -1727,12 +2025,14 @@ define ('x_ite/Components/RigidBodyPhysics/CollisionSpace',[
 	"x_ite/Basic/FieldDefinitionArray",
 	"x_ite/Components/RigidBodyPhysics/X3DNBodyCollisionSpaceNode",
 	"x_ite/Bits/X3DConstants",
+	"x_ite/Bits/X3DCast",
 ],
 function (Fields,
           X3DFieldDefinition,
           FieldDefinitionArray,
           X3DNBodyCollisionSpaceNode, 
-          X3DConstants)
+          X3DConstants,
+          X3DCast)
 {
 "use strict";
 
@@ -1741,6 +2041,8 @@ function (Fields,
 		X3DNBodyCollisionSpaceNode .call (this, executionContext);
 
 		this .addType (X3DConstants .CollisionSpace);
+
+		this .collidableNodes = [ ];
 	}
 
 	CollisionSpace .prototype = Object .assign (Object .create (X3DNBodyCollisionSpaceNode .prototype),
@@ -1765,6 +2067,35 @@ function (Fields,
 		getContainerField: function ()
 		{
 			return "children";
+		},
+		getCollidables: function ()
+		{
+			this .collidableNodes .length = 0;
+
+			for (var i = 0, iLength = this .collidables_ .length; i < iLength; ++ i)
+			{
+				var collidableNode = X3DCast (X3DConstants .X3DNBodyCollidableNode, this .collidables_ [i]);
+
+				if (collidableNode)
+				{
+					this .collidableNodes .push (collidableNode);
+					continue;
+				}
+
+				var collisionSpaceNode = X3DCast (X3DConstants .X3DNBodyCollisionSpaceNode, this .collidables_ [i]);
+
+				if (collisionSpaceNode)
+				{
+					var collidableNodes = collisionSpaceNode .getCollidables ();
+
+					for (var c = 0, cLength = collidableNodes .length; c < cLength; ++ c)
+					{
+						this .collidableNodes .push (collidableNodes [c]);
+					}
+				}
+			}
+
+			return this .collidableNodes;
 		},
 	});
 
@@ -1847,7 +2178,7 @@ function (Fields,
 	
 		this .position_                 .setUnit ("length");
 		this .depth_                    .setUnit ("length");
-		this .minBounceSpeed_           .setUnit ("speed");
+		this .minbounceSpeed_           .setUnit ("speed");
 		this .surfaceSpeed_             .setUnit ("speed");
 		this .softnessConstantForceMix_ .setUnit ("force");
 	}
@@ -2895,6 +3226,7 @@ define ('x_ite/Components/RigidBodyPhysics/RigidBodyCollection',[
 	"x_ite/Components/Core/X3DChildNode",
 	"x_ite/Bits/X3DConstants",
 	"x_ite/Bits/X3DCast",
+	"x_ite/Browser/RigidBodyPhysics/AppliedParametersType",
 	"lib/ammojs/Ammo",
 ],
 function (Fields,
@@ -2903,6 +3235,7 @@ function (Fields,
           X3DChildNode, 
           X3DConstants,
           X3DCast,
+          AppliedParametersType,
           Ammo)
 {
 "use strict";
@@ -3042,7 +3375,7 @@ function (Fields,
 		{
 			if (this .colliderNode && this .colliderNode .enabled_ .getValue ())
 			{
-				if (this .colliderNode .getAppliedParameters () .BOUNCE)
+				if (this .colliderNode .getAppliedParameters () .has (AppliedParametersType .BOUNCE))
 				{
 					for (var i = 0, length = this .bodyNodes .length; i < length; ++ i)
 					{
@@ -3065,7 +3398,7 @@ function (Fields,
 		{
 			if (this .colliderNode && this .colliderNode .enabled_ .getValue ())
 			{
-				if (this .colliderNode .getAppliedParameters () .FRICTION_COEFFICIENT_2)
+				if (this .colliderNode .getAppliedParameters () .has (AppliedParametersType .FRICTION_COEFFICIENT_2))
 				{
 					for (var i = 0, length = this .bodyNodes .length; i < length; ++ i)
 					{
--- src/x_ite/Browser/VERSION.js
+++ src/x_ite/Browser/VERSION.js
@@ -49,5 +49,5 @@
 
 define (function ()
 {
-	return "4.4.6";
+	return "4.4.7a";
 });
