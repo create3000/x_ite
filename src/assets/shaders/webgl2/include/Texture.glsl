a6e7fc71fe54330e31e1d508cf7d3682ee08dc39
Holger Seelig <holger.seelig@gmail.com>
2019-04-29 06:01:00 +0200

    Use switch in shaders.

--- src/assets/shaders/webgl2/include/Texture.glsl
+++ src/assets/shaders/webgl2/include/Texture.glsl
@@ -443,13 +443,18 @@ getTextureColor (const in vec4 diffuseColor, const in vec4 specularColor)
 	if (x3d_GeometryType == x3d_Geometry2D && ! gl_FrontFacing)
 		texCoord .s = 1.0 - texCoord .s;
 
-	if (x3d_TextureType [0] == x3d_TextureType2D)
+	switch (x3d_TextureType [0])
 	{
-		textureColor = texture2D (x3d_Texture2D [0], vec2 (texCoord));
-	}
- 	else if (x3d_TextureType [0] == x3d_TextureTypeCubeMapTexture)
-	{
-		textureColor = textureCube (x3d_CubeMapTexture [0], vec3 (texCoord));
+		case x3d_TextureType2D:
+		{
+			textureColor = texture2D (x3d_Texture2D [0], vec2 (texCoord));
+			break;
+		}
+		case x3d_TextureTypeCubeMapTexture:
+		{
+			textureColor = textureCube (x3d_CubeMapTexture [0], vec3 (texCoord));
+			break;
+		}
 	}
 
 	return diffuseColor * textureColor;
