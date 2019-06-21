c1b2bcf99be8bc68b0947eb15182ffc2d78df852
Holger Seelig <holger.seelig@gmail.com>
2019-06-21 16:16:20 +0200

    Added version-number.pl

---
+++ build/bin/version-number.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+# -*- Mode: Perl; coding: utf-8; tab-width: 3; indent-tabs-mode: t; c-basic-offset: 3 -*-
+
+use strict;
+use warnings;
+use v5.10.0;
+use open qw/:std :utf8/;
+
+sub check_version {
+	my $VERSION = `cat package.json`;
+	$VERSION =~ /"version":\s*"(.*?)"/;
+	$VERSION = $1;
+
+	say "VERSION »$VERSION«";
+
+	my $BROWSER = `cat src/x_ite/Browser/VERSION.js`;
+	$BROWSER =~ s/"(.*?a?)"/"$VERSION"/;
+
+	open BROWSER, ">", "src/x_ite/Browser/VERSION.js";
+	print BROWSER $BROWSER;
+	close $BROWSER;
+}
+
+check_version;
