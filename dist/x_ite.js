fed0ce2ea83d43c3238e275395e41e687a9b75c0
Holger Seelig <holger.seelig@gmail.com>
2019-07-17 09:39:23 +0200

    Published version 4.5.10-831

--- dist/x_ite.js
+++ dist/x_ite.js
@@ -1,4 +1,4 @@
-/* X_ITE v4.5.10a-830 */
+/* X_ITE v4.5.10-831 */
 
 (function () {
 
@@ -25135,7 +25135,7 @@ function (SFBool,
 
 define ('x_ite/Browser/VERSION',[],function ()
 {
-	return "4.5.10a";
+	return "4.5.10";
 });
 
 /* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
--- src/x_ite/Browser/VERSION.js
+++ src/x_ite/Browser/VERSION.js
@@ -49,5 +49,5 @@
 
 define (function ()
 {
-	return "4.5.10a";
+	return "4.5.10";
 });
