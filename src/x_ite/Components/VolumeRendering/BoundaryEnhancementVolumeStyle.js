957a6699898b7f01bfff61c3e1b1da18e7ffa6f5
Holger Seelig <holger.seelig@gmail.com>
2019-10-16 17:14:29 +0200

    BoundaryEnhancementVolumeStyle

--- src/x_ite/Components/VolumeRendering/BoundaryEnhancementVolumeStyle.js
+++ src/x_ite/Components/VolumeRendering/BoundaryEnhancementVolumeStyle.js
@@ -114,6 +114,17 @@ function (Fields,
 			string += "uniform float boundaryOpacity_" + this .getId () + ";\n";
 			string += "uniform float opacityFactor_" + this .getId () + ";\n";
 
+			string += "\n";
+			string += "float\n";
+			string += "getBoundaryEnhancementStyle (in float originalAlpha, in float retainedOpacity, in float boundaryOpacity, in float opacityFactor, in vec3 texCoord)\n";
+			string += "{\n";
+			string += "	float f0 = texture (x3d_Texture3D [0], texCoord) .r;\n";
+			string += "	float f1 = texture (x3d_Texture3D [0], texCoord + vec3 (0.0, 0.0, 1.0 / x3d_TextureSize .z)) .r;\n";
+			string += "	float f  = abs (f0 - f1);\n";
+			string += "\n";
+			string += "	return originalAlpha * (retainedOpacity + pow (boundaryOpacity * f, opacityFactor));\n";
+			string += "}\n";
+
 			return string;
 		},
 		getFunctionsText: function ()
@@ -126,20 +137,7 @@ function (Fields,
 			string += "\n";
 			string += "	// BoundaryEnhancementVolumeStyle\n";
 			string += "\n";
-			string += "	{\n";
-
-			string += "		float f0 = texture (x3d_Texture3D [0], texCoord) .r;\n";
-			string += "		float f1 = texture (x3d_Texture3D [0], texCoord + vec3 (0.0, 0.0, 1.0 / x3d_TextureSize .z)) .r;\n";
-			string += "		float f  = abs (f0 - f1);\n";
-			string += "\n";
-			string += "		float retainedOpacity = retainedOpacity_" + this .getId () + ";\n";
-			string += "		float boundaryOpacity = boundaryOpacity_" + this .getId () + ";\n";
-			string += "		float opacityFactor   = opacityFactor_" + this .getId () + ";\n";
-			string += "		float boundaryAlpha   = textureColor .a * (retainedOpacity + pow (boundaryOpacity * f, opacityFactor));\n";
-			string += "\n";
-			string += "		textureColor .a = boundaryAlpha;\n"
-
-			string += "	}\n";
+			string += "	textureColor .a = getBoundaryEnhancementStyle (textureColor .a, retainedOpacity_" + this .getId () + ", boundaryOpacity_" + this .getId () + ", opacityFactor_" + this .getId () + ", texCoord);\n";
 
 			return string;
 		},
