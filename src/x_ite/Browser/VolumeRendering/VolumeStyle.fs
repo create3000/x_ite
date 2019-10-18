7ce2ff9e326ef0d39c21c0f015f257c315e9d2e3
Holger Seelig <holger.seelig@gmail.com>
2019-10-18 15:33:27 +0200

    Published version 4.5.17a-844

--- src/x_ite/Browser/VolumeRendering/VolumeStyle.fs
+++ src/x_ite/Browser/VolumeRendering/VolumeStyle.fs
@@ -23,8 +23,69 @@ in vec4  texCoord;
 
 out vec4 x3d_FragColor;
 
-#pragma X3D include "include/ClipPlanes.glsl"
-#pragma X3D include "include/Fog.glsl"
+
+uniform int  x3d_NumClipPlanes;
+uniform vec4 x3d_ClipPlane [x3d_MaxClipPlanes];
+
+void
+clip ()
+{
+	for (int i = 0; i < x3d_MaxClipPlanes; ++ i)
+	{
+		if (i == x3d_NumClipPlanes)
+			break;
+
+		if (dot (vertex, x3d_ClipPlane [i] .xyz) - x3d_ClipPlane [i] .w < 0.0)
+			discard;
+	}
+}
+
+
+uniform x3d_FogParameters x3d_Fog;
+
+float
+getFogInterpolant ()
+{
+	// Returns 0.0 for fog color and 1.0 for material color.
+
+	if (x3d_Fog .type == x3d_None)
+		return 1.0;
+
+	if (x3d_Fog .fogCoord)
+		return clamp (1.0 - fogDepth, 0.0, 1.0);
+
+	float visibilityRange = x3d_Fog .visibilityRange;
+
+	if (visibilityRange <= 0.0)
+		return 1.0;
+
+	float dV = length (x3d_Fog .matrix * vertex);
+
+	if (dV >= visibilityRange)
+		return 0.0;
+
+	switch (x3d_Fog .type)
+	{
+		case x3d_LinearFog:
+		{
+			return (visibilityRange - dV) / visibilityRange;
+		}
+		case x3d_ExponentialFog:
+		{
+			return exp (-dV / (visibilityRange - dV));
+		}
+		default:
+		{
+			return 1.0;
+		}
+	}
+}
+
+vec3
+getFogColor (const in vec3 color)
+{
+	return mix (x3d_Fog .color, color, getFogInterpolant ());
+}
 
 vec4
 getTextureColor (in vec3 texCoord)
