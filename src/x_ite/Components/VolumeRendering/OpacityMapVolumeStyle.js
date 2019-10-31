f9b69bf62c2de86d5aea4f2095ccd4a27efe598d
Holger Seelig <holger.seelig@gmail.com>
2019-10-31 16:13:14 +0100

    Fixed OpacityMapVolumeStyle.

--- src/x_ite/Components/VolumeRendering/OpacityMapVolumeStyle.js
+++ src/x_ite/Components/VolumeRendering/OpacityMapVolumeStyle.js
@@ -140,7 +140,7 @@ function (Fields,
 				string += "vec4\n";
 				string += "getOpacityMapStyle_" + this .getId () + " (in vec4 originalColor)\n";
 				string += "{\n";
-				string += "	return texture (transferFunction_" + this .getId () + ", originalColor .rg / originalColor .a);\n";
+				string += "	return texture (transferFunction_" + this .getId () + ", originalColor .rg);\n";
 				string += "}\n";
 			}
 			else
@@ -151,7 +151,7 @@ function (Fields,
 				string += "vec4\n";
 				string += "getOpacityMapStyle_" + this .getId () + " (in vec4 originalColor)\n";
 				string += "{\n";
-				string += "	return texture (transferFunction_" + this .getId () + ", originalColor .rgb / originalColor .a);\n";
+				string += "	return texture (transferFunction_" + this .getId () + ", originalColor .rgb);\n";
 				string += "}\n";
 			}
 
