3757b81b9db64070618e3f86de0b0103cf772477
Holger Seelig <holger.seelig@gmail.com>
2019-10-31 10:09:38 +0100

    DICOM handle bitsAllocated === 1.

--- src/x_ite/Browser/Texturing3D/DICOMParser.js
+++ src/x_ite/Browser/Texturing3D/DICOMParser.js
@@ -267,8 +267,8 @@ function (dicomParser,
 					default:
 						throw new Error ("DICOM: unsupported image type '" + this .photometricInterpretation + "'.");
 				}
-			}
-			.bind (this));
+			},
+			this);
 
 			// Set Uint8Array.
 
@@ -305,13 +305,28 @@ function (dicomParser,
 					pixelElement .fragments .forEach (function (fragment)
 					{
 						frames .push (new Uint8Array (this .dataSet .byteArray .buffer, fragment .position, fragment .length));
-					}
-					.bind (this));
+					},
+					this);
 				}
 				else
 				{
 					frames .push (new Uint8Array (this .dataSet .byteArray .buffer, pixelElement .dataOffset, pixelElement .length));
 				}
+			}
+
+			if (this .bitsAllocated === 1)
+			{
+				var pixelsPerFrame = this .dicom .width * this .dicom .height * this .dicom .components;
+
+				frames = frames .map (function (frame, i)
+				{
+					var frameOffset = pixelElement .dataOffset + i * pixelsPerFrame / 8;
+
+					return this .unpackBinaryFrame (this .dataSet .byteArray, frameOffset, pixelsPerFrame);
+				},
+				this);
+
+				this .bitsAllocated = 8;
 			}
 
 			return frames;
@@ -341,6 +356,33 @@ function (dicomParser,
 			}
 
 			return { offset: min, factor: 1 / (max - min) * 255 };
+		},
+		unpackBinaryFrame: function (byteArray, frameOffset, pixelsPerFrame)
+		{
+			function isBitSet (byte, bitPos)
+			{
+				return byte & (1 << bitPos);
+			}
+
+			// Create a new pixel array given the image size
+			var pixelData = new Uint8Array (pixelsPerFrame);
+
+			for (var i = 0; i < pixelsPerFrame; ++ i)
+			{
+				// Compute byte position
+				var bytePos = Math .floor (i / 8);
+
+				// Get the current byte
+				var byte = byteArray [bytePos + frameOffset];
+
+				// Bit position (0-7) within byte
+				var bitPos = (i % 8);
+
+				// Check whether bit at bitpos is set
+				pixelData [i] = isBitSet (byte, bitPos) ? 1 : 0;
+			}
+
+			return pixelData;
 		},
 		decodeLittleEndian: function (pixelData)
 		{
