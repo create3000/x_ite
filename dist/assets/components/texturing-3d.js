1b1f7e197c886871936bcd9a9b6f2f0d3736726f
Holger Seelig <holger.seelig@gmail.com>
2019-11-01 02:28:15 +0100

    Published version 4.6.4a-910

--- dist/assets/components/texturing-3d.js
+++ dist/assets/components/texturing-3d.js
@@ -22457,6 +22457,17 @@ function (dicomParser,
 						frames .push (dicomParser .readEncapsulatedPixelDataFromFragments (this .dataSet, pixelElement, i));
 				}
 			}
+			else if (this .bitsAllocated === 32)
+			{
+				var pixelsPerFrame = this .dicom .width * this .dicom .height * this .dicom .components;
+
+				for (var i = 0, length = this .dicom .depth; i < length; ++ i)
+				{
+					var frameOffset = pixelElement .dataOffset + i * pixelsPerFrame * 4;
+
+					frames .push (new Uint8Array (this .dataSet .byteArray .buffer, frameOffset, pixelsPerFrame * 4));
+				}
+			}
 			else
 			{
 				if (pixelElement .fragments)
@@ -22532,8 +22543,8 @@ function (dicomParser,
 		{
 			var
 				buffer = pixelData .buffer,
-				offset = pixelData.byteOffset,
-				length = pixelData.length;
+				offset = pixelData .byteOffset,
+				length = pixelData .length;
 
 			if (this .bitsAllocated === 16)
 			{
@@ -22552,11 +22563,11 @@ function (dicomParser,
 			else if (this .bitsAllocated === 32)
 			{
 				// if pixel data is not aligned on even boundary, shift it
-				if (offset % 2)
+				if (offset % 4)
 				{
 					buffer = buffer .slice (offset);
 					offset = 0;
-			  	}
+				}
 
 				return new Uint8Array (buffer, offset, length);
 			}
