cab41254be2d4069742d42807eba2b8168fa7ef5
Holger Seelig <holger.seelig@gmail.com>
2019-12-05 13:32:45 +0100

    Published version 4.6.9-967

--- dist/x_ite.js
+++ dist/x_ite.js
@@ -1,4 +1,4 @@
-/* X_ITE v4.6.9a-966 */
+/* X_ITE v4.6.9-967 */
 
 (function () {
 
@@ -25263,7 +25263,7 @@ function (SFBool,
 
 define ('x_ite/Browser/VERSION',[],function ()
 {
-	return "4.6.9a";
+	return "4.6.9";
 });
 
 /* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
