c35dade534ef505d44c0bbe0de4ffa4b4ab36e9c
Holger Seelig <holger.seelig@gmail.com>
2020-08-22 03:18:20 +0200

    Published version 4.6.11-993

--- build/bin/version.pl
+++ build/bin/version.pl
@@ -63,7 +63,7 @@ sub upload
 
 	chdir $code;
 
-	system "echo -e 'a\n*\nq\n' | git add -i";
+	system "git add -A";
 	system "git", "commit", "-am", "Published version $VERSION-$REVISION";
 	system "git", "push";
 	system "git", "push", "origin";
