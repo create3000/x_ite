e4b609437b191bf46c1e3ee553533739a8f00255
Holger Seelig <holger.seelig@gmail.com>
2021-12-18 23:29:57 +0100

    Published version 4.7.1-1084

--- dist/x_ite.js
+++ dist/x_ite.js
@@ -1,4 +1,4 @@
-/* X_ITE v4.7.1a-1083 */
+/* X_ITE v4.7.1-1084 */
 
 (function () {
 
@@ -25542,7 +25542,7 @@ function (SFBool,
 
 define ('x_ite/Browser/VERSION',[],function ()
 {
-	return "4.7.1a";
+	return "4.7.1";
 });
 
 /* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
--- src/x_ite/Browser/VERSION.js
+++ src/x_ite/Browser/VERSION.js
@@ -49,5 +49,5 @@
 
 define (function ()
 {
-	return "4.7.1a";
+	return "4.7.1";
 });
