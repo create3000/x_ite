776f4af4ca7a4340e02f691871d389659e45076d
Holger Seelig <holger.seelig@gmail.com>
2022-01-24 21:49:34 +0100

    Published version 4.7.6-1128

--- dist/x_ite.js
+++ dist/x_ite.js
@@ -66013,8 +66013,8 @@ function (TextureBuffer,
 				// Matrices
 
 				gl .uniformMatrix4fv (shaderNode .x3d_ProjectionMatrix, false, new Float32Array (Camera .ortho (-1, 1, -1, 1, -1, 1, new Matrix4 ())));
-				gl .uniformMatrix4fv (shaderNode .x3d_ModelViewMatrix,  false, new Float32Array (new Matrix4 ()));
-				gl .uniformMatrix3fv (shaderNode .x3d_NormalMatrix,     false, new Float32Array (new Matrix3 ()));
+				gl .uniformMatrix4fv (shaderNode .x3d_ModelViewMatrix,  false, new Float32Array (Matrix4 .Identity));
+				gl .uniformMatrix3fv (shaderNode .x3d_NormalMatrix,     false, new Float32Array (Matrix3 .Identity));
 
 				// Set clip planes and lights to none.
 
@@ -66057,9 +66057,7 @@ function (TextureBuffer,
 
 				frameBuffer .unbind ();
 
-				console .log (data)
-
-				return data [0] == 255 && data [1] == 0 && data [2] == 0 && data [3] == 255;
+				return true || (data [0] == 255 && data [1] == 0 && data [2] == 0 && data [3] == 255);
 			};
 		})(),
 	};
@@ -66325,9 +66323,6 @@ function (Shading,
 		set_unlit_shader_valid__: function (valid)
 		{
 			this .unlitShader .isValid_ .removeInterest ("set_unlit_shader_valid__", this);
-
-			console .log (valid .getValue (), ShaderTest .verify (this, this .unlitShader));
-			return;
 
 			if (valid .getValue () && ShaderTest .verify (this, this .unlitShader))
 				return;
