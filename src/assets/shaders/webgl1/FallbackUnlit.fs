dfd8289329e9d2a4bb4f092f28fcb2dca526751c
Holger Seelig <holger.seelig@gmail.com>
2022-01-24 22:35:55 +0100

    Published version 4.7.6-1129

--- src/assets/shaders/webgl1/FallbackUnlit.fs
+++ src/assets/shaders/webgl1/FallbackUnlit.fs
@@ -14,7 +14,7 @@ main ()
 {
 	vec4 finalColor = x3d_NumTextures > 0 ? texture2D (x3d_Texture2D [0], texCoord .st) : vec4 (1.0);
 
-	finalColor .rgb += x3d_Material .emissiveColor;
+	finalColor .rgb *= x3d_Material .emissiveColor;
 	finalColor .a   *= color .a * (1.0 - x3d_Material .transparency);
 
 	gl_FragColor = finalColor;
