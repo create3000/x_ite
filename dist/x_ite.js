7244f7c2f049e74cbe0e34fcc5e15911f1f0d566
Holger Seelig <holger.seelig@gmail.com>
2022-01-25 22:52:54 +0100

    Uncommeted other shaders.

--- dist/x_ite.js
+++ dist/x_ite.js
@@ -112404,6 +112404,334 @@ function (Fields,
 	return Matrix4VertexAttribute;
 });
 
+/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraße 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2015, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the X_ITE Project.
+ *
+ * X_ITE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * X_ITE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with X_ITE.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+
+define ('x_ite/Components/Shaders/PackagedShader',[
+	"x_ite/Fields",
+	"x_ite/Basic/X3DFieldDefinition",
+	"x_ite/Basic/FieldDefinitionArray",
+	"x_ite/Components/Shaders/X3DShaderNode",
+	"x_ite/Components/Networking/X3DUrlObject",
+	"x_ite/Components/Shaders/X3DProgrammableShaderObject",
+	"x_ite/Bits/X3DConstants",
+],
+function (Fields,
+          X3DFieldDefinition,
+          FieldDefinitionArray,
+          X3DShaderNode,
+          X3DUrlObject,
+          X3DProgrammableShaderObject,
+          X3DConstants)
+{
+"use strict";
+
+	function PackagedShader (executionContext)
+	{
+		X3DShaderNode               .call (this, executionContext);
+		X3DUrlObject                .call (this, executionContext);
+		X3DProgrammableShaderObject .call (this, executionContext);
+
+		this .addType (X3DConstants .PackagedShader);
+	}
+
+	PackagedShader .prototype = Object .assign (Object .create (X3DShaderNode .prototype),
+		X3DUrlObject .prototype,
+		X3DProgrammableShaderObject .prototype,
+	{
+		constructor: PackagedShader,
+		fieldDefinitions: new FieldDefinitionArray ([
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "metadata",             new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .inputOnly,      "activate",             new Fields .SFBool ()),
+			new X3DFieldDefinition (X3DConstants .outputOnly,     "isSelected",           new Fields .SFBool ()),
+			new X3DFieldDefinition (X3DConstants .outputOnly,     "isValid",              new Fields .SFBool ()),
+			new X3DFieldDefinition (X3DConstants .initializeOnly, "language",             new Fields .SFString ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "url",                  new Fields .MFString ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "autoRefresh",          new Fields .SFTime ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "autoRefreshTimeLimit", new Fields .SFTime (3600)),
+		]),
+		getTypeName: function ()
+		{
+			return "PackagedShader";
+		},
+		getComponentName: function ()
+		{
+			return "Shaders";
+		},
+		getContainerField: function ()
+		{
+			return "shaders";
+		},
+		getSourceText: function ()
+		{
+			return this .url_;
+		},
+		requestImmediateLoad: function (cache = true)
+		{ },
+	});
+
+	return PackagedShader;
+});
+
+/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraße 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2015, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the X_ITE Project.
+ *
+ * X_ITE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * X_ITE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with X_ITE.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+
+define ('x_ite/Components/Shaders/ProgramShader',[
+	"x_ite/Fields",
+	"x_ite/Basic/X3DFieldDefinition",
+	"x_ite/Basic/FieldDefinitionArray",
+	"x_ite/Components/Shaders/X3DShaderNode",
+	"x_ite/Bits/X3DConstants",
+],
+function (Fields,
+          X3DFieldDefinition,
+          FieldDefinitionArray,
+          X3DShaderNode, 
+          X3DConstants)
+{
+"use strict";
+
+	function ProgramShader (executionContext)
+	{
+		X3DShaderNode .call (this, executionContext);
+
+		this .addType (X3DConstants .ProgramShader);
+	}
+
+	ProgramShader .prototype = Object .assign (Object .create (X3DShaderNode .prototype),
+	{
+		constructor: ProgramShader,
+		fieldDefinitions: new FieldDefinitionArray ([
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "metadata",   new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .inputOnly,      "activate",   new Fields .SFBool ()),
+			new X3DFieldDefinition (X3DConstants .outputOnly,     "isSelected", new Fields .SFBool ()),
+			new X3DFieldDefinition (X3DConstants .outputOnly,     "isValid",    new Fields .SFBool ()),
+			new X3DFieldDefinition (X3DConstants .initializeOnly, "language",   new Fields .SFString ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "programs",   new Fields .MFNode ()),
+		]),
+		getTypeName: function ()
+		{
+			return "ProgramShader";
+		},
+		getComponentName: function ()
+		{
+			return "Shaders";
+		},
+		getContainerField: function ()
+		{
+			return "shaders";
+		},
+	});
+
+	return ProgramShader;
+});
+
+
+
+/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
+ *******************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright create3000, Scheffelstraße 31a, Leipzig, Germany 2011.
+ *
+ * All rights reserved. Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * The copyright notice above does not evidence any actual of intended
+ * publication of such source code, and is an unpublished work by create3000.
+ * This material contains CONFIDENTIAL INFORMATION that is the property of
+ * create3000.
+ *
+ * No permission is granted to copy, distribute, or create derivative works from
+ * the contents of this software, in whole or in part, without the prior written
+ * permission of create3000.
+ *
+ * NON-MILITARY USE ONLY
+ *
+ * All create3000 software are effectively free software with a non-military use
+ * restriction. It is free. Well commented source is provided. You may reuse the
+ * source in any way you please with the exception anything that uses it must be
+ * marked to indicate is contains 'non-military use only' components.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2015, 2016 Holger Seelig <holger.seelig@yahoo.de>.
+ *
+ * This file is part of the X_ITE Project.
+ *
+ * X_ITE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 only, as published by the
+ * Free Software Foundation.
+ *
+ * X_ITE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License version 3 for more
+ * details (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with X_ITE.  If not, see <http://www.gnu.org/licenses/gpl.html> for a
+ * copy of the GPLv3 License.
+ *
+ * For Silvio, Joy and Adi.
+ *
+ ******************************************************************************/
+
+
+define ('x_ite/Components/Shaders/ShaderProgram',[
+	"x_ite/Fields",
+	"x_ite/Basic/X3DFieldDefinition",
+	"x_ite/Basic/FieldDefinitionArray",
+	"x_ite/Components/Core/X3DNode",
+	"x_ite/Components/Networking/X3DUrlObject",
+	"x_ite/Components/Shaders/X3DProgrammableShaderObject",
+	"x_ite/Bits/X3DConstants",
+],
+function (Fields,
+          X3DFieldDefinition,
+          FieldDefinitionArray,
+          X3DNode,
+          X3DUrlObject,
+          X3DProgrammableShaderObject,
+          X3DConstants)
+{
+"use strict";
+
+	function ShaderProgram (executionContext)
+	{
+		X3DNode                     .call (this, executionContext);
+		X3DUrlObject                .call (this, executionContext);
+		X3DProgrammableShaderObject .call (this, executionContext);
+
+		this .addType (X3DConstants .ShaderProgram);
+	}
+
+	ShaderProgram .prototype = Object .assign (Object .create (X3DNode .prototype),
+		X3DUrlObject .prototype,
+		X3DProgrammableShaderObject .prototype,
+	{
+		constructor: ShaderProgram,
+		fieldDefinitions: new FieldDefinitionArray ([
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "metadata",             new Fields .SFNode ()),
+			new X3DFieldDefinition (X3DConstants .initializeOnly, "type",                 new Fields .SFString ("VERTEX")),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "url",                  new Fields .MFString ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "autoRefresh",          new Fields .SFTime ()),
+			new X3DFieldDefinition (X3DConstants .inputOutput,    "autoRefreshTimeLimit", new Fields .SFTime (3600)),
+		]),
+		getTypeName: function ()
+		{
+			return "ShaderProgram";
+		},
+		getComponentName: function ()
+		{
+			return "Shaders";
+		},
+		getContainerField: function ()
+		{
+			return "programs";
+		},
+		getSourceText: function ()
+		{
+			return this .url_;
+		},
+		requestImmediateLoad: function (cache = true)
+		{ },
+	});
+
+	return ShaderProgram;
+});
+
 /*******************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -112458,10 +112786,10 @@ define ('x_ite/Components/Shaders',[
 	"x_ite/Components/Shaders/FloatVertexAttribute",
 	"x_ite/Components/Shaders/Matrix3VertexAttribute",
 	"x_ite/Components/Shaders/Matrix4VertexAttribute",
-//	"x_ite/Components/Shaders/PackagedShader",
-//	"x_ite/Components/Shaders/ProgramShader",
+	"x_ite/Components/Shaders/PackagedShader",
+	"x_ite/Components/Shaders/ProgramShader",
 	"x_ite/Components/Shaders/ShaderPart",
-//	"x_ite/Components/Shaders/ShaderProgram",
+	"x_ite/Components/Shaders/ShaderProgram",
 	"x_ite/Components/Shaders/X3DProgrammableShaderObject",
 	"x_ite/Components/Shaders/X3DShaderNode",
 	"x_ite/Components/Shaders/X3DVertexAttributeNode",
@@ -112471,10 +112799,10 @@ function (SupportedNodes,
           FloatVertexAttribute,
           Matrix3VertexAttribute,
           Matrix4VertexAttribute,
-//          PackagedShader,
-//          ProgramShader,
+          PackagedShader,
+          ProgramShader,
           ShaderPart,
-//          ShaderProgram,
+          ShaderProgram,
           X3DProgrammableShaderObject,
           X3DShaderNode,
           X3DVertexAttributeNode)
@@ -112487,10 +112815,10 @@ function (SupportedNodes,
 		FloatVertexAttribute:   FloatVertexAttribute,
 		Matrix3VertexAttribute: Matrix3VertexAttribute,
 		Matrix4VertexAttribute: Matrix4VertexAttribute,
-//		PackagedShader:         PackagedShader,
-//		ProgramShader:          ProgramShader,
+		PackagedShader:         PackagedShader,
+		ProgramShader:          ProgramShader,
 		ShaderPart:             ShaderPart,
-//		ShaderProgram:          ShaderProgram,
+		ShaderProgram:          ShaderProgram,
 	};
 
 	const AbstractTypes =
