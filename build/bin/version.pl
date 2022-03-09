e35104698e31c89db99a7e2ef67b0538f8d83a37
Holger Seelig <holger.seelig@gmail.com>
2022-03-09 21:37:18 +0100

    Updated script.

--- build/bin/version.pl
+++ build/bin/version.pl
@@ -67,7 +67,7 @@ sub upload
 	system "git", "add", "-A";
 	system "git", "commit", "-am", "Published version $VERSION-$REVISION";
 	system "git", "push";
-	system "git", "push", "origin";
+	system "git", "push", "-u", "origin";
 }
 
 sub docs
