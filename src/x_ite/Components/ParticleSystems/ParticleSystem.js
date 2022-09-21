eb211312525c8c2ea43da235502a6edbf05846d8
Holger Seelig <holger.seelig@gmail.com>
2022-09-22 01:56:02 +0200

    Resize geometry buffer to appropriate size.

--- src/x_ite/Components/ParticleSystems/ParticleSystem.js
+++ src/x_ite/Components/ParticleSystems/ParticleSystem.js
@@ -898,7 +898,7 @@ function (Fields,
             {
                const
                   gl           = this .getBrowser () .getContext (),
-                  geometryData = new Float32Array (this .maxParticles * this .vertexCount * 4 * 6);
+                  geometryData = new Float32Array (this .maxParticles * this .vertexCount * this .stride / Float32Array .BYTES_PER_ELEMENT);
 
                gl .bindBuffer (gl .ARRAY_BUFFER, this .geometryBuffer);
                gl .bufferData (gl .ARRAY_BUFFER, geometryData, gl .STATIC_DRAW);
