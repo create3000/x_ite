3d8472030f6992641345ab3fae8473fc0482d238
Holger Seelig <holger.seelig@gmail.com>
2022-10-06 21:37:39 +0200

    Workaround Windows Firefox issue.

--- src/x_ite/InputOutput/FileLoader.js
+++ src/x_ite/InputOutput/FileLoader.js
@@ -151,6 +151,10 @@ function ($,
       {
          scene ._initLoadCount .addInterest ("set_initLoadCount__", this, scene, success, error);
          scene ._initLoadCount .addEvent ();
+
+         // Firefox 105.0.2 sometimes does not call requestAnimationFrame callback,
+         // but if we trigger it here again, it works.
+         this .browser .requestAnimationFrame ();
       },
       set_initLoadCount__: function (scene, success, error, field)
       {
