e1c44b81764b6a20a62bcebcd325d54f72674ed6
Holger Seelig <holger.seelig@gmail.com>
2022-10-07 00:36:24 +0200

    Removed BOX_NORMALS.

--- src/x_ite/Browser/ParticleSystems/Box3.glsl
+++ src/x_ite/Browser/ParticleSystems/Box3.glsl
@@ -1,41 +1,43 @@
-/* const */ vec3 BOX3_NORMALS [5] = vec3 [ ] (
-   vec3 (0.0,  0.0,  1.0), // front
-   vec3 (0.0,  0.0, -1.0), // back
-   vec3 (0.0,  1.0,  0.0), // top
-   vec3 (0.0, -1.0,  0.0), // bottom
-   vec3 (1.0,  0.0,  0.0)  // right
-);
-
 bool
 intersects (const in vec3 min, const in vec3 max, const in Line3 line)
 {
-   vec3 intersection = vec3 (0.0);
+   vec3 intersection;
 
-   if (intersects (plane3 (max, BOX3_NORMALS [0]), line, intersection))
+   // front
+
+   if (intersects (plane3 (max, vec3 (0.0, 0.0, 1.0)), line, intersection))
    {
       if (all (greaterThanEqual (vec4 (intersection .xy, max .xy), vec4 (min .xy, intersection .xy))))
          return true;
    }
 
-   if (intersects (plane3 (min, BOX3_NORMALS [1]), line, intersection))
+   // back
+
+   if (intersects (plane3 (min, vec3 (0.0, 0.0, -1.0)), line, intersection))
    {
       if (all (greaterThanEqual (vec4 (intersection .xy, max .xy), vec4 (min .xy, intersection .xy))))
          return true;
    }
 
-   if (intersects (plane3 (max, BOX3_NORMALS [2]), line, intersection))
+   // top
+
+   if (intersects (plane3 (max, vec3 (0.0, 1.0, 0.0)), line, intersection))
    {
       if (all (greaterThanEqual (vec4 (intersection .xz, max .xz), vec4 (min .xz, intersection .xz))))
          return true;
    }
 
-   if (intersects (plane3 (min, BOX3_NORMALS [3]), line, intersection))
+   // bottom
+
+   if (intersects (plane3 (min, vec3 (0.0, -1.0, 0.0)), line, intersection))
    {
       if (all (greaterThanEqual (vec4 (intersection .xz, max .xz), vec4 (min .xz, intersection .xz))))
          return true;
    }
 
-   if (intersects (plane3 (max, BOX3_NORMALS [4]), line, intersection))
+   // right
+
+   if (intersects (plane3 (max, vec3 (1.0, 0.0, 0.0)), line, intersection))
    {
       if (all (greaterThanEqual (vec4 (intersection .yz, max .yz), vec4 (min .yz, intersection .yz))))
          return true;
