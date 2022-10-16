d32c8614bf2baafb18c608bc799e0c35fb525c42
Holger Seelig <holger.seelig@gmail.com>
2022-10-16 22:43:47 +0200

    Code clean up.

--- src/x_ite/Components/Rendering/X3DLineGeometryNode.js
+++ src/x_ite/Components/Rendering/X3DLineGeometryNode.js
@@ -190,13 +190,14 @@ function (X3DGeometryNode,
                      viewport         = context .renderer .getViewVolume () .getViewport (),
                      projectionMatrix = context .renderer .getProjectionMatrix () .get (),
                      texCoordArray    = this .getTexCoords () .getValue (),
-                     vertices         = this .getVertices ();
+                     vertices         = this .getVertices (),
+                     numVertices      = vertices .length;
 
                   modelViewProjectionMatrix .assign (context .modelViewMatrix) .multRight (projectionMatrix);
 
                   let lengthSoFar = 0;
 
-                  for (let i = 0, length = vertices .length; i < length; i += 8)
+                  for (let i = 0; i < numVertices; i += 8)
                   {
                      point0 .set (vertices [i],     vertices [i + 1], vertices [i + 2], vertices [i + 3]);
                      point1 .set (vertices [i + 4], vertices [i + 5], vertices [i + 6], vertices [i + 7]);
