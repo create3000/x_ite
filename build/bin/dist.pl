7148bb13481b9a476bb503b0e3fa60bf12752de8
Holger Seelig <holger.seelig@gmail.com>
2022-10-27 17:04:53 +0200

    Updated shader compiler.

--- build/bin/dist.pl
+++ build/bin/dist.pl
@@ -41,7 +41,7 @@ sub shader_source {
 
 	foreach my $line (@lines)
 	{
-		if ($line =~ /^#pragma\s+X3D\s+include\s+"(.*?[^\/]+\.glsl)"\s*$/)
+		if ($line =~ /^\s*#pragma\s+X3D\s+include\s+"(.*?[^\/]+\.glsl)"\s*$/)
 		{
 			$source .= shader_source ("$dirname/$1");
 		}
