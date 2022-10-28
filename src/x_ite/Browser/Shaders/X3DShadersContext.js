b695777d2dbf00db6a5e7ded7416a64002732258
Holger Seelig <holger.seelig@gmail.com>
2022-10-28 12:40:46 +0200

    Use valid shader.

--- src/x_ite/Browser/Shaders/X3DShadersContext.js
+++ src/x_ite/Browser/Shaders/X3DShadersContext.js
@@ -114,9 +114,30 @@ function (Shading,
       },
       getDefaultShader: function ()
       {
-         const
-            vs = "data:x-shader/x-vertex,  void main () { gl_Position  = vec4 (0.0); }",
-            fs = "data:x-shader/x-fragment,void main () { gl_FragColor = vec4 (0.0); }";
+         const vs = /* glsl */ `data:x-shader/x-vertex,
+         precision highp float;
+
+         uniform mat4 x3d_ProjectionMatrix;
+         uniform mat4 x3d_ModelViewMatrix;
+
+         attribute vec4 x3d_Vertex;
+
+         void
+         main ()
+         {
+            gl_Position = x3d_ProjectionMatrix * (x3d_ModelViewMatrix * x3d_Vertex);
+         }
+         `;
+
+         const fs = /* glsl */ `data:x-shader/x-fragment,
+         precision highp float;
+
+         void
+         main ()
+         {
+            gl_FragColor = vec4 (0.0);
+         }
+         `;
 
          this [_defaultShader] = this .createShader ("DefaultShader", vs, fs);
 
