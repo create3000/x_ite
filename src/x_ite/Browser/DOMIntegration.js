ebcebbd88c5b66d0382e5c4426e023a796704b44
Holger Seelig <holger.seelig@gmail.com>
2022-11-20 01:22:27 +0100

    Updated license.

--- src/x_ite/Browser/DOMIntegration.js
+++ src/x_ite/Browser/DOMIntegration.js
@@ -1,5 +1,4 @@
-/* -*- Mode: JavaScript; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
- *******************************************************************************
+/*******************************************************************************
  * MIT License
  *
  * Copyright (c) 2016 Andreas Plesch
