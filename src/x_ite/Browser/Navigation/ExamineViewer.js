c929080325bc587d6ebdb41e9060027deb646804
Holger Seelig <holger.seelig@gmail.com>
2022-11-25 14:08:26 +0100

    Made free spin independent form frame rate.

--- src/x_ite/Browser/Navigation/ExamineViewer.js
+++ src/x_ite/Browser/Navigation/ExamineViewer.js
@@ -665,7 +665,7 @@ ExamineViewer .prototype = Object .assign (Object .create (X3DViewer .prototype)
       else
       {
          this .getBrowser () .prepareEvents () .addInterest ("spin", this);
-         this .rotation .assign (Rotation4 .Identity) .slerp (rotationChange, SPIN_FACTOR);
+         this .rotation .assign (rotationChange);
       }
    },
    spin: function ()
@@ -688,9 +688,11 @@ ExamineViewer .prototype = Object .assign (Object .create (X3DViewer .prototype)
       }
       else
       {
+         const rotation = Rotation4 .Identity .copy () .slerp (this .rotation, SPIN_FACTOR * 60 / this .getBrowser () .getCurrentFrameRate ());
+
          this .orientationOffset .assign (viewpoint ._orientationOffset .getValue ());
 
-         viewpoint ._orientationOffset = this .getOrientationOffset (this .rotation, this .orientationOffset);
+         viewpoint ._orientationOffset = this .getOrientationOffset (rotation, this .orientationOffset);
          viewpoint ._positionOffset    = this .getPositionOffset (viewpoint ._positionOffset .getValue (), this .orientationOffset, viewpoint ._orientationOffset .getValue ());
       }
    },
