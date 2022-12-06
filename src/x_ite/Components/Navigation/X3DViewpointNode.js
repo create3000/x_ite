ba4f9b333429df282b7a442acacb970c038dc484
Holger Seelig <holger.seelig@gmail.com>
2022-12-06 18:00:32 +0100

    Fixed transition nofify.

--- src/x_ite/Components/Navigation/X3DViewpointNode.js
+++ src/x_ite/Components/Navigation/X3DViewpointNode.js
@@ -269,6 +269,8 @@ X3DViewpointNode .prototype = Object .assign (Object .create (X3DBindableNode .p
                }
             }
 
+            layerNode .getNavigationInfo () ._transitionStart = true;
+
             this .timeSensor ._cycleInterval = transitionTime;
             this .timeSensor ._stopTime      = this .getBrowser () .getCurrentTime ();
             this .timeSensor ._startTime     = this .getBrowser () .getCurrentTime ();
