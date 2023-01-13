fb4da659a274fd778db953fa872cefb8ad2600b4
Holger Seelig <holger.seelig@gmail.com>
2023-01-13 21:48:19 +0100

    Trim name.

--- src/x_ite/Parser/X3DParser.js
+++ src/x_ite/Parser/X3DParser.js
@@ -171,14 +171,14 @@ X3DParser .prototype = {
       if (typeof name !== "string")
          return;
 
+      // Spaces
+      name = name .trim () .replace (/\s+/g, "-");
+
       // NonIdFirstChar
       name = name .replace (/^[\x30-\x39\x00-\x20\x22\x23\x27\x2b\x2c\x2d\x2e\x5b\x5c\x5d\x7b\x7d\x7f]*/, "");
 
       // NonIdChars
       name = name .replace (/[\x00-\x20\x22\x23\x27\x2c\x2e\x5b\x5c\x5d\x7b\x7d\x7f]/g, "");
-
-      // Spaces
-      name = name .trim () .replace (/\s+/g, "-");
 
       return name;
    },
