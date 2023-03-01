1960686f00a2c47fb889f9f7283ab14ed4a72435
Holger Seelig <holger.seelig@gmail.com>
2023-03-01 01:13:21 +0100

    Remove normalizeVelocity again.

--- src/x_ite/Components/Interpolation/SquadOrientationInterpolator.js
+++ src/x_ite/Components/Interpolation/SquadOrientationInterpolator.js
@@ -68,13 +68,12 @@ SquadOrientationInterpolator .prototype = Object .assign (Object .create (X3DInt
 {
    constructor: SquadOrientationInterpolator,
    [Symbol .for ("X_ITE.X3DBaseNode.fieldDefinitions")]: new FieldDefinitionArray ([
-      new X3DFieldDefinition (X3DConstants .inputOutput, "metadata",          new Fields .SFNode ()),
-      new X3DFieldDefinition (X3DConstants .inputOnly,   "set_fraction",      new Fields .SFFloat ()),
-      new X3DFieldDefinition (X3DConstants .inputOutput, "closed",            new Fields .SFBool ()),
-      new X3DFieldDefinition (X3DConstants .inputOutput, "key",               new Fields .MFFloat ()),
-      new X3DFieldDefinition (X3DConstants .inputOutput, "keyValue",          new Fields .MFRotation ()),
-      new X3DFieldDefinition (X3DConstants .inputOutput, "normalizeVelocity", new Fields .SFBool ()),
-      new X3DFieldDefinition (X3DConstants .outputOnly,  "value_changed",     new Fields .SFRotation ()),
+      new X3DFieldDefinition (X3DConstants .inputOutput, "metadata",      new Fields .SFNode ()),
+      new X3DFieldDefinition (X3DConstants .inputOnly,   "set_fraction",  new Fields .SFFloat ()),
+      new X3DFieldDefinition (X3DConstants .inputOutput, "closed",        new Fields .SFBool ()),
+      new X3DFieldDefinition (X3DConstants .inputOutput, "key",           new Fields .MFFloat ()),
+      new X3DFieldDefinition (X3DConstants .inputOutput, "keyValue",      new Fields .MFRotation ()),
+      new X3DFieldDefinition (X3DConstants .outputOnly,  "value_changed", new Fields .SFRotation ()),
    ]),
    getTypeName: function ()
    {
