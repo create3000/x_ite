4938bb7833abac672ce0ef4f03ae37980bfbda2a
Holger Seelig <holger.seelig@gmail.com>
2023-03-03 15:46:38 +0100

    Use transitionTime.

--- src/x_ite/Components/Navigation/X3DViewpointNode.js
+++ src/x_ite/Components/Navigation/X3DViewpointNode.js
@@ -479,7 +479,7 @@ X3DViewpointNode .prototype = Object .assign (Object .create (X3DBindableNode .p
    {
       layerNode .getNavigationInfo () ._transitionStart = true;
 
-      this .timeSensor ._cycleInterval = 0.4;
+      this .timeSensor ._cycleInterval = layerNode .getNavigationInfo () ._transitionTime .getValue ();
       this .timeSensor ._stopTime      = this .getBrowser () .getCurrentTime ();
       this .timeSensor ._startTime     = this .getBrowser () .getCurrentTime ();
 
