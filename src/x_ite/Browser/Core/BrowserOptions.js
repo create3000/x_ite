d423b5ed8098363b08fc4e0d479b01697d79a7e7
Holger Seelig <holger.seelig@gmail.com>
2023-03-29 16:39:21 +0200

    Removed option.

--- src/x_ite/Browser/Core/BrowserOptions.js
+++ src/x_ite/Browser/Core/BrowserOptions.js
@@ -92,7 +92,6 @@ BrowserOptions .prototype = Object .assign (Object .create (X3DBaseNode .prototy
       new X3DFieldDefinition (X3DConstants .inputOutput, "Notifications",          new Fields .SFBool (true)),
       new X3DFieldDefinition (X3DConstants .inputOutput, "Multisampling",          new Fields .SFInt32 (4)),
       new X3DFieldDefinition (X3DConstants .inputOutput, "OptimizeStaticGroup",    new Fields .SFBool (true)),
-      new X3DFieldDefinition (X3DConstants .inputOutput, "PointingDevice",         new Fields .SFBool (true)),
       new X3DFieldDefinition (X3DConstants .inputOutput, "StraightenHorizon",      new Fields .SFBool (true)),
       new X3DFieldDefinition (X3DConstants .inputOutput, "Timings",                new Fields .SFBool ()),
    ]),
