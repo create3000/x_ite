b6cce1a8133c5ca7ecce9c4c482d751be80da6ec
Holger Seelig <holger.seelig@gmail.com>
2023-05-03 18:24:58 +0200

    Added function.

--- build/bin/components.js
+++ build/bin/components.js
@@ -37,6 +37,20 @@ function createIndex ()
    return index
 }
 
+function getSpecificationRange (component, node)
+{
+   const
+      filename = `${components}/${component}/${node}.js`,
+      file     = fs .readFileSync (filename) .toString (),
+      match    = file .match (/getSpecificationRange.*?(\[.*?\])/s),
+      range    = eval (match [1])
+
+   if (range [1] == Infinity)
+      return range [0]
+
+   return `${range [0]} - ${range [1]}`
+}
+
 function updateNav ()
 {
    for (const [component, nodes] of createIndex ())
@@ -86,6 +100,37 @@ function updateComponents ()
 
    fs .writeFileSync (md, text)
 }
+
+// function updateComponents ()
+// {
+//    let list = "\n\n"
+
+//    for (const [component, nodes] of createIndex ())
+//    {
+//       list += `## ${component}\n\n`
+//       list += `| Node | Version | Status |\n`
+//       list += `|------|---------|--------|\n`
+
+//       for (const node of nodes .sort ())
+//       {
+//          const
+//             slug    = `${component}/${node}` .toLowerCase () .replace (/_/g, "-"),
+//             version = getSpecificationRange (component, node)
+
+//          list += `| [${node}](${slug}) | ${version} | <span class="green">implemented</span> |\n`
+//       }
+
+//       list += `\n`
+//    }
+
+//    const md = path .resolve (tabs, `components.md`)
+
+//    let text = fs .readFileSync (md) .toString ()
+
+//    text = text .replace (/<!-- COMPONENTS BEGIN -->.*?<!-- COMPONENTS END -->/s, `<!-- COMPONENTS BEGIN -->${list}<!-- COMPONENTS END -->`)
+
+//    fs .writeFileSync (md, text)
+// }
 
 async function addNodeStubs ()
 {
