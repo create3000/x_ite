28b7c8a1626aec5c8ab93b94d0bb2752e39d6395
Holger Seelig <holger.seelig@gmail.com>
2023-06-05 01:41:43 +0200

    Updated valueOf.

--- src/x_ite/Fields/SFBool.js
+++ src/x_ite/Fields/SFBool.js
@@ -67,10 +67,7 @@ SFBool .prototype = Object .assign (Object .create (X3DField .prototype),
    {
       X3DField .prototype .set .call (this, !! value);
    },
-   valueOf: function ()
-   {
-      return this .getValue ();
-   },
+   valueOf: X3DField .prototype .getValue,
    toStream: function (generator)
    {
       generator .string += this .getValue () ? "TRUE" : "FALSE";
