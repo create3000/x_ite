7b6b5bf55c279984e5c88e027ff35900fc0c0fd2
Holger Seelig <holger.seelig@gmail.com>
2023-06-09 12:39:31 +0200

    Fixed typo.

--- src/x_ite/Parser/VRMLParser.js
+++ src/x_ite/Parser/VRMLParser.js
@@ -1227,7 +1227,7 @@ Object .assign (Object .setPrototypeOf (VRMLParser .prototype, X3DParser .protot
             ?? this .getExecutionContext () .createProto (nodeTypeId, false);
 
          if (!baseNode)
-            throw new Error ("Unknown node type or proto '" + nodeTypeId + "', you probably have insufficient component/profile statements, and/or an inappropriate specification version.");
+            throw new Error (`Unknown node type or proto '${nodeTypeId}', you probably have insufficient component/profile statements, and/or an inappropriate specification version.`);
 
          if (nodeNameId .length)
          {
