9174dfe61b3be036ca0083de75fe6c5024db2947
Holger Seelig <holger.seelig@gmail.com>
2023-06-23 16:05:57 +0200

    Updated fields script.

--- build/docs/fields.pl
+++ build/docs/fields.pl
@@ -17,23 +17,25 @@ sub node {
    return if $componentName =~ /^Annotation$/o;
    return if $typeName =~ /^X3D/o;
 
-   #return unless $typeName =~ /^Delay$/o;
+   return unless $typeName =~ /^Delay$/o;
    say "$componentName $typeName";
 
    $md     = "$cwd/docs/_posts/components/$componentName/$typeName.md";
    $file   = `cat $md`;
    @fields = map { /\*\*(.*?)\*\*/o; $_ = $1 } $file =~ /###\s*[SM]F\w+.*/go;
 
-   fill_empty_field ($_, $file) foreach @fields;
+   $file = fill_empty_field ($_, $file) foreach @fields;
 }
 
 sub fill_empty_field {
    $name = shift;
    $file = shift;
 
-   return unless $file =~ m|###.*?\*\*$name\*\*.*?[\s\n]+###|;
+   return $file unless $file =~ /###.*?\*\*$name\*\*.*?[\s\n]+###/;
 
    say "  $name";
+
+   return $file;
 }
 
 $cwd = getcwd ();
