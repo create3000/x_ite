b8491bef0fc00469db6cf01f5153d3333a8455b5
Holger Seelig <holger.seelig@gmail.com>
2023-06-29 12:25:34 +0200

    Parse basic geometries.

--- src/x_ite/Parser/PLYAParser.js
+++ src/x_ite/Parser/PLYAParser.js
@@ -47,6 +47,8 @@
 
 import X3DParser   from "./X3DParser.js";
 import Expressions from "./Expressions.js";
+import Vector3     from "../../standard/Math/Numbers/Vector3.js";
+import Color3      from "../../standard/Math/Numbers/Color3.js";
 
 /*
  *  Grammar
@@ -56,6 +58,7 @@ import Expressions from "./Expressions.js";
 const Grammar = Expressions ({
    // General
    whitespaces: /[\x20\n\t\r]+/gy,
+   whitespacesNoLineTerminator: /[\x20\t]+/gy,
    untilEndOfLine: /([^\r\n]+)/gy,
 
    // Keywords
@@ -63,13 +66,12 @@ const Grammar = Expressions ({
    format: /format ascii 1.0/gy,
    comment: /\bcomment\b/gy,
    element: /\belement\b/gy,
-   vertex: /\bvertex\b/gy,
-   face: /\bface\b/gy,
+   elementType: /\b(vertex|face|edge)\b/gy,
    property: /\bproperty\b/gy,
-   float: /\bfloat\b/gy,
-   x: /\bx\b/gy,
-   y: /\by\b/gy,
-   z: /\bz\b/gy,
+   propertyList: /\blist\b/gy,
+   propertyType: /\b(char|uchar|short|ushort|int|uint|float|double)\b/gy,
+   propertyName: /\b(\S+)\b/gy,
+   endHeader: /\bend_header\b/gy,
 
    //property list uchar int vertex_indices
 
@@ -84,6 +86,17 @@ const Grammar = Expressions ({
 function PLYAParser (scene)
 {
    X3DParser .call (this, scene);
+
+   this .typeMapping = new Map ([
+      ["char",   this .int32],
+      ["uchar",  this .int32],
+      ["short",  this .int32],
+      ["ushort", this .int32],
+      ["int",    this .int32],
+      ["uint",   this .int32],
+      ["float",  this .double],
+      ["double", this .double],
+   ]);
 }
 
 Object .assign (Object .setPrototypeOf (PLYAParser .prototype, X3DParser .prototype),
@@ -119,7 +132,10 @@ Object .assign (Object .setPrototypeOf (PLYAParser .prototype, X3DParser .protot
 
       await this .loadComponents ();
 
-      this .header ();
+      const elements = [ ];
+
+      this .header (elements);
+      this .processElements (elements)
 
       // Create nodes.
 
@@ -133,6 +149,10 @@ Object .assign (Object .setPrototypeOf (PLYAParser .prototype, X3DParser .protot
    whitespaces ()
    {
       return Grammar .whitespaces .parse (this);
+   },
+   whitespacesNoLineTerminator ()
+   {
+      Grammar .whitespacesNoLineTerminator .parse (this);
    },
    comment ()
    {
@@ -151,35 +171,242 @@ Object .assign (Object .setPrototypeOf (PLYAParser .prototype, X3DParser .protot
 
       return false;
    },
-   header ()
+   int32 ()
    {
-      Grammar .ply .parse (this);
-      Grammar .whitespaces .parse (this);
+      this .whitespacesNoLineTerminator ();
 
-      Grammar .format .parse (this);
-      Grammar .whitespaces .parse (this);
+      if (Grammar .int32 .parse (this))
+      {
+         this .value = parseInt (this .result [1]);
 
-      this .elements ();
+         return true;
+      }
 
       return false;
    },
-   elements ()
+   header (elements)
    {
-      while (this .element ())
+      Grammar .ply .parse (this);
+      Grammar .whitespaces .parse (this);
+      Grammar .format .parse (this);
+
+      this .elements (elements);
+
+      Grammar .whitespaces .parse (this);
+      Grammar .endHeader .parse (this);
+   },
+   elements (elements)
+   {
+      while (this .element (elements))
          ;
    },
-   element ()
+   element (elements)
    {
       this .whitespacesOrComments ();
 
       if (Grammar .element .parse (this))
       {
-         console .log ("element");
+         this .whitespacesNoLineTerminator ();
+
+         if (Grammar .elementType .parse (this))
+         {
+            const type = this .result [1];
+
+            if (this .int32 ())
+            {
+               const element =
+               {
+                  type: type,
+                  count: this .value,
+                  properties: [ ],
+               };
+
+               this .properties (element .properties);
+
+               elements .push (element);
+               return true;
+            }
+         }
+
          return true;
       }
 
       return false;
-   }
+   },
+   properties (properties)
+   {
+      while (this .property (properties))
+         ;
+
+      properties .color = properties .some (p => p .name .match (/red|green|blue/));
+   },
+   property (properties)
+   {
+      this .whitespacesOrComments ();
+
+      if (Grammar .property .parse (this))
+      {
+         this .whitespacesNoLineTerminator ();
+
+         if (Grammar .propertyType .parse (this))
+         {
+            const type = this .typeMapping .get (this .result [1]);
+
+            this .whitespacesNoLineTerminator ();
+
+            if (Grammar .propertyName .parse (this))
+            {
+               const name = this .result [1];
+
+               properties .push ({ type, name });
+               return true;
+            }
+         }
+
+         if (Grammar .propertyList .parse (this))
+         {
+            this .whitespacesNoLineTerminator ();
+
+            if (Grammar .propertyType .parse (this))
+            {
+               const count = this .typeMapping .get (this .result [1]);
+
+               this .whitespacesNoLineTerminator ();
+
+               if (Grammar .propertyType .parse (this))
+               {
+                  const type = this .typeMapping .get (this .result [1]);
+
+                  this .whitespacesNoLineTerminator ();
+
+                  if (Grammar .propertyName .parse (this))
+                  {
+                     const name = this .result [1];
+
+                     properties .push ({ count, type, name });
+                     return true;
+                  }
+               }
+            }
+         }
+      }
+
+      return false;
+   },
+   processElements (elements)
+   {
+      for (const element of elements)
+         this .processElement (element);
+
+      if (!this .coord)
+         return;
+
+      const
+         scene      = this .getExecutionContext (),
+         shape      = scene .createNode ("Shape"),
+         appearance = scene .createNode ("Appearance"),
+         material   = scene .createNode ("Material");
+
+      if (!this .geometry)
+         this .geometry = scene .createNode ("PointSet");
+
+      this .geometry .coord = this .coord;
+      this .geometry .color = this .color;
+
+      appearance .material = material;
+      shape .appearance    = appearance;
+      shape .geometry      = this .geometry;
+
+      scene .rootNodes .push (shape);
+   },
+   processElement (element)
+   {
+      switch (element .type)
+      {
+         case "vertex":
+            this .parseVertices (element);
+            break;
+         case "face":
+            this .parseFaces (element);
+            break;
+      }
+   },
+   parseVertices (element)
+   {
+      const
+         scene  = this .getExecutionContext (),
+         coord  = scene .createNode ("Coordinate"),
+         point  = coord .point,
+         color  = scene .createNode ("Color"),
+         colors = color .color;
+
+      const { count, properties } = element;
+
+      for (let i = 0; i < count; ++ i)
+      {
+         let x = 0, y = 0, z = 0, r = 1, g = 1, b = 1;
+
+         this .whitespaces ();
+
+         for (const property of properties)
+         {
+            if (!property .type .call (this))
+               throw new Error (`Couldn't parse value for property ${property .name}.`);
+
+            switch (property .name)
+            {
+               case "x": x = this .value; break;
+               case "y": y = this .value; break;
+               case "z": z = this .value; break;
+               case "red":   r = this .value / 255; break;
+               case "green": g = this .value / 255; break;
+               case "blue":  b = this .value / 255; break;
+            }
+         }
+
+         point .push (new Vector3 (x, y, z));
+
+         if (properties .color)
+            colors .push (new Color3 (r, g, b));
+      }
+
+      this .coord = coord;
+      this .color = colors .length ? color : null;
+   },
+   parseFaces (element)
+   {
+      const
+         scene      = this .getExecutionContext (),
+         geometry   = scene .createNode ("IndexedFaceSet"),
+         coordIndex = geometry .coordIndex;
+
+      const count = element .count;
+
+      for (let i = 0; i < count; ++ i)
+      {
+         this .whitespaces ();
+
+         for (const property of element .properties)
+         {
+            if (!property .count .call (this))
+               throw new Error (`Couldn't parse property count for ${property .name}.`);
+
+            const count = this .value;
+
+            for (let i = 0; i < count; ++ i)
+            {
+               if (!property .type .call (this))
+                  throw new Error (`Couldn't parse a property value for ${property .name}.`);
+
+               coordIndex .push (this .value);
+            }
+
+            coordIndex .push (-1);
+         }
+      }
+
+      this .geometry = geometry;
+   },
 });
 
 export default PLYAParser;
