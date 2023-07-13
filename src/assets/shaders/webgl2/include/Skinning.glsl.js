4edac3519acfa45103cdf7e6d762aad2742414d7
Holger Seelig <holger.seelig@gmail.com>
2023-07-14 00:49:47 +0200

    Added skinning shader part.

--- src/assets/shaders/webgl2/include/Skinning.glsl.js
+++ src/assets/shaders/webgl2/include/Skinning.glsl.js
@@ -16,27 +16,19 @@ getJointMatrix (const in int joint)
    vec4 c = texelFetch (x3d_JointMatricesTexture, joint * 4 + 2, 0);
    vec4 d = texelFetch (x3d_JointMatricesTexture, joint * 4 + 3, 0);
 
-   return mat4 (a, b, c, d);
+   return transpose (mat4 (a, b, c, d));
 }
 
 vec4
 getSkinningVertex (const in vec4 vertex)
 {
-   int  coordIndex = int (x3d_CoordIndex);
-   vec4 joints     = texelFetch (x3d_JointsTexture,  coordIndex, 0);
-   vec4 weights    = texelFetch (x3d_WeightsTexture, coordIndex, 0);
+   int   coordIndex = int (x3d_CoordIndex);
+   ivec4 joints     = ivec4 (texelFetch (x3d_JointsTexture, coordIndex, 0));
+   vec4  weights    = texelFetch (x3d_WeightsTexture, coordIndex, 0);
+   vec4  skin       = vertex;
 
-   mat4 jointMatrix0 = getJointMatrix (int (joints [0]));
-   mat4 jointMatrix1 = getJointMatrix (int (joints [1]));
-   mat4 jointMatrix2 = getJointMatrix (int (joints [2]));
-   mat4 jointMatrix3 = getJointMatrix (int (joints [3]));
-
-   vec4 skin = vertex;
-
-   skin += (jointMatrix0 [0] * vertex - vertex) * weights [0];
-   skin += (jointMatrix1 [1] * vertex - vertex) * weights [1];
-   skin += (jointMatrix2 [2] * vertex - vertex) * weights [2];
-   skin += (jointMatrix3 [3] * vertex - vertex) * weights [3];
+   for (int i = 0; i < 4; ++ i)
+      skin += (getJointMatrix (joints [i]) * vertex - vertex) * weights [i];
 
    return skin;
 }
