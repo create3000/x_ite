dc8defac23e65d805bd355522d54db96c017bea9
Holger Seelig <holger.seelig@gmail.com>
2023-07-13 19:43:01 +0200

    Moved requestImmediateLoad to setProtoNode.

--- src/x_ite/Components/Core/X3DPrototypeInstance.js
+++ src/x_ite/Components/Core/X3DPrototypeInstance.js
@@ -97,17 +97,7 @@ Object .assign (Object .setPrototypeOf (X3DPrototypeInstance .prototype, X3DNode
       if (protoNode .isExternProto)
       {
          if (protoNode .checkLoadState () !== X3DConstants .COMPLETE_STATE)
-         {
-            this [_body] = null;
-
-            if (this .isInitialized ())
-               X3DChildObject .prototype .addEvent .call (this);
-
-            protoNode ._updateInstances .addInterest ("construct", this);
-            protoNode .requestImmediateLoad () .catch (Function .prototype);
-
             return;
-         }
 
          for (const protoField of proto .getUserDefinedFields ())
          {
@@ -246,7 +236,20 @@ Object .assign (Object .setPrototypeOf (X3DPrototypeInstance .prototype, X3DNode
 
       protoNode ._name_changed .addFieldInterest (this ._typeName_changed);
 
-      this .construct ();
+      if (protoNode .isExternProto && protoNode .checkLoadState () !== X3DConstants .COMPLETE_STATE)
+      {
+         this [_body] = null;
+
+         if (this .isInitialized ())
+            X3DChildObject .prototype .addEvent .call (this);
+
+         protoNode ._updateInstances .addInterest ("construct", this);
+         protoNode .requestImmediateLoad () .catch (Function .prototype);
+      }
+      else
+      {
+         this .construct ();
+      }
    },
    getBody ()
    {
