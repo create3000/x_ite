c41ab0c299e5dbdd6e7aa8eda4bddf351c57d4fa
Holger Seelig <holger.seelig@gmail.com>
2023-07-14 06:03:23 +0200

    Handle skinning normals.

--- src/assets/shaders/webgl2/include/Skinning.glsl.js
+++ src/assets/shaders/webgl2/include/Skinning.glsl.js
@@ -34,10 +34,28 @@ getSkinningVertex (const in vec4 vertex)
 }
 
 #if defined (X3D_NORMALS)
+mat3
+getJointNormalMatrix (const in int joint)
+{
+   vec4 a = texelFetch (x3d_JointNormalMatricesTexture, joint * 4 + 0, 0);
+   vec4 b = texelFetch (x3d_JointNormalMatricesTexture, joint * 4 + 1, 0);
+   vec4 c = texelFetch (x3d_JointNormalMatricesTexture, joint * 4 + 2, 0);
+
+   return mat3 (a .xyz, vec3 (a .w, b .xy), vec3 (b .zw, c .x));
+}
+
 vec3
 getSkinningNormal (const in vec3 normal)
 {
-   return normal;
+   int   coordIndex = int (x3d_CoordIndex);
+   ivec4 joints     = ivec4 (texelFetch (x3d_JointsTexture, coordIndex, 0));
+   vec4  weights    = texelFetch (x3d_WeightsTexture, coordIndex, 0);
+   vec3  skin       = normal;
+
+   for (int i = 0; i < 4; ++ i)
+      skin += (getJointNormalMatrix (joints [i]) * normal - normal) * weights [i];
+
+   return skin;
 }
 #endif
 
