64055408d7f4d9ee36bbf6f42287cb935b2691d7
Holger Seelig <holger.seelig@gmail.com>
2023-07-29 22:46:50 +0200

    Handle style properties texture in shader.

--- src/assets/shaders/webgl1/include/Hatch.glsl.js
+++ src/assets/shaders/webgl1/include/Hatch.glsl.js
@@ -8,13 +8,12 @@ getHatchColor (vec4 color)
 {
    vec4 finalColor = x3d_FillProperties .filled ? color : vec4 (0.0);
 
-   if (x3d_FillProperties .hatched)
-   {
+   #if defined (X3D_STYLE_PROPERTIES_TEXTURE)
       vec4 hatch = texture2D (x3d_FillProperties .texture, gl_FragCoord .xy / (32.0 * x3d_FillProperties .scale));
 
       hatch .rgb *= x3d_FillProperties .hatchColor;
       finalColor  = mix (finalColor, hatch, hatch .a);
-   }
+   #endif
 
    return finalColor;
 }
