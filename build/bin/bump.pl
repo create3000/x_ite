a00f7bc57cc64ddd7d375f049272f9b9dd1340c6
Holger Seelig <holger.seelig@gmail.com>
2023-07-31 18:17:56 +0200

    Determine name from package.json.

--- build/bin/bump.pl
+++ build/bin/bump.pl
@@ -5,13 +5,16 @@ use v5.10.0;
 use utf8;
 use open qw/:std :utf8/;
 
+my $name = `node -p "require('./package.json').name"`;
+chomp $name;
+
 my $version = `npm pkg get version | sed 's/"//g'`;
 chomp $version;
 #say "package.json version $version";
 
 exit if $version =~ /a$/;
 
-my $online = `npm view x_ite version`;
+my $online = `npm view $name version`;
 chomp $online;
 #say "NPM version $online";
 
