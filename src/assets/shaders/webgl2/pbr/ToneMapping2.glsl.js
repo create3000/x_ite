ad324641c318982c36c478091940ce6dae32f634
Holger Seelig <holger.seelig@gmail.com>
2023-09-27 23:55:40 +0200

    Updated defines.

--- src/assets/shaders/webgl2/pbr/ToneMapping2.glsl.js
+++ src/assets/shaders/webgl2/pbr/ToneMapping2.glsl.js
@@ -4,22 +4,6 @@ const float exposure = 1.0;
 const float GAMMA     = 2.2;
 const float INV_GAMMA = 1.0 / GAMMA;
 
-// sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
-const mat3 ACESInputMat = mat3
-(
-   0.59719, 0.07600, 0.02840,
-   0.35458, 0.90834, 0.13383,
-   0.04823, 0.01566, 0.83777
-);
-
-// ODT_SAT => XYZ => D60_2_D65 => sRGB
-const mat3 ACESOutputMat = mat3
-(
-   1.60475, -0.10208, -0.00327,
-   -0.53108,  1.10813, -0.07276,
-   -0.07367, -0.00605,  1.07602
-);
-
 // linear to sRGB approximation
 // see http://chilliant.blogspot.com/2012/08/srgb-approximations-for-hlsl.html
 vec3
@@ -59,6 +43,22 @@ toneMapACES_Narkowicz (const in vec3 color)
 #endif
 
 #if defined (TONEMAP_ACES_HILL) || defined (TONEMAP_ACES_HILL_EXPOSURE_BOOST)
+// sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
+const mat3 ACESInputMat = mat3
+(
+   0.59719, 0.07600, 0.02840,
+   0.35458, 0.90834, 0.13383,
+   0.04823, 0.01566, 0.83777
+);
+
+// ODT_SAT => XYZ => D60_2_D65 => sRGB
+const mat3 ACESOutputMat = mat3
+(
+   1.60475, -0.10208, -0.00327,
+   -0.53108,  1.10813, -0.07276,
+   -0.07367, -0.00605,  1.07602
+);
+
 // ACES filmic tone map approximation
 // see https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
 vec3
