2949d52c87fd57031d69568cbe08579fae2bb265
Holger Seelig <holger.seelig@gmail.com>
2023-10-07 15:55:05 +0200

    Add spaces.

--- src/x_ite/Browser/CubeMapTexturing/Panorama.fs.js
+++ src/x_ite/Browser/CubeMapTexturing/Panorama.fs.js
@@ -5,7 +5,7 @@ precision highp sampler2D;
 
 const float M_PI = 3.1415926535897932384626433832795;
 
-in vec2 texCoord;
+in  vec2 texCoord;
 out vec4 x3d_FragColor;
 
 uniform sampler2D x3d_PanoramaTexture;
