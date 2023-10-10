1fbcaf103bb4bffddab038a4cd7a1a96b69e1c95
Holger Seelig <holger.seelig@gmail.com>
2023-10-10 15:19:44 +0200

    Code clean up.

--- src/x_ite/Browser/Texturing/KTXDecoder.js
+++ src/x_ite/Browser/Texturing/KTXDecoder.js
@@ -13,65 +13,66 @@ export default class KTXDecoder {
          }
          if (externalKtxlib !== undefined)
          {
-            this.initialized = this.init(context, externalKtxlib, scriptDir);
+            this .initialized = this .init (context, externalKtxlib, scriptDir);
          }
          else
          {
-            console.error('Failed to initalize KTXDecoder: ktx library undefined');
+            console .error ("Failed to initalize KTXDecoder: ktx library undefined");
             return undefined;
          }
       }
       else
       {
-         console.error('Failed to initalize KTXDecoder: WebGL context undefined');
+         console .error ("Failed to initalize KTXDecoder: WebGL context undefined");
          return undefined;
       }
    }
 
    async init(context, externalKtxlib, scriptDir)
    {
-      this.libktx = await externalKtxlib({preinitializedWebGLContext: context}, scriptDir);
-      this.libktx.GL.makeContextCurrent(this.libktx.GL.createContext(null, { majorVersion: 2.0 }));
+      this .libktx = await externalKtxlib({preinitializedWebGLContext: context}, scriptDir);
+
+      this .libktx .GL .makeContextCurrent (this .libktx .GL .createContext (null, { majorVersion: 2.0 }));
    }
 
    transcode(ktexture)
    {
-      if (ktexture.needsTranscoding) {
-         let format;
+      if (!ktexture .needsTranscoding)
+         return;
 
-         let astcSupported = false;
-         let etcSupported = false;
-         let dxtSupported = false;
-         let bptcSupported = false;
-         let pvrtcSupported = false;
+      let format;
 
-         astcSupported = !!this.gl.getExtension('WEBGL_compressed_texture_astc');
-         etcSupported = !!this.gl.getExtension('WEBGL_compressed_texture_etc1');
-         dxtSupported = !!this.gl.getExtension('WEBGL_compressed_texture_s3tc');
-         bptcSupported = !!this.gl.getExtension('EXT_texture_compression_bptc');
+      let astcSupported  = false;
+      let etcSupported   = false;
+      let dxtSupported   = false;
+      let bptcSupported  = false;
+      let pvrtcSupported = false;
 
-         pvrtcSupported = !!(this.gl.getExtension('WEBGL_compressed_texture_pvrtc')) || !!(this.gl.getExtension('WEBKIT_WEBGL_compressed_texture_pvrtc'));
+      astcSupported = !!this .gl .getExtension ("WEBGL_compressed_texture_astc");
+      etcSupported  = !!this .gl .getExtension ("WEBGL_compressed_texture_etc1");
+      dxtSupported  = !!this .gl .getExtension ("WEBGL_compressed_texture_s3tc");
+      bptcSupported = !!this .gl .getExtension ("EXT_texture_compression_bptc");
 
-         if (astcSupported) {
-            format = this.libktx.TranscodeTarget.ASTC_4x4_RGBA;
-         } else if (bptcSupported) {
-            format = this.libktx.TranscodeTarget.BC7_RGBA;
-         } else if (dxtSupported) {
-            format = this.libktx.TranscodeTarget.BC1_OR_3;
-         } else if (pvrtcSupported) {
-            format = this.libktx.TranscodeTarget.PVRTC1_4_RGBA;
-         } else if (etcSupported) {
-            format = this.libktx.TranscodeTarget.ETC;
-         } else {
-            format = this.libktx.TranscodeTarget.RGBA8888;
-         }
-         if (ktexture.transcodeBasis(format, 0) != this.libktx.ErrorCode.SUCCESS) {
-            console.warn('Texture transcode failed. See console for details.');
-         }
-      }
+      pvrtcSupported = !!(this .gl .getExtension ("WEBGL_compressed_texture_pvrtc")) || !!(this .gl .getExtension ("WEBKIT_WEBGL_compressed_texture_pvrtc"));
+
+      if (astcSupported)
+         format = this .libktx .TranscodeTarget .ASTC_4x4_RGBA;
+      else if (bptcSupported)
+         format = this .libktx .TranscodeTarget .BC7_RGBA;
+      else if (dxtSupported)
+         format = this .libktx .TranscodeTarget .BC1_OR_3;
+      else if (pvrtcSupported)
+         format = this .libktx .TranscodeTarget .PVRTC1_4_RGBA;
+      else if (etcSupported)
+         format = this .libktx .TranscodeTarget .ETC;
+      else
+         format = this .libktx .TranscodeTarget .RGBA8888;
+
+      if (ktexture .transcodeBasis (format, 0) !== this .libktx .ErrorCode .SUCCESS)
+         console .warn ("Texture transcode failed. See console for details.");
    }
 
-   async loadKtxFromUri(uri)
+   async loadKtxFromUri (uri)
    {
       await this .initialized;
 
@@ -92,6 +93,7 @@ export default class KTXDecoder {
       uploadResult .texture .levels     = 1 + Math .floor (Math .log2 (texture .baseWidth));
       uploadResult .texture .baseWidth  = texture .baseWidth;
       uploadResult .texture .baseHeight = texture .baseHeight;
+      uploadResult .texture .baseDepth  = texture .baseDepth;
       uploadResult .texture .target     = uploadResult .target;
 
       return uploadResult .texture;
@@ -99,15 +101,26 @@ export default class KTXDecoder {
 
    async loadKtxFromBuffer(data)
    {
-      await this.initialized;
-      const texture = new this.libktx.ktxTexture(data);
-      this.transcode(texture);
-      const uploadResult = texture.glUpload();
-      if (uploadResult.texture == null)
+      await this .initialized;
+
+      const texture = new this .libktx .ktxTexture (data);
+
+      this .transcode (texture);
+
+      const uploadResult = texture .glUpload ();
+
+      if (uploadResult .texture === null)
       {
-         console.error("Could not load KTX data");
+         console .error ("Could not load KTX data");
          return undefined;
       }
-      return uploadResult.texture;
+
+      uploadResult .texture .levels     = 1 + Math .floor (Math .log2 (texture .baseWidth));
+      uploadResult .texture .baseWidth  = texture .baseWidth;
+      uploadResult .texture .baseHeight = texture .baseHeight;
+      uploadResult .texture .baseDepth  = texture .baseDepth;
+      uploadResult .texture .target     = uploadResult .target;
+
+      return uploadResult .texture;
    }
 }
