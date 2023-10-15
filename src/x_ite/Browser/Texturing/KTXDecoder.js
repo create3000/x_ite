4b3efd4958ee354cc3fc8e0b8703f2670311a56e
Holger Seelig <holger.seelig@gmail.com>
2023-10-15 11:10:30 +0200

    Code clean up.

--- src/x_ite/Browser/Texturing/KTXDecoder.js
+++ src/x_ite/Browser/Texturing/KTXDecoder.js
@@ -40,33 +40,25 @@ export default class KTXDecoder {
       if (!ktexture .needsTranscoding)
          return;
 
-      let format;
-
-      let astcSupported  = false;
-      let etcSupported   = false;
-      let dxtSupported   = false;
-      let bptcSupported  = false;
-      let pvrtcSupported = false;
-
-      astcSupported = !!this .gl .getExtension ("WEBGL_compressed_texture_astc");
-      etcSupported  = !!this .gl .getExtension ("WEBGL_compressed_texture_etc1");
-      dxtSupported  = !!this .gl .getExtension ("WEBGL_compressed_texture_s3tc");
-      bptcSupported = !!this .gl .getExtension ("EXT_texture_compression_bptc");
-
-      pvrtcSupported = !!(this .gl .getExtension ("WEBGL_compressed_texture_pvrtc")) || !!(this .gl .getExtension ("WEBKIT_WEBGL_compressed_texture_pvrtc"));
+      const
+         astcSupported  = !!this .gl .getExtension ("WEBGL_compressed_texture_astc"),
+         etcSupported   = !!this .gl .getExtension ("WEBGL_compressed_texture_etc1"),
+         dxtSupported   = !!this .gl .getExtension ("WEBGL_compressed_texture_s3tc"),
+         bptcSupported  = !!this .gl .getExtension ("EXT_texture_compression_bptc"),
+         pvrtcSupported = !!this .gl .getExtension ("WEBGL_compressed_texture_pvrtc") || !!this .gl .getExtension ("WEBKIT_WEBGL_compressed_texture_pvrtc");
 
       if (astcSupported)
-         format = this .libktx .TranscodeTarget .ASTC_4x4_RGBA;
+         var format = this .libktx .TranscodeTarget .ASTC_4x4_RGBA;
       else if (bptcSupported)
-         format = this .libktx .TranscodeTarget .BC7_RGBA;
+         var format = this .libktx .TranscodeTarget .BC7_RGBA;
       else if (dxtSupported)
-         format = this .libktx .TranscodeTarget .BC1_OR_3;
+         var format = this .libktx .TranscodeTarget .BC1_OR_3;
       else if (pvrtcSupported)
-         format = this .libktx .TranscodeTarget .PVRTC1_4_RGBA;
+         var format = this .libktx .TranscodeTarget .PVRTC1_4_RGBA;
       else if (etcSupported)
-         format = this .libktx .TranscodeTarget .ETC;
+         var format = this .libktx .TranscodeTarget .ETC;
       else
-         format = this .libktx .TranscodeTarget .RGBA8888;
+         var format = this .libktx .TranscodeTarget .RGBA8888;
 
       if (ktexture .transcodeBasis (format, 0) !== this .libktx .ErrorCode .SUCCESS)
          console .warn ("Texture transcode failed. See console for details.");
@@ -74,8 +66,6 @@ export default class KTXDecoder {
 
    async loadKTXFromURL (uri)
    {
-      await this .initialized;
-
       const response = await fetch (uri);
 
       return this .loadKTXFromBuffer (await response .arrayBuffer ());
@@ -92,7 +82,7 @@ export default class KTXDecoder {
 
       const uploadResult = texture .glUpload ();
 
-      if (uploadResult .texture == null)
+      if (uploadResult .texture === null)
          throw new Error ("Could not load KTX data");
 
       uploadResult .texture .levels        = 1 + Math .floor (Math .log2 (texture .baseWidth));
