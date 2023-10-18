d62168fbc980699c1f10aa34181ec850c32c65dd
Holger Seelig <holger.seelig@gmail.com>
2023-10-18 09:41:53 +0200

    Added comment.

--- src/x_ite/Browser/Texturing/TexturingConfiguration.js
+++ src/x_ite/Browser/Texturing/TexturingConfiguration.js
@@ -1,4 +1,4 @@
-// The numbers must remain in single digits, otherwise the base must
+// The numbers must remain in single digits, otherwise the number base must
 // be increased in the number output for the material shader key.
 
 export const maxTextureTransforms = 4;
