15affdc10b11323095c6836fce1ac4d63b969058
Holger Seelig <holger.seelig@gmail.com>
2023-11-07 12:10:48 +0100

    Use Uint as value.

--- src/standard/Utility/BitSet.js
+++ src/standard/Utility/BitSet.js
@@ -47,9 +47,11 @@
 
 import Algorithm from "../Math/Algorithm.js";
 
+const _value = Symbol ();
+
 function BitSet (value = 0)
 {
-   this .value = value;
+   this [_value] = value >>> 0;
 }
 
 Object .assign (BitSet .prototype,
@@ -57,7 +59,7 @@ Object .assign (BitSet .prototype,
    *[Symbol .iterator] ()
    {
       let
-         value = this .value,
+         value = this [_value],
          index = 0;
 
       while (value)
@@ -73,7 +75,7 @@ Object .assign (BitSet .prototype,
    {
       const mask = 1 << index;
 
-      return !! (this .value & mask);
+      return !! (this [_value] & mask);
    },
    set (index, value)
    {
@@ -84,15 +86,17 @@ Object .assign (BitSet .prototype,
    },
    add (index, mask = 1)
    {
-      this .value |= mask << index;
+      this [_value] |= mask << index;
+      this [_value] >>>= 0;
    },
    remove (index, mask = 1)
    {
-      this .value &= ~(mask << index);
+      this [_value] &= ~(mask << index);
+      this [_value] >>>= 0;
    },
    clear ()
    {
-      this .value = 0;
+      this [_value] = 0;
    },
    *entries ()
    {
@@ -101,11 +105,11 @@ Object .assign (BitSet .prototype,
    },
    valueOf ()
    {
-      return this .value >>> 0;
+      return this [_value];
    },
    toString (radix)
    {
-      return this .value .toString (radix);
+      return this [_value] .toString (radix);
    },
 });
 
@@ -116,7 +120,7 @@ Object .defineProperty (BitSet .prototype, "size",
 {
    get ()
    {
-      return Algorithm .bitCount (this .value);
+      return Algorithm .bitCount (this [_value]);
    },
 });
 
