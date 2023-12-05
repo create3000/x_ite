68efa2079276644562d9e2af0db521ae71ee81b0
Holger Seelig <holger.seelig@gmail.com>
2023-12-05 14:26:31 +0100

    Added X3DExecutionContext.

--- tests/module.js
+++ tests/module.js
@@ -2,4 +2,4 @@ import X3D from "../src/x_ite.js";
 
 const browser = X3D .createBrowser () .browser;
 const scene = browser .currentScene;
-const nodes = scene .rootNodes;
+const nodes = scene .addRoute ();
