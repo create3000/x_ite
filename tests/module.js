a4532f82d432d60e9e93bf1a4c425002d4f92883
Holger Seelig <holger.seelig@gmail.com>
2023-12-07 12:14:01 +0100

    Updated dist.

--- tests/module.js
+++ tests/module.js
@@ -2,7 +2,7 @@ import X3D from "../src/x_ite.js";
 
 const browser = X3D .createBrowser () .browser;
 const scene = browser .currentScene;
-const nodes = scene .addRoute ();
+const route = scene .addRoute ();
 const r = scene .rootNodes;
 const n = scene .createNode ("BlendMode");
 n .sourceColorFactor = "O"
--- tests/require.js
+++ tests/require.js
@@ -1,7 +1,7 @@
 const X3D = require ("../src/x_ite.js")
 const browser = X3D .createBrowser () .browser;
 const scene = browser .currentScene;
-const nodes = scene .addRoute ();
+const route = scene .addRoute ();
 const r = scene .rootNodes;
 const n = scene .createNode ("Appearance");
 n .blendMode .destinationAlphaFactor = 'ON'
