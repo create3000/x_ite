5252e88535d3bf5c0be11e13c892dbfc8d810b5d
Holger Seelig <holger.seelig@gmail.com>
2024-05-24 22:42:18 +0200

    Use destructuring.

--- docs/assets/js/playground.js
+++ docs/assets/js/playground.js
@@ -276,7 +276,7 @@ function addVRMLEncoding (monaco, browser)
       accessTypes: [
          "initializeOnly", "inputOnly", "outputOnly", "inputOutput",
       ],
-      // fieldTypes: Array .from (browser .fieldTypes, fieldType => ({typeName}) => typeName),
+      // fieldTypes: Array .from (browser .fieldTypes, ({typeName}) => typeName),
       fieldTypes: [
          "SFBool", "SFColor", "SFColorRGBA", "SFDouble", "SFFloat", "SFImage", "SFInt32", "SFMatrix3d", "SFMatrix3f", "SFMatrix4d", "SFMatrix4f", "SFNode", "SFRotation", "SFString", "SFTime", "SFVec2d", "SFVec2f", "SFVec3d", "SFVec3f", "SFVec4d", "SFVec4f", "MFBool", "MFColor", "MFColorRGBA", "MFDouble", "MFFloat", "MFImage", "MFInt32", "MFMatrix3d", "MFMatrix3f", "MFMatrix4d", "MFMatrix4f", "MFNode", "MFRotation", "MFString", "MFVec2d", "MFVec2f", "MFVec3d", "MFVec3f", "MFVec4d", "MFVec4f",
       ],
