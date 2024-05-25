ebabb8e16f459ca552b8c8b28655b49328246543
Holger Seelig <holger.seelig@gmail.com>
2024-05-25 11:46:08 +0200

    Use pixellated images in context menu.

--- dist/assets/components/Texturing3D.js
+++ dist/assets/components/Texturing3D.js
@@ -1,7 +1,7 @@
 /* X_ITE v9.6.2 */(() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 327:
+/***/ 287:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -38,7 +38,7 @@ var Ib=[cx,_q,cr,Yr,as,fs,hs,Hu,Su,cx,cx,cx,cx,cx,cx,cx];var Jb=[dx,si,gi,Wh,Kh,
 
 /***/ }),
 
-/***/ 297:
+/***/ 537:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -72,7 +72,7 @@ var _a=[yj,od,ef,yj];var $a=[zj,Li,di,bi,Kb,Lb,Mb,Nb,Rc,Sc,Uc,jd,xd,Ye,lf,yd,zd,
 
 /***/ }),
 
-/***/ 237:
+/***/ 997:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
 /*! dicom-parser - 1.8.12 - 2023-02-20 | (c) 2017 Chris Hafey | https://github.com/cornerstonejs/dicomParser */
@@ -4028,7 +4028,7 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_zlib__;
 
 /***/ }),
 
-/***/ 729:
+/***/ 337:
 /***/ ((module) => {
 
 /* -*- tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
@@ -7341,10 +7341,10 @@ var Decoder = class _Decoder {
 
 //# 
 ;// CONCATENATED MODULE: ./src/x_ite/Browser/Texturing3D/DICOMParser.js
-/* provided dependency */ var dicomParser = __webpack_require__(237);
-/* provided dependency */ var JpegImage = __webpack_require__(729);
-/* provided dependency */ var CharLS = __webpack_require__(327);
-/* provided dependency */ var OpenJPEG = __webpack_require__(297);
+/* provided dependency */ var dicomParser = __webpack_require__(997);
+/* provided dependency */ var JpegImage = __webpack_require__(337);
+/* provided dependency */ var CharLS = __webpack_require__(287);
+/* provided dependency */ var OpenJPEG = __webpack_require__(537);
 /*******************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
