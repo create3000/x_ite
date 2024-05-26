2e9880510bcb0856820f10ee206a19a1e53c0b10
Holger Seelig <holger.seelig@gmail.com>
2024-05-26 12:43:27 +0200

    Updated VRML lexer.

--- docs/_plugins/vrml.rb
+++ docs/_plugins/vrml.rb
@@ -17,8 +17,6 @@ Jekyll::Hooks.register :site, :pre_render do |site|
 
     mimetypes "model/x3d+vrml", "model/vrml", "x-world/x-vrml"
 
-    # Pseudo-documentation: https://stackoverflow.com/questions/1661197/what-characters-are-valid-for-javascript-variable-names
-
     def self.detect?(text)
       return true if text =~ /\A#(?:X3D|VRML)\b/
     end
@@ -28,11 +26,21 @@ Jekyll::Hooks.register :site, :pre_render do |site|
       rule %r/#.*?$/, Comment::Single
     end
 
+    def self.id_regex
+      %r/[^\x30-\x39\x00-\x20\x22\x23\x27\x2b\x2c\x2d\x2e\x5b\x5c\x5d\x7b\x7d\x7f]{1}[^\x00-\x20\x22\x23\x27\x2c\x2e\x5b\x5c\x5d\x7b\x7d\x7f]*/
+    end
+
+    id = self.id_regex
+
     state :root do
       rule %r/[,:.]/, Punctuation
       rule %r/[{}\[\]]/, Punctuation
       rule %r/PROTO|EXTERNPROTO/, Keyword, :typeName
-      rule %r/DEF|ROUTE|TO|IMPORT|EXPORT|AS/, Keyword, :name
+      rule %r/DEF|ROUTE|TO|EXPORT|AS/, Keyword, :name
+
+      rule %r/(IMPORT)(\s*)(#{id})(\s*)(.)(\s*)(#{id})/ do
+        groups Keyword, Text, Str::Regex, Text, Punctuation, Text, Str::Regex
+      end
 
       mixin :comments_and_whitespace
       mixin :keywords
@@ -43,9 +51,8 @@ Jekyll::Hooks.register :site, :pre_render do |site|
       rule %r/TRUE|FALSE|NULL/, Keyword::Constant
       rule %r/SFBool|SFColor|SFColorRGBA|SFDouble|SFFloat|SFImage|SFInt32|SFMatrix3d|SFMatrix3f|SFMatrix4d|SFMatrix4f|VrmlMatrix|SFNode|SFRotation|SFString|SFTime|SFVec2d|SFVec2f|SFVec3d|SFVec3f|SFVec4d|SFVec4f|MFBool|MFColor|MFColorRGBA|MFDouble|MFFloat|MFImage|MFInt32|MFMatrix3d|MFMatrix3f|MFMatrix4d|MFMatrix4f|MFNode|MFRotation|MFString|MFTime|MFVec2d|MFVec2f|MFVec3d|MFVec3f|MFVec4d|MFVec4f/, Keyword::Declaration
 
-      rule %r/[^\x30-\x39\x00-\x20\x22\x23\x27\x2b\x2c\x2d\x2e\x5b\x5c\x5d\x7b\x7d\x7f]{1}[^\x00-\x20\x22\x23\x27\x2c\x2e\x5b\x5c\x5d\x7b\x7d\x7f]*(?=\s*\{)/, Name::Class
-
-      rule %r/[^\x30-\x39\x00-\x20\x22\x23\x27\x2b\x2c\x2d\x2e\x5b\x5c\x5d\x7b\x7d\x7f]{1}[^\x00-\x20\x22\x23\x27\x2c\x2e\x5b\x5c\x5d\x7b\x7d\x7f]*/, Name::Attribute
+      rule %r/#{id}(?=\s*\{)/, Name::Class # typeNames
+      rule %r/#{id}/, Name::Attribute # fieldNames
 
       rule %r/[+-]?(?:(?:(?:\d*\.\d+)|(?:\d+(?:\.)?))(?:[eE][+-]?\d+)?)/, Num::Float
       rule %r/(?:0[xX][\da-fA-F]+)|(?:[+-]?\d+)/, Num::Integer
@@ -63,12 +70,12 @@ Jekyll::Hooks.register :site, :pre_render do |site|
 
     state :typeName do
       mixin :comments_and_whitespace
-      rule %r/[^\x30-\x39\x00-\x20\x22\x23\x27\x2b\x2c\x2d\x2e\x5b\x5c\x5d\x7b\x7d\x7f]{1}[^\x00-\x20\x22\x23\x27\x2c\x2e\x5b\x5c\x5d\x7b\x7d\x7f]*/, Name::Class, :pop!
+      rule %r/#{id}/, Name::Class, :pop!
     end
 
     state :name do
       mixin :comments_and_whitespace
-      rule %r/[^\x30-\x39\x00-\x20\x22\x23\x27\x2b\x2c\x2d\x2e\x5b\x5c\x5d\x7b\x7d\x7f]{1}[^\x00-\x20\x22\x23\x27\x2c\x2e\x5b\x5c\x5d\x7b\x7d\x7f]*/, Str::Regex, :pop!
+      rule %r/#{id}/, Str::Regex, :pop!
     end
 
     state :dq do
