cf87745146844323eeb8064d3f205af6a28e7703
Holger Seelig <holger.seelig@gmail.com>
2024-05-29 20:03:26 +0200

    Updated earthquake.

--- docs/_posts/laboratory/real-time-earth-quake-information.md
+++ docs/_posts/laboratory/real-time-earth-quake-information.md
@@ -6,40 +6,97 @@ categories: [Laboratory]
 tags: [Real Time, Earth Quake, Laboratory]
 ---
 <style>
-/* Table */
-
-table {
-  width: 100%;
-  height: 100%;
-  table-layout: fixed;
+#topbar-wrapper {
+   border-bottom: 1px solid var(--main-border-color);
 }
 
-table td {
-   vertical-align: top;
-   padding-right: 2rem;
+.row {
+   margin: 0 !important;
 }
 
+/* Paragraph */
+article > p {
+   padding: 0 2rem;
+}
+
+p {
+   margin: 1rem 0;
+}
+
+/* Viewer */
+.viewer {
+   box-sizing: border-box;
+   height: 100%;
+   border-top: 1px solid var(--main-border-color);
+   height: calc(100vh - 128px - 3.7rem);
+}
+
+@media all and (max-width: 849px)
+{
+   .viewer {
+      border-bottom: 1px solid var(--main-border-color);
+   }
+}
+
+.viewer-row {
+   display: flex;
+   height: 100%;
+}
+
+.viewer-column-70 {
+   flex: 70%;
+   width: 70%;
+   height: 100%;
+}
+
+.viewer-column-30 {
+   flex: 30%;
+   width: 30%;
+   height: 100%;
+   padding-left: 2rem;
+}
+
+.quakes {
+   height: 100%;
+   overflow: hidden;
+}
+
+.locations {
+   height: calc(100% - 4rem);
+}
+
+.link-list {
+   overflow: scroll;
+   height: 100%;
+}
+
+x3d-canvas {
+   display: block;
+   width: 100%;
+   height: 100%;
+   aspect-ratio: unset;
+}
 </style>
 
-<script type="module" src="https://create3000.github.io/media/laboratory/earthquakes/earthquakes.mjs"></script>
+<script type="module" src="/x_ite/assets/laboratory/earthquakes/earthquakes.mjs"></script>
 
-<p>
-   This page uses data from the <a href="https://earthquake.usgs.gov" target="_blank">USGS Earthquakes Hazards Program (EHP)</a> to provide real-time earthquake information.
-</p>
+<p>This page uses data from the <a href="https://earthquake.usgs.gov" target="_blank">USGS Earthquakes Hazards Program (EHP)</a> to provide real-time earthquake information.</p>
 
-<table>
-   <tr>
-      <td><x3d-canvas src="https://create3000.github.io/media/laboratory/earthquakes/earthquakes.x3d" splashScreen="false"></x3d-canvas></td>
-      <td style="width: 30%;">
-         <p>
-            <select id="time">
-               <option value="3">Past 30 Days</option>
-               <option value="2">Past 7 Days</option>
-               <option selected="selected" value="1">Past Day</option>
-               <option value="0">Past Hour</option>
-            </select>
-         </p>
-         <div class="locations"></div>
-      </td>
-   </tr>
-</table>
+<div class="viewer">
+   <div class="viewer-row">
+      <div class="viewer-column-70"><x3d-canvas src="/x_ite/assets/laboratory/earthquakes/earthquakes.x3d" splashScreen="false"></x3d-canvas></div>
+      <div class="viewer-column-30">
+         <div class="quakes">
+            <p>
+               <select id="time">
+                  <option value="3">Past 30 Days</option>
+                  <option value="2">Past 7 Days</option>
+                  <option selected="selected" value="1">Past Day</option>
+                  <option value="0">Past Hour</option>
+               </select>
+            </p>
+            <div class="locations"></div>
+         </div>
+      </div>
+   </div>
+</div>
---
+++ docs/assets/laboratory/earthquakes/earthquakes.mjs
@@ -0,0 +1,187 @@
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/atom.php
+
+$("#time") .on ("change", selectTime);
+$("x3d-canvas") .on ("load", init);
+
+function init ()
+{
+	console .log ("init");
+	selectTime ();
+}
+
+function selectTime ()
+{
+	loadData (parseInt ($("#time") .val ()));
+}
+
+async function loadData (index)
+{
+	const urls = [
+		"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom",
+		"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.atom",
+		"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom",
+		"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.atom",
+	];
+
+	const
+		Browser = X3D .getBrowser (),
+		loading = Browser .currentScene .getNamedNode ("LoadingSwitch");
+
+	loading .whichChoice = 1;
+
+	const
+		response = await fetch (urls [index]),
+		xml      = $.parseXML (await response .text ());
+
+	parseAtom (xml);
+
+	loading .whichChoice = 0;
+}
+
+function parseAtom (xml)
+{
+	console .log ("parseAtom");
+
+	for (var i = 0, length = xml .childNodes .length; i < length; ++ i)
+		generatePoints (parseFeed (xml .childNodes [i]));
+}
+
+function parseFeed (xml)
+{
+	var earthquakes = [ ];
+
+	for (var i = 0, length = xml .childNodes .length; i < length; ++ i)
+	{
+		var earthquake = parseEntry (xml .childNodes [i]);
+
+		if (earthquake)
+			earthquakes .push (earthquake);
+	}
+
+	return earthquakes;
+}
+
+function parseEntry (xml)
+{
+	if (xml .nodeName !== "entry")
+		return;
+
+	var earthquake = { }
+
+	for (var i = 0, length = xml .childNodes .length; i < length; ++ i)
+	{
+		var childNode = xml .childNodes [i];
+
+		switch (childNode .nodeName)
+		{
+			case "title":
+			{
+				var match = childNode .innerHTML .match (/^M\s+([\d\.]+)\s+-\s+(.*?)$/);
+
+				if (! match)
+					break;
+
+				earthquake .magnitude = parseFloat (match [1]);
+				earthquake .location  = match [2];
+				break;
+			}
+			case "georss:point":
+			{
+				var point = childNode .innerHTML .split (" ");
+
+				earthquake .point = [
+					parseFloat (point [0]),
+					parseFloat (point [1]),
+				];
+
+				break;
+			}
+		}
+	}
+
+	if (earthquake .magnitude === undefined)
+		return;
+
+	return earthquake;
+}
+
+function generatePoints (earthquakes)
+{
+	var
+		Browser = X3D .getBrowser (),
+		coord   = Browser .currentScene .getNamedNode ("EarthQuakesCoord"),
+		color   = Browser .currentScene .getNamedNode ("EarthQuakesColor"),
+		red     = new X3D .SFColor (1, 0, 0),
+		yellow  = new X3D .SFColor (1, 1, 0);
+
+	earthquakes .sort (function (lhs, rhs)
+	{
+		if (lhs .magnitude < rhs .magnitude)
+			return 1;
+
+		if (lhs .magnitude > rhs .magnitude)
+			return -1;
+
+		return 0;
+	});
+
+	coord .point .length = 0;
+	color .color .length = 0;
+
+	earthquakes .forEach (function (earthquake)
+	{
+		var m = earthquake .magnitude / 10;
+
+		coord .point .push (new X3D .SFVec3d (earthquake .point [0], earthquake .point [1], 100000 + 1000000 * m));
+		color .color .push (yellow .lerp (red, m));
+	});
+
+	var locations = $(".locations") .empty ();
+
+	var list = $("<ul></ul>")
+		.addClass ("link-list")
+		.appendTo (locations);
+
+	earthquakes .forEach (function (earthquake)
+	{
+		list .append ($("<li></li>")
+			.append ($("<a></a>")
+				.css ({ "cursor": "pointer" })
+				.text (earthquake .magnitude .toFixed (1) + " - " + earthquake .location)
+				.click (selectEarthQuake .bind (null, earthquake))));
+	});
+
+	selectEarthQuake (earthquakes [0])
+}
+
+function selectEarthQuake (earthquake)
+{
+	// Red point
+
+	var
+		Browser = X3D .getBrowser (),
+		coord   = Browser .currentScene .getNamedNode ("EarthQuakeCoord"),
+		timer   = Browser .currentScene .getNamedNode ("EarthQuakeAnimationTimer"),
+		m       = earthquake .magnitude / 10,
+		now     = Date .now () / 1000;
+
+	coord .point [0] = new X3D .SFVec3d (earthquake .point [0], earthquake .point [1], 100000 + 1000000 * m);
+
+	timer .stopTime  = now;
+	timer .startTime = now;
+
+	// Viewpoint
+
+	var
+		timer        = Browser .currentScene .getNamedNode ("ViewpointAnimationTimer"),
+		interpolator = Browser .currentScene .getNamedNode ("ViewpointPositionInterpolator");
+
+	Browser .changeViewpoint ("Viewpoint");
+
+	interpolator .keyValue [0]    = interpolator .geovalue_changed;
+	interpolator .keyValue [1] .x = earthquake .point [0];
+	interpolator .keyValue [1] .y = earthquake .point [1];
+
+	timer .stopTime  = now;
+	timer .startTime = now;
+}
