e070b5b84666197154af09e42acb681b6464cf32
Holger Seelig <holger.seelig@gmail.com>
2024-06-22 10:37:15 +0200

    Added  texParameteri.

--- src/x_ite/Components/CubeMapTexturing/ImageCubeMapTexture.js
+++ src/x_ite/Components/CubeMapTexturing/ImageCubeMapTexture.js
@@ -191,6 +191,8 @@ Object .assign (Object .setPrototypeOf (ImageCubeMapTexture .prototype, X3DEnvir
          gl .texImage2D (gl .TEXTURE_2D, 0, gl .RGBA, gl .RGBA, gl .UNSIGNED_BYTE, this .image [0]);
          gl .texParameteri (gl .TEXTURE_2D, gl .TEXTURE_MIN_FILTER, gl .LINEAR);
          gl .texParameteri (gl .TEXTURE_2D, gl .TEXTURE_MAG_FILTER, gl .LINEAR);
+         gl .texParameteri (gl .TEXTURE_2D, gl .TEXTURE_WRAP_S, gl .CLAMP_TO_EDGE);
+         gl .texParameteri (gl .TEXTURE_2D, gl .TEXTURE_WRAP_T, gl .CLAMP_TO_EDGE);
 
          this .imageToCubeMap (texture, this .image .prop ("width"), this .image .prop ("height"), false);
 
