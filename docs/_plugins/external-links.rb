56a544349b2c8243e85eba636d04213b28e314a2
Holger Seelig <holger.seelig@gmail.com>
2024-07-27 11:57:48 +0200

    Download .zip link files.

--- docs/_plugins/external-links.rb
+++ docs/_plugins/external-links.rb
@@ -5,7 +5,8 @@
          site_url = item.site.config["url"]
 
          content.gsub!(%r{<a\s+href="((?!#{Regexp.escape(site_url)})https?://.*?)"}, "<a href=\"\\1\" target=\"_blank\"")
-         content.gsub!(%r{\.zip" target="_blank"}, ".zip\" download")
+         content.gsub!(%r{\.zip" target="_blank"}, ".zip\"")
+         content.gsub!(%r{\.zip"}, ".zip\" download")
 
          # Update the item content
          item.output = content
