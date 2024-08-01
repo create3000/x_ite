178155333c69f6d8d61441dbe599882f18fd77bc
Holger Seelig <holger.seelig@gmail.com>
2024-08-01 18:57:12 +0200

    Use generateTBN.

--- src/assets/shaders/webgl2/common/Normal2.glsl.js
+++ src/assets/shaders/webgl2/common/Normal2.glsl.js
@@ -42,12 +42,10 @@ getNormalInfo (const in float normalScale)
 {
    #if defined (X3D_NORMAL_TEXTURE)
       vec3 UV = getTexCoord (x3d_NormalTexture .textureTransformMapping, x3d_NormalTexture .textureCoordinateMapping);
-   #else
-      vec3 UV = getTexCoord (0, 0);
-   #endif
 
-   #if defined (X3D_NORMAL_TEXTURE_FLIP_Y)
-      UV .t = 1.0 - UV .t;
+      #if defined (X3D_NORMAL_TEXTURE_FLIP_Y)
+         UV .t = 1.0 - UV .t;
+      #endif
    #endif
 
    vec3 n, t, b, ng;
