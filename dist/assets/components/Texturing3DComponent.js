c274525e8397aad2c1dac2bf9b7946cd48145be5
Holger Seelig <holger.seelig@gmail.com>
2024-08-16 13:56:28 +0200

    Delete key after promise is resolved.

--- dist/assets/components/Texturing3DComponent.js
+++ dist/assets/components/Texturing3DComponent.js
@@ -3,7 +3,7 @@ const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-10.2.0")];
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 485:
+/***/ 466:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -40,7 +40,7 @@ var Ib=[cx,_q,cr,Yr,as,fs,hs,Hu,Su,cx,cx,cx,cx,cx,cx,cx];var Jb=[dx,si,gi,Wh,Kh,
 
 /***/ }),
 
-/***/ 263:
+/***/ 332:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -74,7 +74,7 @@ var _a=[yj,od,ef,yj];var $a=[zj,Li,di,bi,Kb,Lb,Mb,Nb,Rc,Sc,Uc,jd,xd,Ye,lf,yd,zd,
 
 /***/ }),
 
-/***/ 319:
+/***/ 8:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
 /*! dicom-parser - 1.8.12 - 2023-02-20 | (c) 2017 Chris Hafey | https://github.com/cornerstonejs/dicomParser */
@@ -4030,7 +4030,7 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_zlib__;
 
 /***/ }),
 
-/***/ 951:
+/***/ 908:
 /***/ ((module) => {
 
 /* -*- tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
@@ -7314,10 +7314,10 @@ var Decoder = class _Decoder {
 
 //# 
 ;// CONCATENATED MODULE: ./src/x_ite/Browser/Texturing3D/DICOMParser.js
-/* provided dependency */ var dicomParser = __webpack_require__(319);
-/* provided dependency */ var JpegImage = __webpack_require__(951);
-/* provided dependency */ var CharLS = __webpack_require__(485);
-/* provided dependency */ var OpenJPEG = __webpack_require__(263);
+/* provided dependency */ var dicomParser = __webpack_require__(8);
+/* provided dependency */ var JpegImage = __webpack_require__(908);
+/* provided dependency */ var CharLS = __webpack_require__(466);
+/* provided dependency */ var OpenJPEG = __webpack_require__(332);
 /*******************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
