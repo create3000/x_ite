06865e0e3a21b8611759dbbc139876b74c923108
Holger Seelig <holger.seelig@gmail.com>
2024-08-23 21:58:00 +0200

    Build version 10.3.1

--- dist/assets/components/EventUtilitiesComponent.js
+++ dist/assets/components/EventUtilitiesComponent.js
@@ -1,5 +1,5 @@
-/* X_ITE v10.3.0 */
-const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-10.3.0")];
+/* X_ITE v10.3.1 */
+const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-10.3.1")];
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	// The require scope
