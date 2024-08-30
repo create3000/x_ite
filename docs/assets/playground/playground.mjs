a4344108e2bd797470f88419d21d1734859ec9d3
Holger Seelig <holger.seelig@gmail.com>
2024-08-30 15:18:40 +0200

    Updated monaco.

--- docs/assets/playground/playground.mjs
+++ docs/assets/playground/playground.mjs
@@ -6,7 +6,7 @@ class Playground
    constructor ()
    {
       // Also change version in playground.html!
-      require .config ({ paths: { "vs": "https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs" }});
+      require .config ({ paths: { "vs": "https://cdn.jsdelivr.net/npm/monaco-editor@0.51.0/min/vs" }});
       require (["vs/editor/editor.main"], () => this .setup ());
    }
 
