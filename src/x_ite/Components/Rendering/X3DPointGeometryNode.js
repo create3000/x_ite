65c51c5ccca0111fc8a2a38fd434f4ea360461e7
Holger Seelig <holger.seelig@gmail.com>
2024-09-02 18:00:18 +0200

    Check if there are textures.

--- src/x_ite/Components/Rendering/X3DPointGeometryNode.js
+++ src/x_ite/Components/Rendering/X3DPointGeometryNode.js
@@ -80,7 +80,8 @@ Object .assign (Object .setPrototypeOf (X3DPointGeometryNode .prototype, X3DGeom
          attribNodes     = this .getAttrib (),
          attribBuffers   = this .getAttribBuffers ();
 
-      if (!renderContext .transparent)
+      // If we have x3d_TextureMatrix uniforms then we have textures.
+      if (!(renderContext .transparent || shaderNode .x3d_TextureMatrix .length))
          gl .enable (gl .SAMPLE_ALPHA_TO_COVERAGE);
 
       for (const node of renderModeNodes)
@@ -132,7 +133,8 @@ Object .assign (Object .setPrototypeOf (X3DPointGeometryNode .prototype, X3DGeom
          attribNodes     = this .getAttrib (),
          attribBuffers   = this .getAttribBuffers ();
 
-      if (!renderContext .transparent)
+      // If we have x3d_TextureMatrix uniforms then we have textures.
+      if (!(renderContext .transparent || shaderNode .x3d_TextureMatrix .length))
          gl .enable (gl .SAMPLE_ALPHA_TO_COVERAGE);
 
       for (const node of renderModeNodes)
