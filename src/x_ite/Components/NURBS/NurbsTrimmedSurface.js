f8a1708fc76890583dda73767fbd7e5f31686141
Holger Seelig <holger.seelig@gmail.com>
2024-09-04 19:51:23 +0200

    Adjusted MIN_BARYCENTRIC_DISTANCE.

--- src/x_ite/Components/NURBS/NurbsTrimmedSurface.js
+++ src/x_ite/Components/NURBS/NurbsTrimmedSurface.js
@@ -211,7 +211,7 @@ Object .assign (Object .setPrototypeOf (NurbsTrimmedSurface .prototype, X3DNurbs
 
          // Find points in defaultTriangles and interpolate new points.
 
-         const MIN_BARYCENTRIC_DISTANCE = 1e-5;
+         const MIN_BARYCENTRIC_DISTANCE = 1e-3;
 
          for (const p of trimmedTriangles)
          {
