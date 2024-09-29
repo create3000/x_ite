7b8e0c746fda0a5a1a758d84e246e3cc268014ac
Holger Seelig <holger.seelig@gmail.com>
2024-09-29 10:04:15 +0200

    Bump version number.

--- dist/assets/components/HAnimComponent.js
+++ dist/assets/components/HAnimComponent.js
@@ -1,5 +1,5 @@
-/* X_ITE v10.5.3 */
-const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-10.5.3")];
+/* X_ITE v10.5.4 */
+const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-10.5.4")];
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	// The require scope
