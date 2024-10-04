87fcfbd531363587bfaa0d8f34f037162263b465
Holger Seelig <holger.seelig@gmail.com>
2024-10-04 12:27:07 +0200

    Updated dist.

--- dist/assets/components/Texturing3DComponent.js
+++ dist/assets/components/Texturing3DComponent.js
@@ -3,7 +3,7 @@ const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-10.5.4")];
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 882:
+/***/ 772:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -40,7 +40,7 @@ var Ib=[cx,_q,cr,Yr,as,fs,hs,Hu,Su,cx,cx,cx,cx,cx,cx,cx];var Jb=[dx,si,gi,Wh,Kh,
 
 /***/ }),
 
-/***/ 676:
+/***/ 502:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -74,7 +74,7 @@ var _a=[yj,od,ef,yj];var $a=[zj,Li,di,bi,Kb,Lb,Mb,Nb,Rc,Sc,Uc,jd,xd,Ye,lf,yd,zd,
 
 /***/ }),
 
-/***/ 200:
+/***/ 286:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
 /*! dicom-parser - 1.8.12 - 2023-02-20 | (c) 2017 Chris Hafey | https://github.com/cornerstonejs/dicomParser */
@@ -4030,7 +4030,7 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_zlib__;
 
 /***/ }),
 
-/***/ 164:
+/***/ 966:
 /***/ ((module) => {
 
 /* -*- tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
@@ -7314,10 +7314,10 @@ var Decoder = class _Decoder {
 
 //# 
 ;// ./src/x_ite/Browser/Texturing3D/DICOMParser.js
-/* provided dependency */ var dicomParser = __webpack_require__(200);
-/* provided dependency */ var JpegImage = __webpack_require__(164);
-/* provided dependency */ var CharLS = __webpack_require__(882);
-/* provided dependency */ var OpenJPEG = __webpack_require__(676);
+/* provided dependency */ var dicomParser = __webpack_require__(286);
+/* provided dependency */ var JpegImage = __webpack_require__(966);
+/* provided dependency */ var CharLS = __webpack_require__(772);
+/* provided dependency */ var OpenJPEG = __webpack_require__(502);
 /*******************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
