d0730c9db5c0372594e86fbaab4f2917b32744ae
Holger Seelig <holger.seelig@gmail.com>
2024-10-06 17:01:25 +0200

    Reverted changes.

--- src/assets/shaders/webgl2/pbr/PBR2.glsl.js
+++ src/assets/shaders/webgl2/pbr/PBR2.glsl.js
@@ -147,7 +147,7 @@ getMaterialColor ()
    #endif
 
    // Calculate lighting contribution from image based lighting source (IBL)
-   #if defined (X3D_USE_IBL) || defined (X3D_TRANSMISSION_MATERIAL_EXT)
+   #if defined (X3D_USE_IBL)
       f_diffuse = getDiffuseLight (n) * baseColor .rgb;
 
       #if defined (X3D_DIFFUSE_TRANSMISSION_MATERIAL_EXT)
