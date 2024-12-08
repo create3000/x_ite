7da28742246c8998b90114652586c1782790f0a0
Holger Seelig <holger.seelig@gmail.com>
2024-12-08 10:13:27 +0100

    Changed default value of upVector to '0 1 0'.

--- src/x_ite/Components/TextureProjection/TextureProjector.js
+++ src/x_ite/Components/TextureProjection/TextureProjector.js
@@ -210,7 +210,7 @@ Object .defineProperties (TextureProjector,
 
          new X3DFieldDefinition (X3DConstants .inputOutput,    "location",         new Fields .SFVec3f ()),
          new X3DFieldDefinition (X3DConstants .inputOutput,    "direction",        new Fields .SFVec3f (0, 0, 1)),
-         new X3DFieldDefinition (X3DConstants .inputOutput,    "upVector",         new Fields .SFVec3f (0, 0, 1)),
+         new X3DFieldDefinition (X3DConstants .inputOutput,    "upVector",         new Fields .SFVec3f (0, 1, 0)),
          new X3DFieldDefinition (X3DConstants .inputOutput,    "fieldOfView",      new Fields .SFFloat (0.785398)),
          new X3DFieldDefinition (X3DConstants .inputOutput,    "nearDistance",     new Fields .SFFloat (-1)),
          new X3DFieldDefinition (X3DConstants .inputOutput,    "farDistance",      new Fields .SFFloat (-1)),
