0661674164efd7248aebd8ab0934e9228507240a
Holger Seelig <holger.seelig@gmail.com>
2025-01-30 11:57:48 +0100

    Refactor code formatting in Jekyll plugins for consistency and readability

--- docs/_plugins/details_tag.rb
+++ docs/_plugins/details_tag.rb
@@ -3,25 +3,25 @@
 
 module Jekyll
    module Tags
-     class DetailsTag < Liquid::Block
+      class DetailsTag < Liquid::Block
 
-       def initialize(tag_name, markup, tokens)
-         super
-         @caption = markup
-       end
+         def initialize(tag_name, markup, tokens)
+            super
+            @caption = markup
+         end
 
-       def render(context)
-         site = context.registers[:site]
-         converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
-         # below Jekyll 3.x use this:
-         # converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
-         caption = converter.convert(@caption).gsub(/<\/?p[^>]*>/, '').chomp
-         body = converter.convert(super(context))
-         "<details><summary>#{caption}</summary>#{body}</details>"
-       end
+         def render(context)
+            site = context.registers[:site]
+            converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+            # below Jekyll 3.x use this:
+            # converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
+            caption = converter.convert(@caption).gsub(/<\/?p[^>]*>/, "").chomp
+            body = converter.convert(super(context))
+            "<details><summary>#{caption}</summary>#{body}</details>"
+         end
 
-     end
+      end
    end
- end
+end
 
- Liquid::Template.register_tag('details', Jekyll::Tags::DetailsTag)
+Liquid::Template.register_tag("details", Jekyll::Tags::DetailsTag)
--- docs/_plugins/posts-lastmod-hook.rb
+++ docs/_plugins/posts-lastmod-hook.rb
@@ -4,11 +4,11 @@
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+   commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
-  end
+   if commit_num.to_i > 1
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+      post.data['last_modified_at'] = lastmod_date
+   end
 
 end
