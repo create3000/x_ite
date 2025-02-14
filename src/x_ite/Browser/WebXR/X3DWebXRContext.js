eead8d9653c233d1bd22009ebe78fb3c4cb6e934
Holger Seelig <holger.seelig@gmail.com>
2025-02-14 16:34:33 +0100

    Add visibility state check to prevent drawing when session is blurred or hidden.

--- src/x_ite/Browser/WebXR/X3DWebXRContext.js
+++ src/x_ite/Browser/WebXR/X3DWebXRContext.js
@@ -403,6 +403,13 @@ Object .assign (X3DWebXRContext .prototype,
 
          // Draw input source rays.
 
+         switch (this .getSession () .visibilityState)
+         {
+            case "visible-blurred":
+            case "hidden":
+               return;
+         }
+
          for (const [i, { viewMatrix, projectionMatrix }] of pose .views .entries ())
          {
             const frameBuffer = this .getFramebuffers () [i];
