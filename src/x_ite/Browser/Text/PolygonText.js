843ccead73a8f797005041a8c1022b55a8d0fdf7
Holger Seelig <holger.seelig@gmail.com>
2025-02-27 20:28:24 +0100

    Made contours const.

--- src/x_ite/Browser/Text/PolygonText.js
+++ src/x_ite/Browser/Text/PolygonText.js
@@ -243,14 +243,14 @@ Object .assign (Object .setPrototypeOf (PolygonText .prototype, X3DTextGeometry 
       // Get contours for the current glyph.
 
       const
-         steps = this .getBezierSteps (primitiveQuality),
-         path  = glyph .getPath (0, 0, 1);
+         steps    = this .getBezierSteps (primitiveQuality),
+         path     = glyph .getPath (0, 0, 1),
+         contours = [ ];
 
       let
-         contours = [ ],
-         points   = [ ],
-         x        = 0,
-         y        = 0;
+         points = [ ],
+         x      = 0,
+         y      = 0;
 
       // Fix add missing Z command, to make sure the last contour is added.
       path .commands .push ({ type: "Z" });
