4f274bc1bc24f89309ec666d33019092410291ce
Holger Seelig <holger.seelig@gmail.com>
2025-02-27 21:22:26 +0100

    Remove unused imports from PointLight, X3DGeometryNode, and PLYAParser.

--- src/x_ite/Components/Lighting/PointLight.js
+++ src/x_ite/Components/Lighting/PointLight.js
@@ -58,7 +58,6 @@ import ViewVolume           from "../../../standard/Math/Geometry/ViewVolume.js"
 import Vector3              from "../../../standard/Math/Numbers/Vector3.js";
 import Vector4              from "../../../standard/Math/Numbers/Vector4.js";
 import Rotation4            from "../../../standard/Math/Numbers/Rotation4.js";
-import Matrix3              from "../../../standard/Math/Numbers/Matrix3.js";
 import Matrix4              from "../../../standard/Math/Numbers/Matrix4.js";
 import MatrixStack          from "../../../standard/Math/Utility/MatrixStack.js";
 import Algorithm            from "../../../standard/Math/Algorithm.js";
