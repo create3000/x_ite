575b8d83d245164c4cba611e31f39c5cb29f17f3
Holger Seelig <holger.seelig@gmail.com>
2025-03-22 00:28:24 +0100

    Refactor debug logging in ImageTexture to use browser options instead of DEVELOPMENT constant.

--- src/x_ite/Components/Texturing/ImageTexture.js
+++ src/x_ite/Components/Texturing/ImageTexture.js
@@ -53,7 +53,6 @@ import X3DTexture2DNode     from "./X3DTexture2DNode.js";
 import X3DUrlObject         from "../Networking/X3DUrlObject.js";
 import X3DConstants         from "../../Base/X3DConstants.js";
 import Algorithm            from "../../../standard/Math/Algorithm.js";
-import DEVELOPMENT          from "../../DEVELOPMENT.js";
 
 function ImageTexture (executionContext)
 {
@@ -147,7 +146,7 @@ Object .assign (Object .setPrototypeOf (ImageTexture .prototype, X3DTexture2DNod
       if (texture .target !== this .getTarget ())
          return this .setError ({ type: "Invalid KTX texture target, must be 'TEXTURE_2D'." });
 
-      if (DEVELOPMENT)
+      if (this .getBrowser () .getBrowserOption ("Debug"))
       {
          if (this .URL .protocol !== "data:")
             console .info (`Done loading image texture '${decodeURI (this .URL .href)}'.`);
@@ -171,7 +170,7 @@ Object .assign (Object .setPrototypeOf (ImageTexture .prototype, X3DTexture2DNod
    },
    setImage ()
    {
-      if (DEVELOPMENT)
+      if (this .getBrowser () .getBrowserOption ("Debug"))
       {
          if (this .URL .protocol !== "data:")
             console .info (`Done loading image texture '${decodeURI (this .URL .href)}'.`);
