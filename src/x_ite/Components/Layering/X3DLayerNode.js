4f3a0eb5ce4e94b0598d493694833c8d9650d293
Holger Seelig <holger.seelig@gmail.com>
2025-03-23 13:18:25 +0100

    Removed transition active test for collision detection.

--- src/x_ite/Components/Layering/X3DLayerNode.js
+++ src/x_ite/Components/Layering/X3DLayerNode.js
@@ -456,15 +456,11 @@ Object .assign (Object .setPrototypeOf (X3DLayerNode .prototype, X3DNode .protot
 
       return function (type, renderObject)
       {
-         const navigationInfoNode = this .getNavigationInfo ();
-
-         if (navigationInfoNode ._transitionActive .getValue ())
-            return;
-
          const
-            collisionRadius = navigationInfoNode .getCollisionRadius (),
-            avatarHeight    = navigationInfoNode .getAvatarHeight (),
-            size            = Math .max (collisionRadius * 2, avatarHeight * 2);
+            navigationInfoNode = this .getNavigationInfo (),
+            collisionRadius    = navigationInfoNode .getCollisionRadius (),
+            avatarHeight       = navigationInfoNode .getAvatarHeight (),
+            size               = Math .max (collisionRadius * 2, avatarHeight * 2);
 
          Camera .ortho (-size, size, -size, size, -size, size, projectionMatrix);
 
