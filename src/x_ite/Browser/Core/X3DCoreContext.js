86b754c77e11b32aff55e68e8b4a985fb1bca255
Holger Seelig <holger.seelig@gmail.com>
2025-04-18 15:50:54 +0200

    Remove unused styles.

--- src/x_ite/Browser/Core/X3DCoreContext.js
+++ src/x_ite/Browser/Core/X3DCoreContext.js
@@ -130,7 +130,7 @@ function X3DCoreContext (element)
       this [_shadow] = element .prepend (browser);
    }
 
-   $("<div></div>") .addClass ("x_ite-private-x_ite") .html (this .getName () + "<span class='x_ite-private-x3d'>X3D</span>") .appendTo (progress);
+   $("<div></div>") .addClass ("x_ite-private-x_ite") .html (this .getName () + "<b>X3D</b>") .appendTo (progress);
    $("<div></div>") .addClass ("x_ite-private-progressbar")  .appendTo (progress) .append ($("<div></div>"));
    $("<div></div>") .addClass ("x_ite-private-spinner-text") .appendTo (progress);
 
