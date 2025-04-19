dad3239601b1740dd1c7717ad84674a9c4596d8d
Holger Seelig <holger.seelig@gmail.com>
2025-04-19 18:44:03 +0200

    Namespace ui fonts.

--- src/x_ite/Browser/Core/Fonts.js
+++ src/x_ite/Browser/Core/Fonts.js
@@ -54,7 +54,7 @@ function add (path, bold, italic)
       // In case of people embed the CSS file in a HTML page, we need to check if the
       // font is already loaded. If so, we should not add it again.
 
-      document .fonts .add (new FontFace ("PT Sans", `url(${URLs .getFontsURL (path)})`,
+      document .fonts .add (new FontFace ("X_ITE PT Sans", `url(${URLs .getFontsURL (path)})`,
       {
          style: italic ? "italic" : "normal",
          weight: bold ? "700" : "400",
