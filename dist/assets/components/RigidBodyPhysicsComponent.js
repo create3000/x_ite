c457e6c3c5aefdd38d03dd1f6bb5db668c4a0f0d
Holger Seelig <holger.seelig@gmail.com>
2025-04-29 21:14:06 +0200

    Build version 11.5.5

--- dist/assets/components/RigidBodyPhysicsComponent.js
+++ dist/assets/components/RigidBodyPhysicsComponent.js
@@ -1,5 +1,5 @@
-/* X_ITE v11.5.4 */
-const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-11.5.4")];
+/* X_ITE v11.5.5 */
+const __X_ITE_X3D__ = window [Symbol .for ("X_ITE.X3D-11.5.5")];
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
