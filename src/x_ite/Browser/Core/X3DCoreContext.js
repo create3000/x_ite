45f9f0be01a299683c2f5e7e886bd07d32a9b1aa
Holger Seelig <holger.seelig@gmail.com>
2025-05-15 17:36:06 +0200

    Refactor URL handling to simplify string formatting in X3DCoreContext.

--- src/x_ite/Browser/Core/X3DCoreContext.js
+++ src/x_ite/Browser/Core/X3DCoreContext.js
@@ -468,10 +468,10 @@ Object .assign (X3DCoreContext .prototype,
       {
          const url = new Fields .MFString ();
 
-         if (/^\s*\[.*?\]\s*$/s .test (urlCharacters))
-            url .fromString (urlCharacters, this .getExecutionContext ());
-         else
-            url .fromString (`[${urlCharacters}]`, this .getExecutionContext ());
+         if (!/^\[.*?\]$/s .test (urlCharacters))
+            urlCharacters = `[${urlCharacters}]`;
+
+         url .fromString (urlCharacters, this .getExecutionContext ());
 
          return url;
       }
