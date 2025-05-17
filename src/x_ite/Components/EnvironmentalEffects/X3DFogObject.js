4cb78b853374eed3f6023c74b6d777d66ad5ca62
Holger Seelig <holger.seelig@gmail.com>
2025-05-17 16:42:02 +0200

    Fix visibility range calculation in Fog2 shader and update fog type logic in X3DFogObject.

--- src/x_ite/Components/EnvironmentalEffects/X3DFogObject.js
+++ src/x_ite/Components/EnvironmentalEffects/X3DFogObject.js
@@ -144,7 +144,7 @@ Object .assign (X3DFogObject .prototype,
 
       return function ()
       {
-         if (this ._hidden .getValue () || this .visibilityRange === 0)
+         if (this ._hidden .getValue () || (this .visibilityRange === 0 && this .visibilityStart === 0))
             this .fogType = 0;
          else
             this .fogType = fogTypes .get (this ._fogType .getValue ()) || 1;
@@ -156,8 +156,8 @@ Object .assign (X3DFogObject .prototype,
    },
    set_visibilityRange__ ()
    {
+      this .visibilityStart = Math .max (this ._visibilityStart .getValue (), 0);
       this .visibilityRange = Math .max (this ._visibilityRange .getValue (), 0);
-      this .visibilityStart = Algorithm .clamp (this ._visibilityStart .getValue (), 0, this .visibilityRange - 0.001);
 
       this .set_fogType__ ();
    },
