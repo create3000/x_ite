02874dc3462cf6b5b388739a4be46b97ddbaaa5b
Holger Seelig <holger.seelig@gmail.com>
2025-05-24 11:33:14 +0200

    Add comments to clarify normal texture scaling in tangent space

--- src/assets/shaders/webgl2/common/Normal2.glsl.js
+++ src/assets/shaders/webgl2/common/Normal2.glsl.js
@@ -110,6 +110,9 @@ getNormalInfo (const in float normalScale)
          #endif
       #endif
 
+      // Convert from [0, 1] to [-1, 1] range.
+      // Scale by normalScale.
+      // The normal texture is expected to be in tangent space.
       ntex  = ntex * 2.0 - vec3 (1.0);
       ntex *= vec3 (vec2 (normalScale), 1.0);
       ntex  = normalize (ntex);
