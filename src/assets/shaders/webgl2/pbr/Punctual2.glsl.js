494ac152a40a54b7c0a12bb43236458de1603d3f
Holger Seelig <holger.seelig@gmail.com>
2025-06-15 17:39:18 +0200

    Fix UV calculation in getSubsurfaceScattering function by removing unnecessary viewMatrix multiplication

--- src/assets/shaders/webgl2/pbr/Punctual2.glsl.js
+++ src/assets/shaders/webgl2/pbr/Punctual2.glsl.js
@@ -138,7 +138,7 @@ uniform sampler2D x3d_ScatterDepthSamplerEXT;
 // vec3
 // getSubsurfaceScattering (const in vec3 vertex, const in mat4 projectionMatrix, const in float attenuationDistance)
 // {
-//    vec2  uv           = vec2 (projectionMatrix * viewMatrix * vec4 (vertex, 1.0)); // TODO: vertex is already in world space
+//    vec2  uv           = vec2 (projectionMatrix * vec4 (vertex, 1.0)); // TODO: vertex is already in world space
 //    float centerDepth  = texture (x3d_ScatterDepthSamplerEXT, uv) .x;
 //    vec2  texelSize    = 1.0 / vec2 (x3d_Viewport .zw);
 //    vec2  centerVector = uv * centerDepth;
