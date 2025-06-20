7ab8f74d000204af9c0b07eff3604906356c14e0
Holger Seelig <holger.seelig@gmail.com>
2025-06-20 07:24:24 +0200

    Add Subsurface Scattering shader and integrate with Volume Scatter Material Extension.

--- src/x_ite/Components/X_ITE/VolumeScatterMaterialExtension.js
+++ src/x_ite/Components/X_ITE/VolumeScatterMaterialExtension.js
@@ -61,10 +61,12 @@ ExtensionKeys .add ("VOLUME_SCATTER_MATERIAL_EXTENSION");
 
 // Register shaders.
 
-import ShaderRegistry from "../../Browser/Shaders/ShaderRegistry.js";
-import Scatter2       from "../../../assets/shaders/webgl2/pbr/Scatter2.glsl.js";
+import ShaderRegistry       from "../../Browser/Shaders/ShaderRegistry.js";
+import Scatter2              from "../../../assets/shaders/webgl2/pbr/Scatter2.glsl.js";
+import SubsurfaceScattering2 from "../../../assets/shaders/webgl2/pbr/SubsurfaceScattering2.glsl.js";
 
-ShaderRegistry .addInclude ("Scatter", undefined, Scatter2);
+ShaderRegistry .addInclude ("Scatter",              undefined, Scatter2);
+ShaderRegistry .addInclude ("SubsurfaceScattering", undefined, SubsurfaceScattering2);
 
 /**
  * THIS NODE IS STILL EXPERIMENTAL.
