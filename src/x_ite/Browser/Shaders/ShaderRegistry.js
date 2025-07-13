9ca2961f355273e6f62b4ecd7344e19bd01803a5
Holger Seelig <holger.seelig@gmail.com>
2025-07-13 18:41:23 +0200

    Sort imports.

--- src/x_ite/Browser/Shaders/ShaderRegistry.js
+++ src/x_ite/Browser/Shaders/ShaderRegistry.js
@@ -3,11 +3,11 @@ import ClipPlanes2 from "../../../assets/shaders/webgl2/common/ClipPlanes2.glsl.
 import Fog2        from "../../../assets/shaders/webgl2/common/Fog2.glsl.js";
 import Fragment2   from "../../../assets/shaders/webgl2/common/Fragment2.glsl.js";
 import Hatch2      from "../../../assets/shaders/webgl2/common/Hatch2.glsl.js";
+import Instancing2 from "../../../assets/shaders/webgl2/common/Instancing2.glsl.js";
 import Lighting2   from "../../../assets/shaders/webgl2/common/Lighting2.glsl.js";
 import Line22      from "../../../assets/shaders/webgl2/common/Line22.glsl.js";
 import Material2   from "../../../assets/shaders/webgl2/common/Material2.glsl.js";
 import Normal2     from "../../../assets/shaders/webgl2/common/Normal2.glsl.js";
-import Instancing2 from "../../../assets/shaders/webgl2/common/Instancing2.glsl.js";
 import Perlin2     from "../../../assets/shaders/webgl2/common/Perlin2.glsl.js";
 import Point2      from "../../../assets/shaders/webgl2/common/Point2.glsl.js";
 import PointSize2  from "../../../assets/shaders/webgl2/common/PointSize2.glsl.js";
@@ -27,13 +27,13 @@ import Punctual2     from "../../../assets/shaders/webgl2/pbr/Punctual2.glsl.js"
 import ToneMapping2  from "../../../assets/shaders/webgl2/pbr/ToneMapping2.glsl.js";
 
 /* WebGL 2 Vertex and Fragment */
-import FullScreenVertex2      from "../../../assets/shaders/webgl2/FullScreen2.vs.js";
 import DefaultVertex2         from "../../../assets/shaders/webgl2/Default2.vs.js";
 import DepthFragment2         from "../../../assets/shaders/webgl2/Depth2.fs.js";
 import DepthVertex2           from "../../../assets/shaders/webgl2/Depth2.vs.js";
-import MaterialFragment2      from "../../../assets/shaders/webgl2/Material2.fs.js";
+import FullScreenVertex2      from "../../../assets/shaders/webgl2/FullScreen2.vs.js";
 import LineTransformFragment2 from "../../../assets/shaders/webgl2/LineTransform2.fs.js";
 import LineTransformVertex2   from "../../../assets/shaders/webgl2/LineTransform2.vs.js";
+import MaterialFragment2      from "../../../assets/shaders/webgl2/Material2.fs.js";
 import OITComposeFragment2    from "../../../assets/shaders/webgl2/OITCompose2.fs.js";
 import PhysicalFragment2      from "../../../assets/shaders/webgl2/Physical2.fs.js";
 import PointingFragment2      from "../../../assets/shaders/webgl2/Pointing2.fs.js";
