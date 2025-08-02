1bf2c7bc11497a311b30ad6aa0b97d1c795a8b8f
Holger Seelig <holger.seelig@gmail.com>
2025-08-02 16:28:59 +0200

    Added test.

--- src/tests.js
+++ src/tests.js
@@ -69,6 +69,7 @@ const X_ITE_TESTS = [
    { path: "Extrusion/ExtrusionXYZ.wrl" },
    { path: "Extrusion/ExtrusionXZ-hi.wrl" },
    { path: "Extrusion/ExtrusionYZ.wrl" },
+   { path: "Extrusion/Möbius.x3d" },
    { path: "Extrusion/pipe.wrl" },
    { path: "Extrusion/Rotations.x3d" },
    { path: "Extrusion/Scales.x3d" },
