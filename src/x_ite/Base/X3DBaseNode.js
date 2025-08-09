b4a03290428514f3afece06dbd7f749921ad7add
Holger Seelig <holger.seelig@gmail.com>
2025-08-09 23:11:10 +0200

    Fix revert changes.

--- src/x_ite/Base/X3DBaseNode.js
+++ src/x_ite/Base/X3DBaseNode.js
@@ -36,11 +36,19 @@ function X3DBaseNode (executionContext, browser = executionContext .getBrowser (
    this [_live]              = true;
    this [_initialized]       = false;
 
+   // Create fields.
+
    if (this .canUserDefinedFields ())
       this [_fieldDefinitions] = new FieldDefinitionArray (this [_fieldDefinitions]);
 
    for (const fieldDefinition of this [_fieldDefinitions])
       this .addPredefinedField (fieldDefinition);
+
+   // Create events.
+
+   this .addChildObjects (X3DConstants .outputOnly, "name_changed",     new Fields .SFTime (),
+                          X3DConstants .outputOnly, "typeName_changed", new Fields .SFTime (),
+                          X3DConstants .outputOnly, "parents_changed",  new Fields .SFTime ());
 }
 
 Object .assign (Object .setPrototypeOf (X3DBaseNode .prototype, X3DChildObject .prototype),
@@ -547,24 +555,6 @@ for (const key of Object .keys (X3DBaseNode .prototype))
 
 Object .defineProperties (X3DBaseNode .prototype,
 {
-   ... Object .fromEntries (["name_changed", "typeName_changed", "parents_changed"] .map (name =>
-   {
-      return [`_${name}`,
-      {
-         get ()
-         {
-            this .addChildObjects (X3DConstants .outputOnly, name, new Fields .SFTime ());
-
-            return this [`_${name}`];
-         },
-         set (value)
-         {
-            this .addChildObjects (X3DConstants .outputOnly, name, new Fields .SFTime ());
-
-            this [`_${name}`] .setValue (value);
-         },
-      }];
-   })),
    name_changed:
    {
       get () { return this ._name_changed; },
