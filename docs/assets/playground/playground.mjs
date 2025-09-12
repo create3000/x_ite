a434c0e255ce4a51542a1fce98c54315bc13c275
Holger Seelig <holger.seelig@gmail.com>
2025-09-13 01:37:59 +0200

    Reverted changes because of an fatal bug.

--- docs/assets/playground/playground.mjs
+++ docs/assets/playground/playground.mjs
@@ -11,7 +11,7 @@ class Playground
    constructor ()
    {
       // Also change version in playground.html!
-      require .config ({ paths: { "vs": "https://cdn.jsdelivr.net/npm/monaco-editor@0.53.0/min/vs" }});
+      require .config ({ paths: { "vs": "https://cdn.jsdelivr.net/npm/monaco-editor@0.52.0/min/vs" }});
       require (["vs/editor/editor.main"], () => this .setup ());
    }
 
