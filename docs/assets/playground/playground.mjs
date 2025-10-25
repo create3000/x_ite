a6fdbff2b5f35087ef4c143f850094da9a27a7ee
Holger Seelig <holger.seelig@gmail.com>
2025-10-25 22:11:42 +0200

    Update MONACO_VERSION handling in Playground to dynamically fetch version from script source.

--- docs/assets/playground/playground.mjs
+++ docs/assets/playground/playground.mjs
@@ -1,3 +1,5 @@
+const MONACO_VERSION = $(`script[src*="monaco-editor"]`) .attr ("src") .match (/\/monaco-editor(@?.*?)\//) [1];
+
 class Playground
 {
    autoUpdate = true;
@@ -11,7 +13,7 @@ class Playground
    constructor ()
    {
       // Also change version in playground.md!
-      require .config ({ paths: { "vs": "https://cdn.jsdelivr.net/npm/monaco-editor@0.54.0/min/vs" }});
+      require .config ({ paths: { "vs": `https://cdn.jsdelivr.net/npm/monaco-editor${MONACO_VERSION}/min/vs` }});
       require (["vs/editor/editor.main"], () => this .setup ());
    }
 
