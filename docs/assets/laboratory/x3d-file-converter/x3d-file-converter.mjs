5da20f35ea1e3cc64e99fa668a7720da113a482a
Holger Seelig <holger.seelig@gmail.com>
2025-11-12 14:14:05 +0100

    Added file size.

--- docs/assets/laboratory/x3d-file-converter/x3d-file-converter.mjs
+++ docs/assets/laboratory/x3d-file-converter/x3d-file-converter.mjs
@@ -166,11 +166,35 @@ ${scene .toXMLString ({ html: true, indent: " " .repeat (6) }) .trimEnd ()}
 function link (mimeType, name, x3dSyntax)
 {
    const a = $("<a></a>")
-      .text (name)
+      .text (`${name} (${humanFileSize (x3dSyntax .length, true)})`)
       .attr ("href", "#")
       .on ("click", () => download (mimeType, name, x3dSyntax));
 
    $("<li></li>") .append (a) .appendTo ($("#download-links"));
+}
+
+function humanFileSize (bytes, si = false, dp = 1)
+{
+   const thresh = si ? 1000 : 1024;
+
+   if (Math .abs (bytes) < thresh)
+      return bytes + ' B';
+
+   const units = si
+      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+
+   const r = 10 ** dp;
+
+   let u = -1;
+
+   do {
+      bytes /= thresh;
+      ++ u;
+   }
+   while (Math .round (Math .abs (bytes) * r) / r >= thresh && u < units .length - 1);
+
+   return bytes .toFixed (dp) + ' ' + units [u];
 }
 
 function download (mimeType, name, x3dSyntax)
