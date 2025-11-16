8dcc0c9f65d4dffc2b9a26f8f19ad056a936bd62
Holger Seelig <holger.seelig@gmail.com>
2025-11-16 19:03:29 +0100

    Updated HTML page output.

--- docs/assets/laboratory/x3d-file-converter/x3d-file-converter.mjs
+++ docs/assets/laboratory/x3d-file-converter/x3d-file-converter.mjs
@@ -152,7 +152,7 @@ function getHTML (scene, filename)
 <html>
   <head>
     <meta charset="utf-8">
-    <script src="https://cdn.jsdelivr.net/npm/x_ite@latest/dist/x_ite.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/x_ite@latest/dist/x_ite.min.js"></script>
     <style>
 body {
   display: flex;
