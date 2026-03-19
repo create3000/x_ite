544331a909b3810944160e4be3f236f2b1762831
Holger Seelig <holger.seelig@gmail.com>
2026-03-19 14:14:45 +0100

    Fixed reflect.

--- src/standard/Math/Numbers/Vector3.js
+++ src/standard/Math/Numbers/Vector3.js
@@ -215,7 +215,7 @@ Object .assign (Vector3 .prototype,
    reflect (normal)
    {
       const
-         d = 2 * normal .dot (i),
+         d = 2 * normal .dot (this),
          x = normal .x * d,
          y = normal .y * d,
          z = normal .z * d;
--- src/standard/Math/Numbers/Vector4.js
+++ src/standard/Math/Numbers/Vector4.js
@@ -221,7 +221,7 @@ Object .assign (Vector4 .prototype,
    reflect (normal)
    {
       const
-         d = 2 * normal .dot (i),
+         d = 2 * normal .dot (this),
          x = normal .x * d,
          y = normal .y * d,
          z = normal .z * d,
