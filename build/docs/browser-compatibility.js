4357585c3e7f9ae95615aa5aff207932aa13be55
Holger Seelig <holger.seelig@gmail.com>
2026-03-22 13:30:55 +0100

    Check json file.

--- build/docs/browser-compatibility.js
+++ build/docs/browser-compatibility.js
@@ -30,24 +30,17 @@ function browserCompatibility (js)
       js_f = sh ("cat", js),
       md   = `docs/_posts/components/${component}/${typeName}.md`;
 
-   let md_f = sh ("cat", md);
-
-   let
-      x_ite   = js_f .match (/THIS NODE IS NOT SUPPORTED/) ? false : true,
-      castle  = undefined,
-      x3dom   = undefined,
-      freewrl = undefined;
-
    const map = new Map ([
       [undefined, `<i class="fa-solid fa-circle-question blue" title="Unknown Support"></i>`],
       [true,      `<i class="fa-solid fa-circle-check green" title="Supported"></i>`],
       [false,     `<i class="fa-solid fa-circle-xmark red" title="Not Supported"></i>`],
    ]);
 
-   x_ite   = map .get (x_ite);
-   castle  = map .get (castle);
-   x3dom   = map .get (x3dom);
-   freewrl = map .get (freewrl);
+   const
+      x_ite   = map .get (js_f .match (/THIS NODE IS NOT SUPPORTED/) ? false : true),
+      castle  = map .get (json [typeName] ?.["castle"]),
+      x3dom   = map .get (json [typeName] ?.["x3dom"]),
+      freewrl = map .get (json [typeName] ?.["freewrl"]);
 
    const replacement = () => `## Browser Compatibility
 
@@ -57,6 +50,8 @@ function browserCompatibility (js)
 {: .browser-compatibility }
 
 ## `;
+
+   let md_f = sh ("cat", md);
 
    md_f = md_f .replace (/## Browser Compatibility.*?##\s/s, replacement);
 
