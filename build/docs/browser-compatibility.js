ce5dc8a81df1b1cdac000151404651723e2ebf0e
Holger Seelig <holger.seelig@gmail.com>
2026-03-22 12:40:09 +0100

    Read files.

--- build/docs/browser-compatibility.js
+++ build/docs/browser-compatibility.js
@@ -1,9 +1,27 @@
 const
-   json = require ("./browser-compatibility.json");
+   json   = require ("./browser-compatibility.json"),
+   { sh } = require ("shell-tools"),
+   fs     = require ("fs");
 
 function main ()
 {
    console .log ("Update browser compatibility ...");
+
+   const files = sh ("ls -C1 src/x_ite/Components/**/*.js") .trim () .split ("\n");
+
+   files .forEach (filename => browserCompatibility (filename));
+}
+
+function browserCompatibility (filename)
+{
+   const [, component, typeName] = filename .match (/([^/]+?)\/([^/]+?)\.js$/);
+
+   if (typeName .startsWith ("X3D"))
+      return;
+
+   console .log (component, typeName);
+
+   let file = sh ("cat", filename);
 }
 
 main ();
