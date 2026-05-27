d53c2535a176636a34e97dd33a050f27314ac4b2
Holger Seelig <holger.seelig@gmail.com>
2026-05-27 11:17:15 +0200

    Use premultiplied alpha.

--- src/x_ite/Components/X_ITE/GaussianSplats.js
+++ src/x_ite/Components/X_ITE/GaussianSplats.js
@@ -331,7 +331,7 @@ main ()
    if (alpha < 1.0 / 255.0)
       discard;
 
-   vec4 finalColor = vec4 (color .rgb, alpha); // premultiplied-alpha output
+   vec4 finalColor = vec4 (color .rgb * alpha, alpha); // premultiplied-alpha output
 
    #if defined (X3D_ORDER_INDEPENDENT_TRANSPARENCY)
       oit (finalColor);
@@ -642,15 +642,13 @@ Object .assign (Object .setPrototypeOf (GaussianSplatsShape .prototype, X3DShape
       }
 
       // TODO: sort splats.
-      // gl .blendFunc (gl .ONE, gl .ONE_MINUS_SRC_ALPHA);
 
+      gl .blendFunc (gl .ONE, gl .ONE_MINUS_SRC_ALPHA);
       gl .frontFace (gl .CCW);
       gl .enable (gl .CULL_FACE);
 
       gl .drawArraysInstanced (gl .TRIANGLES, 0, 6, this .numSplats);
-
-      // TODO: sort splats.
-      // gl .blendFuncSeparate (gl .SRC_ALPHA, gl .ONE_MINUS_SRC_ALPHA, gl .ONE, gl .ONE_MINUS_SRC_ALPHA);
+      gl .blendFuncSeparate (gl .SRC_ALPHA, gl .ONE_MINUS_SRC_ALPHA, gl .ONE, gl .ONE_MINUS_SRC_ALPHA);
    },
    getShader (renderContext)
    {
