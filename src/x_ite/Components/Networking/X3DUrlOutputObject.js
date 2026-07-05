9846e87226fd12b8e4413e71593148ba7a59f8c0
Holger Seelig <holger.seelig@gmail.com>
2026-07-05 20:32:04 +0200

    Made X3DUrlOutputObject of X3DNetworkSensorNode.

--- src/x_ite/Components/Networking/X3DUrlOutputObject.js
+++ src/x_ite/Components/Networking/X3DUrlOutputObject.js
@@ -1,18 +1,15 @@
-import X3DNode      from "../Core/X3DNode.js";
-import X3DConstants from "../../Base/X3DConstants.js";
+import X3DNode              from "../Core/X3DNode.js";
+import X3DConstants         from "../../Base/X3DConstants.js";
+import X3DNetworkSensorNode from "./X3DNetworkSensorNode.js";
 
 function X3DUrlOutputObject (executionContext)
 {
+   X3DNetworkSensorNode .call (this, executionContext);
+
    this .addType (X3DConstants .X3DUrlOutputObject);
 }
 
-Object .assign (X3DUrlOutputObject .prototype,
-{
-   initialize ()
-   { },
-   dispose ()
-   { },
-});
+Object .setPrototypeOf (X3DUrlOutputObject .prototype, X3DNetworkSensorNode .prototype);
 
 Object .defineProperties (X3DUrlOutputObject, X3DNode .getStaticProperties ("X3DUrlOutputObject", "Networking", 4));
 
