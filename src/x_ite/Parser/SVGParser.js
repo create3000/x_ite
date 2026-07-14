10eb4d61a66bb4f256983c2fa08f6e8ee43bc533
Holger Seelig <holger.seelig@gmail.com>
2026-07-15 01:07:09 +0200

    Remove need of jQuery.

--- src/x_ite/Parser/SVGParser.js
+++ src/x_ite/Parser/SVGParser.js
@@ -196,7 +196,7 @@ Object .assign (Object .setPrototypeOf (SVGParser .prototype, X3DParser .prototy
       {
          case "#document":
          {
-            const svg = Array .from (xmlElement .children) .filter (child => child .matches ("svg"))
+            const svg = Array .from (xmlElement .children) .filter (child => child .matches ("svg"));
 
             for (const xmlElement of svg)
                await this .svgElement (xmlElement);
