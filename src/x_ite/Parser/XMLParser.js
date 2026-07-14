7d70d9516d3eb2847fc4405f19eae427b66d9b38
Holger Seelig <holger.seelig@gmail.com>
2026-07-15 01:23:11 +0200

    Remove need of jQuery.

--- src/x_ite/Parser/XMLParser.js
+++ src/x_ite/Parser/XMLParser.js
@@ -97,7 +97,7 @@ Object .assign (Object .setPrototypeOf (XMLParser .prototype, X3DParser .prototy
       {
          case "#document":
          {
-            const X3D = $(xmlElement) .children ("X3D");
+            const X3D = Array .from (xmlElement .children) .filter (child => child .matches ("X3D"));
 
             if (X3D .length)
             {
