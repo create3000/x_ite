e48e4d769a34cee90462338e77296a75bee274b3
Holger Seelig <holger.seelig@gmail.com>
2026-07-15 16:24:54 +0200

    Updated eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,7 @@ export default defineConfig ([
     rules: {
       "no-async-promise-executor": "off",
       "no-empty": ["error", { "allowEmptyCatch": true }],
+      "semi": "error",
     },
   },
   {
