919346dd456d37a238bf71c9b8d0578217522c7a
Holger Seelig <holger.seelig@gmail.com>
2026-07-16 10:41:14 +0200

    Remove jest workaround.

--- src/x_ite/Browser/Rendering/X3DRenderingContext.js
+++ src/x_ite/Browser/Rendering/X3DRenderingContext.js
@@ -58,11 +58,6 @@ Object .assign (X3DRenderingContext .prototype,
       {
          this .reshape ();
 
-         // Unfortunately jest environment doesn't like a traverse here.
-         // TODO: figure out why.
-         if (typeof jest !== "undefined")
-            return;
-
          this [Symbol .for ("X_ITE.X3DBrowserContext.traverse")] (performance .now ());
       });
 
