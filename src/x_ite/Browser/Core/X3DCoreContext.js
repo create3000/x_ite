9fa52c3f5b636605dd1a9c6a7e88b2d5c87c3709
Holger Seelig <holger.seelig@gmail.com>
2026-07-17 19:58:49 +0200

    Reorder properties.

--- src/x_ite/Browser/Core/X3DCoreContext.js
+++ src/x_ite/Browser/Core/X3DCoreContext.js
@@ -107,9 +107,9 @@ function X3DCoreContext (element)
    this [_localStorage] = new DataStorage (localStorage, `X_ITE.X3DBrowser(${this [_instanceId]}).`);
    this [_element]      = element;
    this [_surface]      = this [_shadow] .querySelector (".x_ite-private-surface");
+   this [_splashScreen] = this [_shadow] .querySelector (".x_ite-private-splash-screen");
    this [_canvas]       = this [_shadow] .querySelector ("canvas");
    this [_context]      = Context .create (this [_canvas], element .getAttribute ("preserveDrawingBuffer") === "true");
-   this [_splashScreen] = this [_shadow] .querySelector (".x_ite-private-splash-screen");
 
    const privateScene = this .getPrivateScene ();
 
