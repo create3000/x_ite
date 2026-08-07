f2b2e41bc5f50733aac557f0f6f686a5ef04df59
Holger Seelig <holger.seelig@gmail.com>
2026-08-07 09:38:16 +0200

    Reverted changes.

--- src/x_ite/Parser/GLTF2Parser.js
+++ src/x_ite/Parser/GLTF2Parser.js
@@ -1781,10 +1781,7 @@ function eventsProcessed ()
       if (name)
       {
          for (const shapeNode of shapeNodes)
-         {
             scene .addNamedNode (scene .getUniqueName (name), shapeNode);
-            scene .addNamedNode (scene .getUniqueName (name + "-Geometry"), shapeNode ._geometry);
-         }
       }
 
       return mesh .shapeNodes = shapeNodes;
