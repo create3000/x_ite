2716fd04fb55a8f9a1fb61bc160a84798f2d5aeb
Holger Seelig <holger.seelig@gmail.com>
2026-08-07 23:58:44 +0200

    Updated dist.

--- dist/x_ite.js
+++ dist/x_ite.js
@@ -12,7 +12,7 @@
 return /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 934
+/***/ 220
 (module, exports) {
 
 var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*
@@ -1017,7 +1017,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 
 /***/ },
 
-/***/ 420
+/***/ 582
 (module) {
 
 (function webpackUniversalModuleDefinition(root, factory) {
@@ -2013,7 +2013,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 
-/***/ 768
+/***/ 222
 (module) {
 
 /**
@@ -20017,14 +20017,12 @@ Object .assign (Object .setPrototypeOf (BrowserOptions .prototype, Base_X3DBaseN
       {
          if (this .wasLive === true)
             browser .beginUpdate ();
-         else if (this .wasLive === false)
-            browser .endUpdate ();
 
          this .wasLive = undefined;
       }
       else
       {
-         // If this case is made a second time, ignore browser is live state.
+         // If this branch is processed a second time, ignore browser is live state.
          this .wasLive ??= browser .isLive ();
 
          browser .endUpdate ();
@@ -31162,7 +31160,7 @@ const Plane3_default_ = Plane3;
 
 /* harmony default export */ const Geometry_Plane3 = (x_ite_Namespace .add ("Plane3", Plane3_default_));
 ;// ./src/standard/Math/Geometry/Triangle3.js
-/* provided dependency */ var libtess = __webpack_require__(768);
+/* provided dependency */ var libtess = __webpack_require__(222);
 
 
 const Triangle3 =
@@ -47828,7 +47826,7 @@ const Bezier_default_ = Bezier;
 
 /* harmony default export */ const Algorithms_Bezier = (x_ite_Namespace .add ("Bezier", Bezier_default_));
 ;// ./src/x_ite/Parser/SVGParser.js
-/* provided dependency */ var SVGParser_libtess = __webpack_require__(768);
+/* provided dependency */ var SVGParser_libtess = __webpack_require__(222);
 
 
 
@@ -91150,8 +91148,8 @@ const PNGMedia_default_ = PNGMedia;
 
 /* harmony default export */ const Texturing_PNGMedia = (x_ite_Namespace .add ("PNGMedia", PNGMedia_default_));
 ;// ./src/x_ite/Components/Texturing/MovieTexture.js
-/* provided dependency */ var SuperGif = __webpack_require__(934);
-/* provided dependency */ var APNG = __webpack_require__(420);
+/* provided dependency */ var SuperGif = __webpack_require__(220);
+/* provided dependency */ var APNG = __webpack_require__(582);
 
 
 
@@ -93856,7 +93854,7 @@ const QuickSort_default_ = QuickSort;
 
 /* harmony default export */ const Algorithms_QuickSort = (x_ite_Namespace .add ("QuickSort", QuickSort_default_));
 ;// ./src/lib/libtess.js
-/* provided dependency */ var libtess_libtess = __webpack_require__(768);
+/* provided dependency */ var libtess_libtess = __webpack_require__(222);
 const libtess_default_ = libtess_libtess;
 ;
 
