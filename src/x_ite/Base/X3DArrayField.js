2d111bf644bed0be3dd9c439edf93f2f3f05eeb6
Holger Seelig <holger.seelig@gmail.com>
2026-08-17 17:44:42 +0200

    Added comments.

--- src/x_ite/Base/X3DArrayField.js
+++ src/x_ite/Base/X3DArrayField.js
@@ -100,7 +100,7 @@ Object .defineProperties (X3DArrayField,
 
                   return array;
                },
-               configurable: true,
+               configurable: true, // TODO: use Object.setPrototypeOf(X3DArrayField, X3DField)
             },
             fromArray:
             {
@@ -112,7 +112,7 @@ Object .defineProperties (X3DArrayField,
 
                   return array;
                },
-               configurable: true,
+               configurable: true, // TODO: use Object.setPrototypeOf(X3DArrayField, X3DField)
             },
          });
       },
