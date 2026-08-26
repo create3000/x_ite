eebbbaf56b92866992ef1a38de7e73e4dd0a1f86
Holger Seelig <holger.seelig@gmail.com>
2026-08-26 12:55:42 +0200

    Add AudioIntensity and DisplayColorSpace options to X3D namespace.

--- src/x_ite.d.ts
+++ src/x_ite.d.ts
@@ -396,6 +396,7 @@ declare namespace X3D
       Shading:	                     ShadingTypes,
       MotionBlur:                   boolean,
       // Additional options:
+      AudioIntensity:               number,
       AlwaysUpdateGeometries:       boolean,
       AutoUpdate:                   boolean,
       Cache:                        boolean,
@@ -403,6 +404,7 @@ declare namespace X3D
       ContentScale:                 number,
       ContextMenu:                  boolean,
       Debug:                        boolean,
+      DisplayColorSpace:            "SRGB" | "DISPLAY_P3",
       Exposure:                     number,
       Gravity:                      number,
       LoadUrlObjects:               boolean,
